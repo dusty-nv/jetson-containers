66d8a8903389389003a4da9ac015f30bbe037871
Dustin Franklin <dustinf@nvidia.com>
2020-07-31 21:15:39 -0400

    added ROS2 Eloquent container

--- scripts/docker_build_ros.sh
+++ scripts/docker_build_ros.sh
@@ -4,6 +4,9 @@ set -e
 
 BASE_IMAGE="nvcr.io/nvidian/nvidia-l4t-base:r32.4.3"
 
-sh ./scripts/docker_build.sh ros:foxy-ros-base-l4t-r32.4.3 Dockerfile.ros.foxy \
-		--build-arg BASE_IMAGE=$BASE_IMAGE
+# ROS2 Eloquent
+sh ./scripts/docker_build.sh ros:eloquent-ros-base-l4t-r32.4.3 Dockerfile.ros.eloquent --build-arg BASE_IMAGE=$BASE_IMAGE
+
+# ROS2 Foxy
+sh ./scripts/docker_build.sh ros:foxy-ros-base-l4t-r32.4.3 Dockerfile.ros.foxy --build-arg BASE_IMAGE=$BASE_IMAGE
 
