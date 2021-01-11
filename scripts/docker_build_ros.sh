42c941240d4d27209ee91c37486438d15c6c6578
f-fl0 <66578286+f-fl0@users.noreply.github.com>
2021-01-11 16:34:10 +0900

    Simplify ROS related scripts.

--- scripts/docker_build_ros.sh
+++ scripts/docker_build_ros.sh
@@ -4,26 +4,17 @@ set -e
 
 BASE_IMAGE="nvcr.io/nvidia/l4t-base"
 L4T_VERSION="r32.4.4"
+SUPPORTED_ROS_DISTROS=("melodic" "noetic" "eloquent" "foxy")
 ROS_DISTRO=${1:-"all"}
 
 echo "building containers for $ROS_DISTRO..."
 
-# ROS Melodic
-if [[ "$ROS_DISTRO" == "melodic" || "$ROS_DISTRO" == "all" ]]; then
-	sh ./scripts/docker_build.sh ros:melodic-ros-base-l4t-$L4T_VERSION Dockerfile.ros.melodic --build-arg BASE_IMAGE=$BASE_IMAGE:$L4T_VERSION
+if [[ "$ROS_DISTRO" == "all" ]]; then
+	TO_BUILD=${SUPPORTED_ROS_DISTROS[@]}
+else
+	TO_BUILD=($ROS_DISTRO)
 fi
 
-# ROS Noetic
-if [[ "$ROS_DISTRO" == "noetic" || "$ROS_DISTRO" == "all" ]]; then
-	sh ./scripts/docker_build.sh ros:noetic-ros-base-l4t-$L4T_VERSION Dockerfile.ros.noetic --build-arg BASE_IMAGE=$BASE_IMAGE:$L4T_VERSION
-fi
-
-# ROS2 Eloquent
-if [[ "$ROS_DISTRO" == "eloquent" || "$ROS_DISTRO" == "all" ]]; then
-	sh ./scripts/docker_build.sh ros:eloquent-ros-base-l4t-$L4T_VERSION Dockerfile.ros.eloquent --build-arg BASE_IMAGE=$BASE_IMAGE:$L4T_VERSION
-fi
-
-# ROS2 Foxy
-if [[ "$ROS_DISTRO" == "foxy" || "$ROS_DISTRO" == "all" ]]; then
-	sh ./scripts/docker_build.sh ros:foxy-ros-base-l4t-$L4T_VERSION Dockerfile.ros.foxy --build-arg BASE_IMAGE=$BASE_IMAGE:$L4T_VERSION
-fi
+for DISTRO in ${TO_BUILD[@]}; do
+	sh ./scripts/docker_build.sh ros:$DISTRO-ros-base-l4t-$L4T_VERSION Dockerfile.ros.$DISTRO --build-arg BASE_IMAGE=$BASE_IMAGE:$L4T_VERSION
+done
