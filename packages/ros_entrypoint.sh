b38bf0fcec74175767e32c6aa6eafae8295a98e2
dusty-nv <dustinf@nvidia.com>
2021-05-27 13:02:52 -0400

    ROS updates

--- packages/ros_entrypoint.sh
+++ packages/ros_entrypoint.sh
@@ -6,9 +6,7 @@ echo "sourcing /opt/ros/$ROS_DISTRO/setup.bash"
 # setup ROS environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 
-echo "ROS_DISTRO       $ROS_DISTRO"
-echo "ROS_ROOT         $ROS_ROOT"
-echo "ROS_PACKAGE_PATH $ROS_PACKAGE_PATH"
-echo "ROS_MASTER_URI   $ROS_MASTER_URI"
+echo "ROS_DISTRO $ROS_DISTRO"
+echo "ROS_ROOT   $ROS_ROOT"
 
 exec "$@"
