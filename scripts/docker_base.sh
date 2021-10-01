6ff2d834849c17c189513702d08563fbb562e142
Collin Hays <Collin.Hays@torchtechnologies.com>
2021-10-01 15:54:59 -0500

    Update for JetPack 4.5.2

--- scripts/docker_base.sh
+++ scripts/docker_base.sh
@@ -13,6 +13,8 @@ if [ $L4T_RELEASE -eq 32 ]; then
 	elif [ $L4T_REVISION_MAJOR -eq 5 ]; then
 		if [ $L4T_REVISION_MINOR -eq 1 ]; then
 			BASE_IMAGE="nvcr.io/nvidia/l4t-base:r32.5.0"
+		elif [ $L4T_REVISION_MINOR -eq 2 ]; then
+			BASE_IMAGE="nvcr.io/nvidia/l4t-base:r32.5.0"
 		elif [ $L4T_REVISION_MINOR -gt 1 ]; then
 			BASE_IMAGE=$BASE_DEVEL
 		fi
