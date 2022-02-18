020111a2544e42a534b758f2a1c9f1fd5de3c31a
Dustin Franklin <dustinf@nvidia.com>
2022-02-18 10:32:05 -0500

    updated protobuf tests

--- test/test_protobuf.py
+++ test/test_protobuf.py
@@ -12,7 +12,7 @@ protobuf_implementation = str(api_implementation.Type())
 print(f'protobuf default API implementation: {str(api_implementation._default_implementation_type)}')
 print(f'protobuf active API implementation:  {protobuf_implementation}')
 
-#if protobuf_implementation != "cpp":
-#    raise ValueError(f'expected protobuf to have cpp implementation, but instead it has {protobuf_implementation} implementation')
+if protobuf_implementation != "cpp":
+    raise ValueError(f'expected protobuf to have cpp implementation, but instead it has {protobuf_implementation} implementation')
     
 print('protobuf OK\n')
