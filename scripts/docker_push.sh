bf5b4bf4a24f9c247598a54bf31d03756f5de4e6
Dustin Franklin <dustinf@nvidia.com>
2022-03-22 14:49:24 -0400

    updated push script

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -37,7 +37,7 @@ if [[ "$CONTAINERS" == "pytorch" || "$CONTAINERS" == "all" ]]; then
 	#push $NGC_GROUP "l4t-pytorch:r$L4T_VERSION-pth1.6-py3"
 	#push $NGC_GROUP "l4t-pytorch:r$L4T_VERSION-pth1.7-py3"
 	#push $NGC_GROUP "l4t-pytorch:r$L4T_VERSION-pth1.8-py3"
-	push $NGC_GROUP "l4t-pytorch:r$L4T_VERSION-pth1.9-py3"
+	#push $NGC_GROUP "l4t-pytorch:r$L4T_VERSION-pth1.9-py3"
 	push $NGC_GROUP "l4t-pytorch:r$L4T_VERSION-pth1.10-py3"
 fi
 
