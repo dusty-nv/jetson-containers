d1320a167fc18aaa3e7f07d4e63b0d6f7bf9d100
Dustin Franklin <dustinf@nvidia.com>
2022-03-29 14:59:50 -0400

    updated PyTorch container

--- scripts/docker_build_ml.sh
+++ scripts/docker_build_ml.sh
@@ -18,20 +18,20 @@ build_pytorch()
 	local pytorch_tag=$3
 	
 	local vision_version=$4
-	local pillow_version=$5
-	local audio_version=$6
-
-	echo "building PyTorch $pytorch_whl, torchvision $vision_version ($pillow_version), torchaudio $audio_version"
+	local audio_version=$5
+	local cuda_arch_list=${6:-"5.3;6.2;7.2;"}
+	
+	echo "building PyTorch $pytorch_whl, torchvision $vision_version, torchaudio $audio_version, cuda arch $cuda_arch_list"
 
 	sh ./scripts/docker_build.sh $pytorch_tag Dockerfile.pytorch \
 			--build-arg BASE_IMAGE=$BASE_IMAGE \
 			--build-arg PYTORCH_URL=$pytorch_url \
 			--build-arg PYTORCH_WHL=$pytorch_whl \
 			--build-arg TORCHVISION_VERSION=$vision_version \
-			--build-arg PILLOW_VERSION=$pillow_version \
-			--build-arg TORCHAUDIO_VERSION=$audio_version
+			--build-arg TORCHAUDIO_VERSION=$audio_version \
+			--build-arg TORCH_CUDA_ARCH_LIST=$cuda_arch_list
 
-	echo "done building PyTorch $pytorch_whl, torchvision $vision_version ($pillow_version), torchaudio $audio_version"
+	echo "done building PyTorch $pytorch_whl, torchvision $vision_version, torchaudio $audio_version, cuda arch $cuda_arch_list"
 }
 
 if [[ "$CONTAINERS" == "pytorch" || "$CONTAINERS" == "all" ]]; then
@@ -42,36 +42,31 @@ if [[ "$CONTAINERS" == "pytorch" || "$CONTAINERS" == "all" ]]; then
 		#build_pytorch "https://nvidia.box.com/shared/static/lufbgr3xu2uha40cs9ryq1zn4kxsnogl.whl" \
 		#			  "torch-1.2.0-cp36-cp36m-linux_aarch64.whl" \
 		#			  "l4t-pytorch:r$L4T_VERSION-pth1.2-py3" \
-		#			  "v0.4.0" \
-		#			  "pillow<7"
+		#			  "v0.4.0"
 
 		# PyTorch v1.3.0
 		#build_pytorch "https://nvidia.box.com/shared/static/017sci9z4a0xhtwrb4ps52frdfti9iw0.whl" \
 		#			  "torch-1.3.0-cp36-cp36m-linux_aarch64.whl" \
 		#			  "l4t-pytorch:r$L4T_VERSION-pth1.3-py3" \
-		#			  "v0.4.2" \
-		#			  "pillow<7"  
+		#			  "v0.4.2"
 
 		# PyTorch v1.4.0
 		#build_pytorch "https://nvidia.box.com/shared/static/c3d7vm4gcs9m728j6o5vjay2jdedqb55.whl" \
 		#			  "torch-1.4.0-cp36-cp36m-linux_aarch64.whl" \
 		#			  "l4t-pytorch:r$L4T_VERSION-pth1.4-py3" \
-		#			  "v0.5.0" \
-		#			  "pillow" 
+		#			  "v0.5.0"
 
 		# PyTorch v1.5.0
 		#build_pytorch "https://nvidia.box.com/shared/static/3ibazbiwtkl181n95n9em3wtrca7tdzp.whl" \
 		#			  "torch-1.5.0-cp36-cp36m-linux_aarch64.whl" \
 		#			  "l4t-pytorch:r$L4T_VERSION-pth1.5-py3" \
-		#			  "v0.6.0" \
-		#			  "pillow" 
+		#			  "v0.6.0"
 
 		# PyTorch v1.6.0
 		#build_pytorch "https://nvidia.box.com/shared/static/9eptse6jyly1ggt9axbja2yrmj6pbarc.whl" \
 		#			  "torch-1.6.0-cp36-cp36m-linux_aarch64.whl" \
 		#			  "l4t-pytorch:r$L4T_VERSION-pth1.6-py3" \
 		#			  "v0.7.0" \
-		#			  "pillow" \
 		#			  "v0.6.0"
 					  
 		# PyTorch v1.7.0
@@ -79,7 +74,6 @@ if [[ "$CONTAINERS" == "pytorch" || "$CONTAINERS" == "all" ]]; then
 		#			  "torch-1.7.0-cp36-cp36m-linux_aarch64.whl" \
 		#			  "l4t-pytorch:r$L4T_VERSION-pth1.7-py3" \
 		#			  "v0.8.1" \
-		#			  "pillow" \
 		#			  "v0.7.0"
 		
 		# PyTorch v1.8.0
@@ -87,7 +81,6 @@ if [[ "$CONTAINERS" == "pytorch" || "$CONTAINERS" == "all" ]]; then
 		#			"torch-1.8.0-cp36-cp36m-linux_aarch64.whl" \
 		#			"l4t-pytorch:r$L4T_VERSION-pth1.8-py3" \
 		#			"v0.9.0" \
-		#			"pillow" \
 		#			"v0.8.0"
 			
 		# PyTorch v1.9.0
@@ -95,7 +88,6 @@ if [[ "$CONTAINERS" == "pytorch" || "$CONTAINERS" == "all" ]]; then
 					"torch-1.9.0-cp36-cp36m-linux_aarch64.whl" \
 					"l4t-pytorch:r$L4T_VERSION-pth1.9-py3" \
 					"v0.10.0" \
-					"pillow" \
 					"v0.9.0"
 					
 		# PyTorch v1.10.0
@@ -103,7 +95,6 @@ if [[ "$CONTAINERS" == "pytorch" || "$CONTAINERS" == "all" ]]; then
 					"torch-1.10.0-cp36-cp36m-linux_aarch64.whl" \
 					"l4t-pytorch:r$L4T_VERSION-pth1.10-py3" \
 					"v0.11.1" \
-					"pillow" \
 					"v0.10.0"
 					
 		# PyTorch v1.11.0
@@ -111,26 +102,27 @@ if [[ "$CONTAINERS" == "pytorch" || "$CONTAINERS" == "all" ]]; then
 					"torch-1.11.0a0+17540c5-cp36-cp36m-linux_aarch64.whl" \
 					"l4t-pytorch:r$L4T_VERSION-pth1.11-py3" \
 					"v0.11.3" \
-					"pillow" \
 					"v0.10.2"
 					
 	elif [[ $L4T_RELEASE -eq 34 ]]; then   # JetPack 5.x
 	
+		jp5_cuda_arch="7.2;8.7"
+		
 		# PyTorch v1.10.0
 		build_pytorch "https://nvidia.box.com/shared/static/19je2l0ppy1fpq4mw1a5gsbb5y9fopy7.whl" \
 					"torch-1.10.0-cp38-cp38-linux_aarch64.whl" \
 					"l4t-pytorch:r$L4T_VERSION-pth1.10-py3" \
 					"v0.11.1" \
-					"pillow" \
-					"v0.10.0"
+					"v0.10.0" \
+					$jp5_cuda_arch
 				
 		# PyTorch v1.12.0
 		build_pytorch "https://developer.download.nvidia.com/compute/redist/jp/v50/pytorch/torch-1.12.0a0+2c916ef.nv22.3-cp38-cp38-linux_aarch64.whl" \
 					"torch-1.12.0a0+2c916ef.nv22.3-cp38-cp38-linux_aarch64.whl" \
 					"l4t-pytorch:r$L4T_VERSION-pth1.12-py3" \
 					"v0.12.0" \
-					"pillow" \
-					"v0.11.0"
+					"v0.11.0" \
+					$jp5_cuda_arch
 	else
 		echo "warning -- unsupported L4T R$L4T_VERSION, skipping PyTorch..."
 	fi
