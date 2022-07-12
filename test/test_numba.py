ff8394a9b707feb1bde82b675d44cad9b1b336a0
Dustin Franklin <dustinf@nvidia.com>
2022-07-12 16:56:30 -0400

    added guvectorized ufunc test

--- test/test_numba.py
+++ test/test_numba.py
@@ -2,11 +2,13 @@ print('testing numba...')
 
 import math
 import numba
-from numba import vectorize, cuda
+from numba import vectorize, guvectorize, cuda
 import numpy as np
 
 print('numba version: ' + str(numba.__version__))
-print('testing cuda ufunc...')
+
+# test scalar vectorization
+print('testing cuda vectorized ufunc...')
 
 @vectorize(['float32(float32, float32, float32)',
             'float64(float64, float64, float64)'],
@@ -24,7 +26,25 @@ C = np.array(np.random.sample(N), dtype=dtype)
 
 D = cu_discriminant(A, B, C)
 
-print('cuda ufunc result:')
+print('cuda vectorized ufunc result:')
 print(D)  # print result
+
+# test array vectorization
+print('testing cuda guvectorized ufunc...')
+
+@guvectorize(['uint8[:], uint8[:], uint8[:]',
+              'float32[:], float32[:], float32[:]'], 
+              '(n),(n)->(n)',
+             target='cuda')
+def cu_add_arrays(x, y, res):
+    for i in range(x.shape[0]):  # number of channels (3)
+        res[i] = x[i] + y[i]
+    
+A = np.full((2,4,3), 1, dtype)
+B = np.full(A.shape, 2, dtype)
+C = cu_add_arrays(A, B)
+
+print('cuda guvectorized ufunc result:')
+print(C)  # results should be '3'
 
 print('numba OK\n')
