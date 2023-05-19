f06fbbb3dd4f02f69a4cee5d1ab331f25d95fc8b
Dustin Franklin <dustinf@nvidia.com>
2023-05-19 13:50:37 -0400

    update transformers tests

--- test/test_transformers.py
+++ test/test_transformers.py
@@ -11,15 +11,15 @@ from optimum.onnxruntime import ORTModelForCausalLM
 from optimum.exporters.tasks import TasksManager
 
 
-# distilgpt2, optimum/gpt2
-def test_gpt(provider, model='optimum/gpt2', runs=100, warmup=10, verbose=False):
+# distilgpt2, optimum/gpt2, MBZUAI/LaMini-GPT-124M
+def test_gpt(provider, model='optimum/gpt2', runs=100, warmup=10, verbose=False, do_sample=False):
 
     print(f"loading {model} with '{provider}'")
     
     provider_options = {}
     
     if provider == 'TensorrtExecutionProvider':
-        trt_cache_path = 'test/data/transformers/trt_cache'
+        trt_cache_path = os.path.join(os.path.dirname(__file__), 'data/transformers/trt_cache')
         os.makedirs(trt_cache_path, exist_ok=True)
         
         provider_options = {
@@ -52,27 +52,47 @@ def test_gpt(provider, model='optimum/gpt2', runs=100, warmup=10, verbose=False)
     tokenizer = AutoTokenizer.from_pretrained(model)
 
     # encode example input
-    text = "what's your name?"
+    text = "Hello, I’m a language model"  #"what's your name?" "My name is Arthur and I live in"
     tokens = tokenizer(text, return_tensors="pt")
     
     if provider in ['CUDAExecutionProvider', 'TensorrtExecutionProvider']:
         tokens = tokens.to("cuda")
     
     # run inference
-    time_begin = time.perf_counter()
-    response = tokenizer.decode(ort_model.generate(**tokens)[0])
-    time_elapsed = (time.perf_counter() - time_begin) * 1000
+    time_avg = 0.0
+    last_response = ""
     
-    print(response)
-    return time_elapsed
+    for run in range(runs+warmup):
+        time_begin = time.perf_counter()
+        response = tokenizer.decode(ort_model.generate(**tokens, do_sample=do_sample)[0])
+        time_elapsed = (time.perf_counter() - time_begin) * 1000
+    
+        print_time = verbose or (run % 10 == 0)
+        
+        if run >= warmup:
+            time_avg += time_elapsed
+        
+        if response != last_response:
+            print(f"\nResponse: {response}\n")
+            last_response = response
+            print_time = True
+            
+        if print_time:
+            print(f"{provider} {'warmup' if run < warmup else 'run'} {run} -- {time_elapsed:.2f} ms")
+            
+    time_avg /= runs
+    print(f"\nResponse: {response}\n")
+    print(f"done running {model} with '{provider}' (avg={time_avg:.2f} ms, runs={runs}, do_sample={do_sample})")
+    return time_avg
     
 
 if __name__ == '__main__':  
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--model', type=str, default='optimum/gpt2')
-    parser.add_argument('--runs', type=int, default=100)
+    parser.add_argument('--model', type=str, default='MBZUAI/LaMini-GPT-124M')
+    parser.add_argument('--runs', type=int, default=25)
     parser.add_argument('--warmup', type=int, default=10)
+    parser.add_argument('--do-sample', action='store_true')
     parser.add_argument('--verbose', action='store_true')
     
     args = parser.parse_args()
@@ -80,10 +100,10 @@ if __name__ == '__main__':
     # run model inference tests
     perf = {}
     
-    for provider in ['CUDAExecutionProvider', 'CPUExecutionProvider']:  #'TensorrtExecutionProvider', 
+    for provider in ['TensorrtExecutionProvider', 'CUDAExecutionProvider', 'CPUExecutionProvider']: 
         perf[provider] = test_gpt(provider, **vars(args))
     
-    print(f"\nPerformance Summary for {args.model} (over {args.runs} runs)")
+    print(f"\nPerformance Summary for {args.model} (runs={args.runs}, do_sample={args.do_sample})")
     
     for key, value in perf.items():
         print(f"    {key} -- {value:.2f} ms")
