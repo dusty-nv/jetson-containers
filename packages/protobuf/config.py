e62abb5094a151c63988d3346025f5845de19fe3
Dustin Franklin <dustinf@nvidia.com>
2023-07-07 10:32:23 -0400

    added protobuf package

---
+++ packages/protobuf/config.py
@@ -0,0 +1,15 @@
+
+from jetson_containers import L4T_VERSION
+
+if L4T_VERSION.major >= 35:   # JetPack 5.0.2 / 5.1.x
+    PROTOBUF_VERSION = '3.20.3'
+elif L4T_VERSION.major == 34:  # JetPack 5.0 / 5.0.1
+    PROTOBUF_VERSION = '3.20.1'
+elif L4T_VERSION.major == 32:  # JetPack 4
+    PROTOBUF_VERSION = '3.19.4'
+
+package['build_args'] = {
+    'PROTOBUF_VERSION': PROTOBUF_VERSION,
+}
+
+package['alias'] = 'protobuf_cpp'
