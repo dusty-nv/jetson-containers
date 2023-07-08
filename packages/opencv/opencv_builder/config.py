a2769084611306fc9645611e0108d4708d3e81cd
Dustin Franklin <dustinf@nvidia.com>
2023-07-07 23:40:58 -0400

    added opencv_builder package

---
+++ packages/opencv/opencv_builder/config.py
@@ -0,0 +1,17 @@
+
+from jetson_containers import L4T_VERSION, ARCH, CUDA_ARCH_LIST
+
+OPENCV_VERSION = "4.5.0"
+
+if ARCH == 'aarch64':
+    CUDA_ARCH_BIN = ','.join(CUDA_ARCH_LIST)
+    ENABLE_NEON = "ON"
+elif ARCH == 'x86_64':
+    CUDA_ARCH_BIN = ''
+    ENABLE_NEON = "OFF"
+    
+package['build_args'] = {
+    'OPENCV_VERSION': '4.5.0',
+    'CUDA_ARCH_BIN': CUDA_ARCH_BIN,
+    'ENABLE_NEON': ENABLE_NEON
+}
