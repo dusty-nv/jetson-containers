b4ae5d78a66325e79334c5abe7231d1a4864d958
Dustin Franklin <dustinf@nvidia.com>
2023-07-09 12:25:42 -0400

    added TensorFlow test

--- packages/tensorflow/test.py
+++ packages/tensorflow/test.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python3
 print('testing TensorFlow...')
 import tensorflow as tf
 
@@ -19,12 +19,15 @@ if str(tf.__version__).find("1.") == 0:
 
     # https://github.com/tensorflow/models/blob/r1.13.0/research/object_detection/object_detection_tutorial.ipynb
     # https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf1_detection_zoo.md
+    MODEL_DIR = '/tmp'
     MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
     MODEL_FILE = MODEL_NAME + '.tar.gz'
+    MODEL_PATH = os.path.join(MODEL_DIR, MODEL_FILE)
+    
     DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
     # Path to frozen detection graph. This is the actual model that is used for the object detection.
-    PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+    PATH_TO_FROZEN_GRAPH = os.path.join(MODEL_DIR, MODEL_NAME, 'frozen_inference_graph.pb')
 
     # List of the strings that is used to add correct label for each box.
     PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
@@ -33,13 +36,13 @@ if str(tf.__version__).find("1.") == 0:
     print(f'downloading {DOWNLOAD_BASE + MODEL_FILE}')
     
     opener = urllib.request.URLopener()
-    opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-    tar_file = tarfile.open(MODEL_FILE)
+    opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_PATH)
+    tar_file = tarfile.open(MODEL_PATH)
     
     for file in tar_file.getmembers():
         file_name = os.path.basename(file.name)
         if 'frozen_inference_graph.pb' in file_name:
-            tar_file.extract(file, os.getcwd())
+            tar_file.extract(file, MODEL_DIR)  # os.getcwd()
    
     print(f'loading {PATH_TO_FROZEN_GRAPH}')
     
