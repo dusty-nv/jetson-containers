46c4d310049160f45188d4a1c43798535de8e5f3
Dustin Franklin <dustinf@nvidia.com>
2023-07-09 02:02:42 -0400

    fixed zero-length package search dirs

--- jetson_containers/packages.py
+++ jetson_containers/packages.py
@@ -33,7 +33,9 @@ def package_search_dirs(package_dirs, scan=False):
     if isinstance(package_dirs, str) and len(package_dirs) > 0:
         package_dirs = [package_dirs]
         
-    _PACKAGE_DIRS.extend(package_dirs)
+    for package_dir in package_dirs:
+        if len(package_dir) > 0:
+            _PACKAGE_DIRS.append(package_dirs)
     
     if scan:
         scan_packages(_PACKAGE_DIRS, rescan=True)
