e5446ed9c950e4a738a84e0d0703bdd74fdabc99
Dustin Franklin <dustinf@nvidia.com>
2023-07-09 23:09:34 -0400

    added Large Language Model (LLM) packages

---
+++ packages/llm/auto-gptq/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+print('testing AutoGPTQ...')
+
+import auto_gptq
+
+print(auto_gptq.__version__)
+
+print('AutoGPTQ OK\n')
---
+++ packages/llm/bitsandbytes/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+print('testing bitsandbytes...')
+
+import bitsandbytes
+
+print(bitsandbytes.__version__)
+
+print('bitsandbytes OK\n')
