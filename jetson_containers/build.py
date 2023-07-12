89df28fb67d28d045c1dc109aa67a0c9058c57d3
Dustin Franklin <dustinf@nvidia.com>
2023-07-11 22:40:23 -0400

    made --multi-stage the default

--- jetson_containers/build.py
+++ jetson_containers/build.py
@@ -11,14 +11,11 @@
 #
 # Some example build scenarios:
 #
-#   $ jetson-containers/build.sh pytorch tensorflow                # build separate pytorch & tensorflow containers
-#   $ jetson-containers/build.sh --multi-stage pytorch tensorflow  # build one container with both pytorch & tensorflow packages
-#   $ jetson-containers/build.sh ros:humble*                       # build all ROS Humble containers (can use wildcard filters)
-#   $ jetson-containers/build.sh --multi-stage ros:humble-desktop pytorch  # build ROS Humble with PyTorch on top
-#
-# All of the above commands will build one or more multi-stage containers, chaining together intermediate containers until all of the
-# dependency requirements are satisfied.  For example, the "pytorch" package expands to include the "python" and "numpy" dependencies.
-# The --multi-stage flag is used to specify the packages in a singular container from the command-line without any additional config files.
+#   $ jetson-containers/build.sh --name=xyz pytorch jupyterlab     # build container with PyTorch and JupyterLab server
+#   $ jetson-containers/build.sh --multiple pytorch tensorflow     # build separate containers for PyTorch and 
+#   $ jetson-containers/build.sh --multiple ros:humble*            # build all ROS Humble containers (can use wildcards)
+#   $ jetson-containers/build.sh ros:humble-desktop pytorch        # build ROS Humble with PyTorch on top 
+#   $ jetson-containers/build.sh --base=xyz:latest pytorch         # add PyTorch to an existing container
 #
 # Typically the jetson-containers/build.sh wrapper script is used to launch this underlying Python module. jetson-containers can also
 # build external out-of-tree projects that have their own Dockerfile.  And you can add your own package search dirs for other packages.
@@ -38,22 +35,22 @@ parser.add_argument('packages', type=str, nargs='*', default=[], help='packages 
 
 parser.add_argument('--name', type=str, default='', help="the name of the output container to build")
 parser.add_argument('--base', type=str, default='', help="the base container to use at the beginning of the build chain (default: l4t-jetpack)")
-parser.add_argument('--multi-stage', action='store_true', help="launch a multi-stage container build by chaining together the packages")
-parser.add_argument('--build-flags', type=str, default='', help="extra flags to pass to 'docker build'")
+parser.add_argument('--multiple', action='store_true', help="the specified packages should be built independently as opposed to chained together")
+parser.add_argument('--build-flags', type=str, default='', help="extra flags to pass to 'docker build' commands")
 parser.add_argument('--package-dirs', type=str, default='', help="additional package search directories (comma or colon-separated)")
 parser.add_argument('--list-packages', action='store_true', help="show the list of packages that were found under the search directories")
 parser.add_argument('--show-packages', action='store_true', help="show info about one or more packages (if none are specified, all will be listed")
 parser.add_argument('--skip-packages', type=str, default='', help="disable certain packages/containers (filterable by wildcards, comma/colon-separated)")
 parser.add_argument('--skip-tests', type=str, default='', help="comma-separated list of package tests to disable ('intermediate' to disable build-stage tests)")
-parser.add_argument('--skip-errors', action='store_true', help="continue building when errors occur (not used with --multi-stage)")
+parser.add_argument('--skip-errors', action='store_true', help="continue building when errors occur (only relevant when --multiple is in use)")
 parser.add_argument('--simulate', action='store_true', help="print out the build commands without actually building the containers")
 parser.add_argument('--logs', type=str, default='', help="sets the directory to save container build logs to (default: jetson-containers/logs)")
 
 args = parser.parse_args()
 
 # validate args
-if args.multi_stage and args.skip_errors:
-    raise ValueError("--skip-errors can't be used with --multi-stage")
+if args.skip_errors and not args.multiple:
+    raise ValueError("--skip-errors can only be used with --multiple flag")
     
 # split multi-value keyword arguments
 args.package_dirs = re.split(',|;|:', args.package_dirs)
@@ -88,7 +85,7 @@ if args.list_packages or args.show_packages:
     
 # build one multi-stage container from chain of packages
 # or launch multiple independent container builds
-if args.multi_stage:
+if not args.multiple:
     build_container(args.name, args.packages, args.base, args.build_flags, args.simulate, args.skip_tests)
 else:   
     build_containers(args.name, args.packages, args.base, args.build_flags, args.simulate, args.skip_errors, args.skip_packages, args.skip_tests)
