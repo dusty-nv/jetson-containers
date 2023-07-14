ea87caba7b17cd7bfd5766bef6cd8889e369d2d7
Dustin Franklin <dustinf@nvidia.com>
2023-07-13 23:48:10 -0400

    added tritonserver

---
+++ packages/tritonserver/config.py
@@ -0,0 +1,39 @@
+import os
+
+from jetson_containers import L4T_VERSION
+from packaging.version import Version
+
+if L4T_VERSION >= Version('35.3.1'): # JetPack 5.1.1
+    # https://github.com/triton-inference-server/server/releases/tag/v2.35.0
+    TRITON_URL = 'https://github.com/triton-inference-server/server/releases/download/v2.35.0/tritonserver2.35.0-jetpack5.1.2.tgz'
+    TRITON_TAR = 'tritonserver2.35.0-jetpack5.1.2.tgz'
+elif L4T_VERSION >= Version('35.2.1'): # JetPack 5.1
+    # https://github.com/triton-inference-server/server/releases/tag/v2.34.0
+    TRITON_URL = 'https://github.com/triton-inference-server/server/releases/download/v2.34.0/tritonserver2.34.0-jetpack5.1.tgz'
+    TRITON_TAR = 'tritonserver2.34.0-jetpack5.1.tgz'
+elif L4T_VERSION >= Version('35'): # JetPack 5.0.2 / L4T R35.1
+    # https://github.com/triton-inference-server/server/releases/tag/v2.27.0
+    TRITON_URL = 'https://github.com/triton-inference-server/server/releases/download/v2.27.0/tritonserver2.27.0-jetpack5.0.2.tgz'
+    TRITON_TAR = 'tritonserver2.27.0-jetpack5.0.2.tgz'
+elif L4T_VERSION >= Version('34'): # JetPack 5.0/5.0.1 DP
+    # https://github.com/triton-inference-server/server/releases/tag/v2.23.0
+    TRITON_URL = 'https://github.com/triton-inference-server/server/releases/download/v2.23.0/tritonserver2.23.0-jetpack5.0.tgz'
+    TRITON_TAR = 'tritonserver2.23.0-jetpack5.0.tgz'
+elif L4T_VERSION >= Version('32.7'): # JetPack 4.6.1+
+    # https://github.com/triton-inference-server/server/releases/tag/v2.19.0
+    TRITON_URL = 'https://github.com/triton-inference-server/server/releases/download/v2.19.0/tritonserver2.19.0-jetpack4.6.1.tgz'
+    TRITON_TAR = 'tritonserver2.19.0-jetpack4.6.1.tgz'
+elif L4T_VERSION >= Version('32.6'): # JetPack 4.6
+    # https://github.com/triton-inference-server/server/releases/tag/v2.17.0
+    TRITON_URL = 'https://github.com/triton-inference-server/server/releases/download/v2.17.0/tritonserver2.17.0-jetpack4.6.tgz'
+    TRITON_TAR = 'tritonserver2.17.0-jetpack4.6.tgz'
+else:
+    print('-- tritonserver not available before JetPack 4.6 / L4T R32.6')
+    package = None
+    
+if package:
+    package['build_args'] = {
+        'TRITON_URL': TRITON_URL,
+        'TRITON_TAR': TRITON_TAR,
+        'TRITON_VERSION': os.path.basename(os.path.dirname(TRITON_URL)).lstrip('v')
+    }
