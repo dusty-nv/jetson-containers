2459f33cb16bea7eec318023a8ca7f74073fd82a
Dustin Franklin <dustinf@nvidia.com>
2023-07-16 15:47:14 -0400

    disable bitsandbytes on JetPack 4

--- packages/llm/bitsandbytes/config.py
+++ packages/llm/bitsandbytes/config.py
@@ -1,6 +1,6 @@
 
 from jetson_containers import L4T_VERSION
 
-if L4T_VERSION.major < 34:   # JetPack 5
-    print("-- bitsandbytes isn't available for JetPack 4, disabling...")
+if L4T_VERSION.major <= 32:
+    print('-- Disabling bitsandbytes package on JetPack 4')
     package = None
