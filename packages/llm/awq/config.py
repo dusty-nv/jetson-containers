ff413f513f9bec43da890615052e19b537477f4f
Dustin Franklin <dustinf@nvidia.com>
2023-07-16 15:45:05 -0400

    disable AWQ package on JetPack 4

--- packages/llm/awq/config.py
+++ packages/llm/awq/config.py
@@ -2,5 +2,5 @@
 from jetson_containers import L4T_VERSION
 
 if L4T_VERSION.major <= 32:
-    print('-- disabling AWQ package on JetPack 4')
+    print('-- Disabling AWQ package on JetPack 4')
     package = None
