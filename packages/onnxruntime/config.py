28a2bfd2f5eaca20489cf173b5fa63bba458e30a
Dustin Franklin <dustinf@nvidia.com>
2023-07-18 10:44:47 -0400

    fixed onnxruntime build on JetPack 4

--- packages/onnxruntime/config.py
+++ packages/onnxruntime/config.py
@@ -1,14 +1,13 @@
 
 from jetson_containers import L4T_VERSION, CUDA_ARCH_LIST_INT
 
-ONNXRUNTIME_VERSION = 'main'
-CUDA_ARCHITECTURES = ';'.join([str(c) for c in CUDA_ARCH_LIST_INT])
+package['build_args'] = {
+    'CUDA_ARCHITECTURES': ';'.join([str(c) for c in CUDA_ARCH_LIST_INT]),
+}
 
 # onnxruntime >= 1.16 drops support for gcc7/Python 3.6 (JetPack 4)
+# onnxruntime >= 1.14 too few arguments to function cudaStreamWaitEvent
+# onnxruntime <= 1.13 doesn't need/support --allow_running_as_root
 if L4T_VERSION.major <= 32:
-    ONNXRUNTIME_VERSION = 'v1.15.1'
-    
-package['build_args'] = {
-    'ONNXRUNTIME_VERSION': ONNXRUNTIME_VERSION,
-    'CUDA_ARCHITECTURES': CUDA_ARCHITECTURES,
-}
+    package['build_args']['ONNXRUNTIME_VERSION'] = 'v1.13.1'
+    package['build_args']['ONNXRUNTIME_FLAGS'] = ''
