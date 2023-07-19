9cb2afea4ab30be35d416cdfef1bd462a4f83192
Dustin Franklin <dustinf@nvidia.com>
2023-07-19 11:08:00 -0400

    updated protobuf tests for protobuf>4

--- packages/protobuf/protobuf_apt/test.py
+++ packages/protobuf/protobuf_apt/test.py
@@ -4,12 +4,15 @@ print('testing protobuf (apt)...')
 import google.protobuf
 print('protobuf version: ' + str(google.protobuf.__version__))
 
-# verify implementation is cpp
+# verify implementation type
 from google.protobuf.internal import api_implementation
+from packaging.version import Version
 
 protobuf_implementation = str(api_implementation.Type())
 
-print(f'protobuf default API implementation: {str(api_implementation._default_implementation_type)}')
+if Version(google.protobuf.__version__).major < 4:
+    print(f'protobuf default API implementation: {str(api_implementation._default_implementation_type)}')
+    
 print(f'protobuf active API implementation:  {protobuf_implementation}')
 
 print('protobuf (apt) OK\n')
