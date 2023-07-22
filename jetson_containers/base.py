ca58da028c3fc2d0c0414ad9412b45fbd3b21f1d
Dustin Franklin <dustinf@nvidia.com>
2023-07-22 19:04:13 -0400

    adjusted L4T base images

--- jetson_containers/base.py
+++ jetson_containers/base.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 from .l4t_version import L4T_VERSION
+from packaging.version import Version
 
 def get_l4t_base():
     """
     Returns the l4t-base or l4t-jetpack container to use
     """
     if L4T_VERSION.major >= 34:   # JetPack 5
-        return f"nvcr.io/nvidia/l4t-jetpack:r{L4T_VERSION}"
+        if L4T_VERSION >= Version('35.3.1'):
+            return "nvcr.io/nvidia/l4t-jetpack:r35.3.1"
+        else:
+            return f"nvcr.io/nvidia/l4t-jetpack:r{L4T_VERSION}"
     else:
-        return f"nvcr.io/nvidia/l4t-base:r{L4T_VERSION}"
+        if L4T_VERSION >= Version('32.7.1'):
+            return "nvcr.io/nvidia/l4t-jetpack:r32.7.1"
+        else:
+            return f"nvcr.io/nvidia/l4t-base:r{L4T_VERSION}"
