c60a87b47224153072107d346954ac2032425c7c
Dustin Franklin <dustinf@nvidia.com>
2023-07-21 22:13:14 -0400

    added GitHub CLI

--- jetson_containers/ci.py
+++ jetson_containers/ci.py
@@ -29,11 +29,12 @@ def generate_workflow(package, root, l4t_version, simulate=False):
     filename = os.path.join(root, '.github/workflows', f"{name.replace(':','_')}-r{l4t_version}.yml")
     
     txt = f"name: \"{workflow_name}\"\n"
+    txt = f"run-name: \"Build {workflow_name}\"\n"
     txt += "on:\n"
     txt += "  workflow_dispatch: {}\n"
     txt += "  push:\n"
     txt += "    branches:\n"
-    txt += "      - 'dev'\n"
+    txt += "      - 'placeholder'\n"
     txt += "jobs:\n"
     txt += f"  {workflow_name.replace(':','_')}:\n"
     txt += f"    runs-on: self-hosted-jetson r{l4t_version}\n"
@@ -43,6 +44,10 @@ def generate_workflow(package, root, l4t_version, simulate=False):
     print(filename)
     print(txt)
 
+    if not simulate:
+        with open(filename, 'w') as file:
+            file.write(txt)
+            
 
 def register_runner(token, root, repo, labels=[], prefix='runner', simulate=False):
     """
@@ -60,6 +65,7 @@ def register_runner(token, root, repo, labels=[], prefix='runner', simulate=Fals
     
     labels = [x for x in labels if x]
     
+    # github runner package
     run_dir = os.path.join(root, prefix)
     run_tar = os.path.join(run_dir, "actions-runner-linux-arm64-2.304.0.tar.gz")
     run_url = "https://github.com/actions/runner/releases/download/v2.304.0/actions-runner-linux-arm64-2.304.0.tar.gz"
@@ -70,6 +76,17 @@ def register_runner(token, root, repo, labels=[], prefix='runner', simulate=Fals
         wget.download(run_url, run_tar)
         shutil.unpack_archive(run_tar, run_dir)
         
+    # github cli package
+    cli_deb = os.path.join(run_dir, "gh_2.32.0_linux_arm64.deb")
+    cli_url = "https://github.com/cli/cli/releases/download/v2.32.0/gh_2.32.0_linux_arm64.deb"
+    
+    if not os.path.isfile(cli_deb) and not simulate:
+        print(f"-- Downloading GitHub CLI package to {cli_deb}")
+        wget.download(cli_url, cli_deb)
+        cmd = f"sudo dpkg --install {cli_deb}"
+        subprocess.run(cmd, executable='/bin/bash', shell=True, check=True) 
+        
+    # run config command
     cmd = f"cd {run_dir} && "
     cmd += f"./config.sh --url {repo} --token {token} --labels {','.join(labels)} --unattended && sudo ./svc.sh install && sudo ./svc.sh status "
 
