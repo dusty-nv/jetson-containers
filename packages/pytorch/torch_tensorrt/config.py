3365dc9f4db0952cae91f690d7ef11cb80186ee4
Dustin Franklin <dustinf@nvidia.com>
2023-07-31 01:14:53 -0400

    fixed torch_tensorrt on JetPack 4

--- packages/pytorch/torch_tensorrt/config.py
+++ packages/pytorch/torch_tensorrt/config.py
@@ -1,7 +1,14 @@
 
-from jetson_containers import PYTHON_VERSION
+from jetson_containers import PYTHON_VERSION, JETPACK_VERSION
 
+if JETPACK_VERSION.major >= 5:
+    TORCH_TRT_VERSION = 'v1.4.0'  # build setup has changed > 1.4.0 (still ironing it out on aarch64)
+else:
+    TORCH_TRT_VERSION = 'v1.0.0'  # compatability with TensorRT 8.2 and PyTorch 1.10
+    
 package['build_args'] = {
     'PYTHON_VERSION': PYTHON_VERSION,
-    'TORCH_TRT_VERSION': 'v1.4.0',  # build setup has changed > 1.4.0, still ironing it out on aarch64
+    'JETPACK_MAJOR': JETPACK_VERSION.major,
+    'JETPACK_MINOR': 0 if JETPACK_VERSION.major >= 5 else 6,   # only 5.0 and 4.6 are recognized
+    'TORCH_TRT_VERSION': TORCH_TRT_VERSION,  
 }
