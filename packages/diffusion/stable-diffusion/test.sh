0cbf35a5446155b2b58148414d9fa3994c1f991c
Dustin Franklin <dustinf@nvidia.com>
2023-08-04 10:43:55 -0400

    updated stable-diffusion tests for low-memory platforms

--- packages/diffusion/stable-diffusion/test.sh
+++ packages/diffusion/stable-diffusion/test.sh
@@ -14,14 +14,25 @@ fi
 # txt2img needs to be run from it's directory
 cd /opt/stable-diffusion
 
+mem_capacity=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+echo "memory capacity:  $mem_capacity KB"
+
 # generate one image, 25 steps (default is 6 512x512 images, 50 steps)
-/usr/bin/time -v python3 scripts/txt2img.py --plms \
-  --n_samples 1 --n_iter 1 --ddim_steps 25 \
-  --outdir /data/images/stable-diffusion \
-  --ckpt "$MODEL" \
-  --prompt "$PROMPT"
+# (this way consumes ~12GB memory and doesn't run on 8GB boards)
+if [ $mem_capacity -gt 8388608 ]; then
+	echo "running scripts/txt2img.py"
+	
+	/usr/bin/time -v python3 scripts/txt2img.py --plms \
+	  --n_samples 1 --n_iter 1 --ddim_steps 25 \
+	  --outdir /data/images/stable-diffusion \
+	  --ckpt "$MODEL" \
+	  --prompt "$PROMPT"
+fi
 
 # now try it with the memory optimizations
+# (this way consumes ~10GB memory and should run on 8GB boards)
+echo "optimizedSD/optimized_txt2img.py"
+
 /usr/bin/time -v python3 optimizedSD/optimized_txt2img.py \
   --sampler plms --seed 42 \
   --n_samples 1 --n_iter 1 --ddim_steps 25 \
