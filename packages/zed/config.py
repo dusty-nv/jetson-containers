2c7a7b811fa5792907c89455ff15d7f131b31e8d
Dustin Franklin <dustinf@nvidia.com>
2023-08-04 16:16:58 -0400

    add checks for latest ZED SDK available

--- packages/zed/config.py
+++ packages/zed/config.py
@@ -1,6 +1,14 @@
 
 from jetson_containers import L4T_VERSION
 
+from packaging.version import Version
+
+# https://www.stereolabs.com/developers/release/
+latest_zed_version = Version('35.3.1')
+
+if L4T_VERSION > latest_zed_version:
+    L4T_VERSION = latest_zed_version
+    
 package['build_args'] = {
     'L4T_MAJOR_VERSION': L4T_VERSION.major,
     'L4T_MINOR_VERSION': L4T_VERSION.minor,
