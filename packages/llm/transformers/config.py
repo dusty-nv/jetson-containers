712252b39835573a8a18bcafecc9d9bb64605c11
Dustin Franklin <dustinf@nvidia.com>
2023-09-06 12:48:04 -0400

    added option to install transformers from source

--- packages/llm/transformers/config.py
+++ packages/llm/transformers/config.py
@@ -2,4 +2,26 @@
 from jetson_containers import L4T_VERSION
 
 if L4T_VERSION.major >= 34:
-    package['depends'].append('bitsandbytes')
+    package['depends'].extend(['bitsandbytes', 'auto_gptq'])
+
+    # version installed from source
+    package_git = package.copy()
+    
+    package_git['name'] = 'transformers:git'
+    
+    package_git['build_args'] = {
+        'TRANSFORMERS_PACKAGE': "git+https://github.com/huggingface/transformers",
+        'TRANSFORMERS_VERSION': "https://api.github.com/repos/huggingface/transformers/git/refs/heads/main"
+    }
+    
+    # version with nvgpt support
+    package_nvgpt = package.copy()
+    
+    package_nvgpt['name'] = 'transformers:nvgpt'
+    
+    package_nvgpt['build_args'] = {
+        'TRANSFORMERS_PACKAGE': "git+https://github.com/ertkonuk/transformers",
+        'TRANSFORMERS_VERSION': "https://api.github.com/repos/ertkonuk/transformers/git/refs/heads/main"
+    }
+    
+    package = [package, package_git, package_nvgpt]
