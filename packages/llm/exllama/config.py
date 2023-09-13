b7f90e3cad116adcd5c4e1ba2cd9cc5a7ba2b3f4
Dustin Franklin <dustinf@nvidia.com>
2023-09-13 15:42:58 -0400

    added exllamav2

--- packages/llm/exllama/config.py
+++ packages/llm/exllama/config.py
@@ -4,3 +4,11 @@ from jetson_containers import CUDA_ARCHITECTURES
 package['build_args'] = {
     'TORCH_CUDA_ARCH_LIST': ';'.join([f'{x/10:.1f}' for x in CUDA_ARCHITECTURES])
 }
+
+exllama_v2 = package.copy()
+
+exllama_v2['name'] = 'exllama:v2'
+exllama_v2['dockerfile'] = 'Dockerfile.v2'
+exllama_v2['test'] = 'test_v2.sh'
+
+package = [package, exllama_v2]
---
+++ packages/llm/exllama/test_v2.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd /opt/exllamav2
+
+python3 test_inference.py --help
