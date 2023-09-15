08410f5bb15c3ec53255fc76fe69a70a8849cc4b
Dustin Franklin <dustinf@nvidia.com>
2023-09-14 23:36:37 -0400

    updated AWQ benchmark

--- packages/llm/awq/benchmark.py
+++ packages/llm/awq/benchmark.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 # benchmark a quantized AWQ model
 import os
+import sys
 import time
 import datetime
 import argparse
 import resource
 import socket
+import threading
 import torch
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
 from accelerate import init_empty_weights, load_checkpoint_and_dispatch
 
 from awq.quantize.quantizer import real_quantize_model_weight
@@ -35,10 +37,30 @@ parser.add_argument('--save', type=str, default='', help='CSV file to save bench
 parser.add_argument('--w_bit', type=int, default=4)
 parser.add_argument('--q_group_size', type=int, default=128)
 parser.add_argument('--no_zero_point', action='store_true', help="disable zero_point")
-parser.add_argument('--tiny_chat', action='store_true', help="use optimized TinyChat inference")
+parser.add_argument('--no_tinychat_kernels', action='store_true', help="disable tinychat kernels")
+parser.add_argument('--no_tinychat_infer', action='store_true', help="disable tinychat inference")
+parser.add_argument('--no_streaming', action='store_true', help="disable streaming mode")
+parser.add_argument('--no_quant', action='store_true', help="disable quantization and use FP16 through transformers")
+parser.add_argument('--do_sample', action='store_true')
 
 args = parser.parse_args()
 
+#args.prompt="Once upon a time, there was a young man named Jack who lived in a small village nestled in the rolling hills of the countryside. Jack was a curious and adventurous soul, always eager to explore the world beyond his village. One day, while wandering through the nearby forest, he stumbled upon a hidden path that he had never seen before. The path was overgrown with weeds and vines, and it looked as though it had been untouched for many years. Jack's curiosity was piqued, and he decided to follow the path to see where it led"
+#args.prompt="Once upon a time, there was a young man named Jack who lived in a small village nestled in the rolling hills of the countryside. Jack was a curious and adventurous soul, always eager to explore the world beyond his village. One day, while wandering through the nearby forest, he stumbled upon a hidden path that he had never seen before. The path was overgrown with weeds and vines, and it looked as though it had been untouched for many years. Jack's curiosity was piqued, and he decided to follow the path to see where it led. As he walked down the path, the trees grew taller and the air grew colder. Jack could feel a strange energy emanating from the forest, as if it were alive and watching him. He quickened his pace, eager to reach the end of the path and discover its secrets. After a while, the path opened up into a clearing, and Jack found himself standing in front of a massive stone structure. The building was unlike anything he had ever seen before, with intricate carvings and symbols etched into its walls. Jack felt a sense of awe and wonder as he approached the"
+
+print(args)
+
+def get_model_name_from_path(model_path):
+    model_path = model_path.strip("/")
+    model_paths = model_path.split("/")
+    
+    if len(model_paths) > 2:
+        return model_paths[-2] + '/' + model_paths[-1]
+    else:
+        return model_paths[-1]
+        
+model_name = get_model_name_from_path(args.quant)
+       
 # get quantization config (apart from w_bit)
 q_config = {
     "zero_point": not args.no_zero_point,  # by default True
@@ -51,26 +73,46 @@ print("Quantization config:", q_config)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 print(f"Running on device {device}")
-print(f"Loading model {args.model} with quantized weights from {args.quant}")
 
-# load huggingface model, without the weights (just need the model structure)
-with init_empty_weights():
-    model = AutoModelForCausalLM.from_pretrained(args.model, torch_dtype=torch.float16)
+
+if args.no_quant:
+    precision = "fp16"
+    print(f"Loading model {args.model} without quantization ({precision})")
+    model = AutoModelForCausalLM.from_pretrained(args.model, torch_dtype=torch.float16).to(device)
+else:
+    print(f"Loading model {args.model} with quantized weights from {args.quant}")
     
-# prepare model to apply quantized weights
-real_quantize_model_weight(model, w_bit=args.w_bit, q_config=q_config, init_only=True)
+    with init_empty_weights():
+        model = AutoModelForCausalLM.from_pretrained(args.model, torch_dtype=torch.float16)
 
-# load quantized weights
-model = load_checkpoint_and_dispatch(
-    model, args.quant, device_map='balanced', 
-    no_split_module_classes=["OPTDecoderLayer", "LlamaDecoderLayer"]
-)
+    print(model)
+    print(f"model device: {model.device}")
+         
+    # prepare model to apply quantized weights
+    real_quantize_model_weight(model, w_bit=args.w_bit, q_config=q_config, init_only=True)
 
-if args.tiny_chat:
-    make_quant_attn(model, device)
-    make_quant_norm(model)
-    make_fused_mlp(model)
+    # load quantized weights
+    model = load_checkpoint_and_dispatch(
+        model, args.quant, device_map='balanced', 
+        no_split_module_classes=["OPTDecoderLayer", "LlamaDecoderLayer"]
+    )
+    
+    precision = f"int{args.w_bit}"
 
+    print(model)
+    print(f"model device: {model.device}")
+     
+    if not args.no_tinychat_kernels:
+        make_quant_attn(model, device)
+        make_quant_norm(model)
+        make_fused_mlp(model)
+
+    print(model)
+    print(f"model device: {model.device}")
+
+#for name, param in model.named_parameters():
+#    print(f"{name} {param}")
+    
 model.eval()
                   
 # create tokenizer
@@ -78,27 +120,71 @@ tokenizer = AutoTokenizer.from_pretrained(args.model, use_fast=False)
 input_ids = tokenizer(args.prompt, return_tensors="pt").input_ids.to(device)
 
 # benchmark inference
-time_avg = 0
+avg_latency=0
+avg_tokens_sec=0
 
 for run in range(args.runs + args.warmup):
-    time_begin = time.perf_counter()
-    with torch.inference_mode():
-        generated_ids = model.generate(input_ids, do_sample=False, min_length=args.tokens, max_length=args.tokens)  # greedy generation of fixed # of tokens   #max_new_tokens=args.max_new_tokens
-    time_elapsed = (time.perf_counter() - time_begin)
-    
-    print(tokenizer.decode(generated_ids[0], skip_special_tokens=True))
-    
-    if run >= args.warmup:
-        time_avg += time_elapsed
+    if args.no_streaming:
+        time_begin = time.perf_counter()
+        generated_ids = model.generate(input_ids, do_sample=args.do_sample, min_new_tokens=args.tokens, max_new_tokens=args.tokens)
+        time_elapsed = time.perf_counter() - time_begin
         
-    print(f"\n{'WARMUP' if run < args.warmup else 'RUN'} {run} = {time_elapsed:.4f} seconds, {args.tokens/time_elapsed:.1f} tokens/sec (int{args.w_bit})")
+        print(tokenizer.decode(generated_ids[0], skip_special_tokens=False))
+        
+        num_tokens=len(generated_ids[0])
+        tokens_sec=num_tokens / time_elapsed
+        latency=time_elapsed
+    else:
+        streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=False)
+        
+        def generate():
+            with torch.inference_mode():
+                model.generate(
+                    inputs=input_ids,
+                    do_sample=args.do_sample,
+                    min_new_tokens=args.tokens, 
+                    max_new_tokens=args.tokens,
+                    streamer=streamer
+                )
+        
+        thread = threading.Thread(target=generate)
+        thread.start()
+        
+        print(f"Prompt:  {args.prompt}")
+        
+        new_tokens = ''
+        num_tokens = 0
+        time_begin = time.perf_counter()
+       
+        for token in streamer:
+            print(token, end='')
+            sys.stdout.flush()
+
+            if num_tokens == 0:
+                time_first_token=time.perf_counter()
+                latency=time_first_token - time_begin
+                time_begin=time_first_token
+                
+            new_tokens += token
+            num_tokens += 1
+    
+        time_elapsed=time.perf_counter() - time_begin
+        tokens_sec=(num_tokens-1) / time_elapsed
+        
+    print(f"\n{model_name}:  {num_tokens} tokens in {time_elapsed:.2f} sec, {tokens_sec:.2f} tokens/sec, latency {latency:.2f} sec  ({precision})\n")
+            
+    if run >= args.warmup:
+        avg_latency += latency
+        avg_tokens_sec += tokens_sec
   
 # compute statistics
-time_avg /= args.runs  
-tokens_sec = args.tokens / time_avg
+avg_latency /= args.runs
+avg_tokens_sec /= args.runs
+
 memory_usage = (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss + resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss) / 1024  # https://stackoverflow.com/a/7669482
 
-print(f"\nAVG = {time_avg:.4f} seconds, {tokens_sec:.1f} tokens/sec  memory={memory_usage:.2f} MB  (--model={args.model} --quant={args.quant} --w_bit={args.w_bit} --tokens={args.tokens})\n")
+print(f"AVERAGE of {args.runs} runs:")
+print(f"{model_name}:  {avg_tokens_sec:.2f} tokens/sec, latency {avg_latency:.2f} sec, memory={memory_usage:.2f} MB  ({precision})\n")
 
 if args.save:
     if not os.path.isfile(args.save):  # csv header
@@ -106,5 +192,5 @@ if args.save:
             file.write(f"timestamp, hostname, api, model, precision, tokens, tokens/sec, latency, memory\n")
     with open(args.save, 'a') as file:
         file.write(f"{datetime.datetime.now().strftime('%Y%m%d %H:%M:%S')}, {socket.gethostname()}, {'tinychat' if args.tiny_chat else 'awq'}, ")
-        file.write(f"{args.quant}, int{args.w_bit}, {args.tokens}, {tokens_sec}, {time_avg}, {memory_usage}\n")
+        file.write(f"{model_name}, {precision}, {args.tokens}, {avg_tokens_sec}, {avg_latency}, {memory_usage}\n")
             
