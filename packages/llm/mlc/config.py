c95ba18deca34a821e2c94db8c73e3f97e1b72e6
Dustin Franklin <dustinf@nvidia.com>
2023-09-21 14:45:32 -0400

    added dev package

--- packages/llm/mlc/config.py
+++ packages/llm/mlc/config.py
@@ -5,3 +5,10 @@ package['build_args'] = {
     'CUDAARCHS': ';'.join([str(x) for x in CUDA_ARCHITECTURES]),
     'TORCH_CUDA_ARCH_LIST': ';'.join([f'{x/10:.1f}' for x in CUDA_ARCHITECTURES])
 }
+
+dev_package = package.copy()
+
+dev_package['name'] = 'mlc:dev'
+dev_package['dockerfile'] = 'Dockerfile.dev'
+
+package = [package, dev_package]
