42904b7545b4b9ca3ad60486d7575c905a0f4ccd
Dustin Franklin <dustinf@nvidia.com>
2023-10-23 21:54:51 -0400

    updated ROS docs

--- packages/ros/config.py
+++ packages/ros/config.py
@@ -11,9 +11,9 @@ template['group'] = 'ros'
 template['depends'] = ['cmake', 'python', 'opencv']
 template['postfix'] = f"l4t-r{L4T_VERSION}"
 
-template['docs'] = "Various ROS/ROS2 containers for JetPack.  These build ROS from source to run them on the needed versions of Ubuntu.\n\n"
-template['docs'] += f"Supported ROS distros:   {' '.join([f'`{distro}`' for distro in ROS_DISTROS])}\n<br>\n"
-template['docs'] += f"Supported ROS packages:  {' '.join([f'`{pkg}`' for pkg in ROS_PACKAGES])}\n"
+template['docs'] = "docs.md"
+#template['docs'] += f"Supported ROS distros:   {' '.join([f'`{distro}`' for distro in ROS_DISTROS])}\n<br>\n"
+#template['docs'] += f"Supported ROS packages:  {' '.join([f'`{pkg}`' for pkg in ROS_PACKAGES])}\n"
 
 package = []
 
---
+++ packages/ros/docs.md
@@ -0,0 +1,7 @@
+> [!NOTE]  
+> See the optimized [Isaac ROS](https://github.com/NVIDIA-ISAAC-ROS) packages and containers for Humble and JetPack 5
+
+Various ROS/ROS2 containers for JetPack.  These build ROS from source to run them on the needed versions of Ubuntu.
+
+Supported ROS distros: `melodic` `noetic` `foxy` `galactic` `humble` `iron`
+Supported ROS packages: `ros_base` `ros_core` `desktop`
