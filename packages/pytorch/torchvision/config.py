785a1143568fef8e53b879b5f2f6d5627f0e9a59
Dustin Franklin <dustinf@nvidia.com>
2023-11-10 23:27:40 -0500

    updated torchvision for JetPack 6

--- packages/pytorch/torchvision/config.py
+++ packages/pytorch/torchvision/config.py
@@ -1,7 +1,9 @@
 
 from jetson_containers import L4T_VERSION, CUDA_ARCHITECTURES
 
-if L4T_VERSION.major >= 35:    # JetPack 5.0.2 / 5.1.x
+if L4T_VERSION.major >= 36:    # JetPack 6.0
+    TORCHVISION_VERSION = 'v0.16.0'
+elif L4T_VERSION.major >= 35:  # JetPack 5.0.2 / 5.1.x
     TORCHVISION_VERSION = 'v0.15.1'
 elif L4T_VERSION.major == 34:  # JetPack 5.0 / 5.0.1
     TORCHVISION_VERSION = 'v0.12.0'
