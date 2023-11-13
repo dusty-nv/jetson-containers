512f17beb5cddef62a1ca02d58d225efe84fc5b0
Dustin Franklin <dustinf@nvidia.com>
2023-11-12 20:25:27 -0500

    updated OpenCV for JetPack 6

--- packages/opencv/config.py
+++ packages/opencv/config.py
@@ -1,18 +1,29 @@
 
 from jetson_containers import L4T_VERSION, SYSTEM_ARCH
 
-if SYSTEM_ARCH == 'aarch64':
-    if L4T_VERSION.major >= 34:  # JetPack 5
-        OPENCV_URL = 'https://nvidia.box.com/shared/static/2hssa5g3v28ozvo3tc3qwxmn78yerca9.gz'
-        OPENCV_DEB = 'OpenCV-4.5.0-aarch64.tar.gz'
-    elif L4T_VERSION.major == 32:  # JetPack 4
-        OPENCV_URL = 'https://nvidia.box.com/shared/static/5v89u6g5rb62fpz4lh0rz531ajo2t5ef.gz'
-        OPENCV_DEB = 'OpenCV-4.5.0-aarch64.tar.gz'
-elif SYSTEM_ARCH == 'x86_64':
-    OPENCV_URL = 'https://nvidia.box.com/shared/static/vfp7krqf5bws752ts58rckpx3nyopmp1.gz'
-    OPENCV_DEB = 'OpenCV-4.5.0-x86_64.tar.gz'
+
+def install_opencv(version, url, deb, requires=None, default=False):
+    cv = package.copy()
     
-package['build_args'] = {
-    'OPENCV_URL': OPENCV_URL,
-    'OPENCV_DEB': OPENCV_DEB
-}
+    cv['name'] = f'opencv:{version}'
+    
+    cv['build_args'] = {
+        'OPENCV_URL': url,
+        'OPENCV_DEB': deb,
+    }
+    
+    if requires:
+        cv['requires'] = requires
+        
+    if default:
+        cv['alias'] = 'opencv'
+        
+    return cv
+    
+    
+package = [
+    install_opencv('4.8.1', 'https://nvidia.box.com/shared/static/ngp26xb9hb7dqbu6pbs7cs9flztmqwg0.gz', 'OpenCV-4.8.1-aarch64.tar.gz', '==36.*', default=True),
+    install_opencv('4.5.0', 'https://nvidia.box.com/shared/static/2hssa5g3v28ozvo3tc3qwxmn78yerca9.gz', 'OpenCV-4.5.0-aarch64.tar.gz', '==35.*', default=True),
+    install_opencv('4.5.0', 'https://nvidia.box.com/shared/static/5v89u6g5rb62fpz4lh0rz531ajo2t5ef.gz', 'OpenCV-4.5.0-aarch64.tar.gz', '==32.*', default=True),
+    #install_opencv('4.5.0', 'https://nvidia.box.com/shared/static/vfp7krqf5bws752ts58rckpx3nyopmp1.gz', 'OpenCV-4.5.0-x86_64.tar.gz', 'x86_64', default=True),
+]
