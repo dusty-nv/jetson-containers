22b9072b82eaddb719934b7a06db9f73afa158b4
Chitoku YATO <cyato@nvidia.com>
2023-11-28 18:10:04 -0800

    Make GPU load work on JP6

--- packages/hardware/oled/utils.py
+++ packages/hardware/oled/utils.py
@@ -3,7 +3,6 @@ import pkg_resources
 import platform
 import os
 
-
 def notebooks_dir():
     return pkg_resources.resource_filename('jetbot', 'notebooks')
 
@@ -79,8 +78,20 @@ def gpu_usage():
     Returns:
         float: The current GPU usage fraction.
     """
-    with open('/sys/devices/gpu.0/load', 'r') as f:
-        return float(f.read().strip('\n')) / 1000.0
+    load = 1
+    gpu_path = "/sys/class/devfreq/"
+    extensionsToCheck = ('.gv11b', '.gp10b', '.ga10b', '.gpu')
+    for item in os.listdir(gpu_path):
+        if item.endswith(extensionsToCheck):
+            path = os.path.realpath(os.path.join(gpu_path, item, "device"))
+
+    #gpu_load_path = "/sys/class/devfreq/17000000.ga10b/device/load"
+    gpu_load_path = os.path.realpath(os.path.join(path, "load"))
+    
+    with open(gpu_load_path, 'r') as f:
+        # Read current GPU load
+        load = float(f.read()) / 1000.0
+    return load
 
     
 def memory_usage():
@@ -99,3 +110,38 @@ def disk_usage():
         float: The current disk usage fraction.
     """
     return float(subprocess.check_output("df -h | awk '$NF==\"/\"{printf \"%s\", $5}'", shell = True ).decode('utf-8').strip('%')) / 100.0
+
+def cat(path):
+    with open(path, 'r') as f:
+        return f.readline().rstrip('\x00')
+
+def find_igpu(igpu_path):
+    # Check if exist a integrated gpu
+    # if not os.path.exists("/dev/nvhost-gpu") and not os.path.exists("/dev/nvhost-power-gpu"):
+    #     return []
+    igpu = {}
+    if not os.path.isdir(igpu_path):
+        return igpu
+    for item in os.listdir(igpu_path):
+        item_path = os.path.join(igpu_path, item)
+        if os.path.isfile(item_path) or os.path.islink(item_path):
+            # Check name device
+            name_path = "{item}/device/of_node/name".format(item=item_path)
+            if os.path.isfile(name_path):
+                # Decode name
+                name = cat(name_path)
+                # Check if gpu
+                if name in ['gv11b', 'gp10b', 'ga10b', 'gpu']:
+                    # Extract real path GPU device
+                    path = os.path.realpath(os.path.join(item_path, "device"))
+                    frq_path = os.path.realpath(item_path)
+                    igpu[name] = {'type': 'integrated', 'path': path, 'frq_path': frq_path}
+                    # Check if railgate exist
+                    path_railgate = os.path.join(path, "railgate_enable")
+                    if os.path.isfile(path_railgate):
+                        igpu[name]['railgate'] = path_railgate
+                    # Check if 3d scaling exist
+                    path_3d_scaling = os.path.join(path, "enable_3d_scaling")
+                    if os.path.isfile(path_3d_scaling):
+                        igpu[name]['3d_scaling'] = path_3d_scaling
+    return igpu
