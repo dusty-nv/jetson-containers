a15c54086fe4c0d4b86c86d9d8250505e46add9e
Dustin Franklin <dustinf@nvidia.com>
2023-11-30 12:14:58 -0500

    fixed LD_PRELOAD on JetPack 5

--- packages/llm/text-generation-webui/config.py
+++ packages/llm/text-generation-webui/config.py
@@ -3,7 +3,7 @@ from jetson_containers import L4T_VERSION
 
 if L4T_VERSION.major <= 35:
     package['depends'].append('bitsandbytes')
-    #package['build_args'] = {
-    #    'LD_PRELOAD_LIBS': '/usr/local/lib/python3.8/dist-packages/sklearn/__check_build/../../scikit_learn.libs/libgomp-d22c30c5.so.1.0.0'
-    #}
+    package['build_args'] = {
+        'LD_PRELOAD_LIBS': '/usr/local/lib/python3.8/dist-packages/sklearn/__check_build/../../scikit_learn.libs/libgomp-d22c30c5.so.1.0.0'
+    }
 
