99a3a1faaa8abbea0dd8bf2c2483ba5d567c5dcb
Dustin Franklin <dustinf@nvidia.com>
2024-02-10 00:57:58 -0500

    updated MLC version

--- packages/llm/mlc/config.py
+++ packages/llm/mlc/config.py
@@ -59,8 +59,10 @@ log_debug('-- MLC latest commit:', latest_sha)
 #default_dev=(L4T_VERSION.major >= 36)
 
 package = [
-    mlc(latest_sha, 'patches/51fb0f4.diff', llvm=18, tag='dev'),
-    mlc('9bf5723', 'patches/9bf5723.diff', llvm=17, requires='==35.*'), # 10/20/2023
-    mlc('51fb0f4', 'patches/51fb0f4.diff', llvm=17, default=True),      # 12/15/2023
-    mlc('d840de5', 'patches/d840de5.diff', llvm=17),                    # 02/06/2024
+    mlc(latest_sha, 'patches/3feed05.diff', tag='dev'),
+    mlc('9bf5723', 'patches/9bf5723.diff', requires='==35.*'), # 10/20/2023
+    mlc('51fb0f4', 'patches/51fb0f4.diff', default=True),      # 12/15/2023
+    mlc('d840de5', 'patches/d840de5.diff'),                    # 02/06/2024
+    mlc('006e138', 'patches/d840de5.diff'),                    # 02/07/2024
+    mlc('3feed05', 'patches/3feed05.diff'),                    # 02/08/2024
 ]
