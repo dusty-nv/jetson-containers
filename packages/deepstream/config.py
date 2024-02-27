b6c788c2bf1564e93a2159e8710edbaf72df11cd
Dustin Franklin <dustinf@nvidia.com>
2024-02-26 23:16:33 -0500

    updated DeepStream container for JP6

--- packages/deepstream/config.py
+++ packages/deepstream/config.py
@@ -2,16 +2,20 @@
 from jetson_containers import L4T_VERSION, PYTHON_VERSION
 from packaging.version import Version
 
-if L4T_VERSION >= Version('35.2.1'): # JetPack 5.1
-    DEEPSTREAM_URL = 'https://nvidia.box.com/shared/static/2q5nnsvj6bdyk4qir4uglfyuc7t55m7e.tbz2'
+if L4T_VERSION >= Version('36.2.0'): # JetPack 6.0
+    DEEPSTREAM_URL = 'https://api.ngc.nvidia.com/v2/resources/org/nvidia/deepstream/6.4/files?redirect=true&path=deepstream_sdk_v6.4.0_jetson.tbz2'
+    DEEPSTREAM_TAR = 'deepstream_sdk_v6.4.0_jetson.tbz2'
+    PYDS_VERSION = 'v1.1.10'
+elif L4T_VERSION >= Version('35.2.1'): # JetPack 5.1
+    DEEPSTREAM_URL = 'https://api.ngc.nvidia.com/v2/resources/org/nvidia/deepstream/6.2/files?redirect=true&path=deepstream_sdk_v6.2.0_jetson.tbz2' #'https://nvidia.box.com/shared/static/2q5nnsvj6bdyk4qir4uglfyuc7t55m7e.tbz2'
     DEEPSTREAM_TAR = 'deepstream_sdk_v6.2.0_jetson.tbz2'
     PYDS_VERSION = 'v1.1.6'
 elif L4T_VERSION >= Version('34'):  # JetPack 5.0
-    DEEPSTREAM_URL = 'https://nvidia.box.com/shared/static/8jdbxu016wrjz8g5q7dzetj2seksmih9.tbz2'
+    DEEPSTREAM_URL = 'https://api.ngc.nvidia.com/v2/resources/org/nvidia/deepstream/6.1/files?redirect=true&path=deepstream_sdk_v6.1.0_jetson.tbz2' #'https://nvidia.box.com/shared/static/8jdbxu016wrjz8g5q7dzetj2seksmih9.tbz2'
     DEEPSTREAM_TAR = 'deepstream_sdk_v6.1.0_jetson.tbz2'
     PYDS_VERSION = 'v1.1.3'
 elif L4T_VERSION >= Version('32.6'): # JetPack 4.6
-    DEEPSTREAM_URL = 'https://nvidia.box.com/shared/static/uetg0vwukwuqf8109i5coq0rizuflniu.tbz2'
+    DEEPSTREAM_URL = 'https://api.ngc.nvidia.com/v2/resources/org/nvidia/deepstream/6.0/files?redirect=true&path=deepstream_sdk_v6.0.0_jetson.tbz2'
     DEEPSTREAM_TAR = 'deepstream_sdk_v6.0.0_jetson.tbz2'
     PYDS_VERSION = 'v1.1.1'
 else:
