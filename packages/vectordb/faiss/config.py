edd4eee71345763b692f0e75b9c72d1218690b78
Dustin Franklin <dustinf@nvidia.com>
2024-03-05 01:58:34 -0500

    updated faiss

--- packages/vectordb/faiss/config.py
+++ packages/vectordb/faiss/config.py
@@ -1,6 +1,40 @@
 
-from jetson_containers import CUDA_ARCHITECTURES
+from jetson_containers import CUDA_ARCHITECTURES, find_container
 
-package['build_args'] = {
-    'CUDA_ARCHITECTURES': ';'.join([str(x) for x in CUDA_ARCHITECTURES]),
-}
+
+def faiss(version, tag=None, requires=None, default=False):
+    builder = package.copy()
+    runtime = package.copy()
+    
+    if default:
+        builder['alias'] = 'faiss:builder'
+        runtime['alias'] = 'faiss'
+        
+    if requires:
+        builder['requires'] = requires
+        runtime['requires'] = requires   
+        
+    if not tag:
+        tag = version
+
+    builder['name'] = f'faiss:{tag}-builder'
+    runtime['name'] = f'faiss:{tag}'
+    
+    builder['dockerfile'] = 'Dockerfile.builder'
+    
+    builder['build_args'] = {
+        'FAISS_VERSION': version,
+        'CUDA_ARCHITECTURES': ';'.join([str(x) for x in CUDA_ARCHITECTURES]),
+    }
+    
+    runtime['build_args'] = {
+        'BUILD_IMAGE': find_container(builder['name']),
+    }
+    
+    return builder, runtime
+    
+package = [
+    faiss('v1.7.3'),
+    #faiss('v1.8.0'),  # encounters type_info build error sometime after be12427
+    faiss('be12427', default=True),  # known good build on JP5/JP6 from 12/12/2023
+]
