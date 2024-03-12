3b831931885b9ba76087143b384b1d9a9a641814
Mieszko Syty <mieszko.syty@fondy.eu>
2024-03-12 13:27:38 +0100

    feat: #381 Add cudnn 9.0 config

--- packages/cuda/cudnn/config.py
+++ packages/cuda/cudnn/config.py
@@ -1,9 +1,7 @@
 import os
 
-from jetson_containers import L4T_VERSION, CUDA_ARCHITECTURES, CUDA_VERSION
 
-
-def cudnn_package(version, url, deb, packages=None, depends=None, requires=None, default=False):
+def cudnn_package(version, url, deb, packages=None, depends=None, requires=None, default=False) -> list:
     """
     Generate containers for a particular version of cuDNN installed from debian packages
     """
@@ -58,9 +56,22 @@ def cudnn_builtin(version=None, requires=None, default=False):
 
     
 package = [
-    
     # JetPack 6
-    cudnn_package('8.9', 'https://nvidia.box.com/shared/static/ht4li6b0j365ta7b76a6gw29rk5xh8cy.deb', 'cudnn-local-tegra-repo-ubuntu2204-8.9.4.25', requires='==36.*', default=True), 
+    cudnn_package(
+        '9.0', # https://developer.nvidia.com/cudnn-downloads?target_os=Linux&target_arch=aarch64-jetson&Compilation=Native&Distribution=Ubuntu&target_version=22.04&target_type=deb_local
+        'https://developer.download.nvidia.com/compute/cudnn/9.0.0/local_installers/cudnn-local-tegra-repo-ubuntu2204-9.0.0_1.0-1_arm64.deb',
+        'cudnn-local-tegra-repo-ubuntu2204-9.0.0_1.0-1_arm64',
+        packages='cudnn-cuda-12',
+        requires='==36.*',
+        default=True
+    ), 
+    cudnn_package(
+        '8.9',
+        'https://nvidia.box.com/shared/static/ht4li6b0j365ta7b76a6gw29rk5xh8cy.deb',
+        'cudnn-local-tegra-repo-ubuntu2204-8.9.4.25',
+        requires='==36.*',
+        default=False
+    ), 
 
     # JetPack 4-5 (cuDNN installed in base container)
     cudnn_builtin(requires='<36', default=True),
