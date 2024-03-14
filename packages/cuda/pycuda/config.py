6f43579b5c9154347411113e4c49dcb866c89219
Mieszko Syty <mieszko.syty@fondy.eu>
2024-03-14 13:02:23 +0100

    fix: Streamlined the pycuda container

--- packages/cuda/pycuda/config.py
+++ packages/cuda/pycuda/config.py
@@ -1,12 +1,19 @@
-
-from jetson_containers import PYTHON_VERSION
+from jetson_containers import PYTHON_VERSION, find_container
 from packaging.version import Version
 
-if PYTHON_VERSION == Version('3.6'):
-    PYCUDA_VERSION = 'v2022.1'  # last version to support Python 3.6
-else:
-    PYCUDA_VERSION = 'main'
 
-package['build_args'] = {
-    'PYCUDA_VERSION': PYCUDA_VERSION,
+builder = package.copy()
+runtime = package.copy()
+
+builder['name'] = 'pycuda:builder'
+builder['dockerfile'] = 'Dockerfile.builder'
+builder['build_args'] = {
+    # v2022.1 is the last version to support Python 3.6
+    'PYCUDA_VERSION': 'v2022.1' if PYTHON_VERSION == Version('3.6') else 'main',
 }
+
+runtime['build_args'] = {
+    'BUILD_IMAGE': find_container(builder['name']),
+}
+
+package = [builder, runtime]
