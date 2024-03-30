13fa8a92b3df0c4412d9fcca0485ba318992b09b
Dustin Franklin <dustinf@nvidia.com>
2024-03-30 14:34:34 -0400

    updates for pip wheel builder cache

--- packages/llm/awq/config.py
+++ packages/llm/awq/config.py
@@ -1,18 +1,22 @@
 
-from jetson_containers import CUDA_ARCHITECTURES
-import copy
+def awq(version, kernels=None, branch=None, default=False):
+    pkg = package.copy()
+    pkg['name'] = f'awq:{version}'
 
-# AWQ package is for Orin only:
-#  ptxas /tmp/tmpxft_000000b4_00000000-7_gemm_cuda_gen.compute_72.ptx, line 889; error
-#   Modifier '.m8n8' requires .target sm_75 or higher
-#   Feature '.m16n8k16' requires .target sm_80 or higher
-package['build_args'] = {
-    'TORCH_CUDA_ARCH_LIST': '8.7',
-}
+    if default:
+        pkg['alias'] = 'awq'
+    
+    if not branch:
+        branch = version
+        
+    pkg['build_args'] = {
+        'AWQ_BRANCH': branch,
+        'AWQ_VERSION': version,
+        'AWQ_KERNEL_VERSION': kernels,
+    }
+    
+    return pkg
 
-dev_package = copy.deepcopy(package)
-
-dev_package['name'] = 'awq:dev'
-dev_package['build_args']['AWQ_BRANCH'] = 'dev/tinychat_update_0918'
-
-package = [package, dev_package]
+package = [
+    awq('0.1.0', kernels='0.0.0', branch='main', default=True),
+]
--- packages/llm/llama_cpp/build.sh
+++ packages/llm/llama_cpp/build.sh
@@ -7,12 +7,12 @@ git clone --branch=v${LLAMA_CPP_BRANCH} --depth=1 --recursive https://github.com
 
 CMAKE_ARGS="-DLLAMA_CUBLAS=on -DLLAMA_CUDA_F16=1 -DCMAKE_CUDA_ARCHITECTURES=${CUDA_ARCHITECTURES}" \
 FORCE_CMAKE=1 \
-pip3 wheel --wheel-dir=/opt --verbose ./llama-cpp-python
+pip3 wheel --wheel-dir=/opt/wheels --verbose ./llama-cpp-python
 
-pip3 install --no-cache-dir --verbose /opt/llama_cpp_python*.whl
+pip3 install --no-cache-dir --verbose /opt/wheels/llama_cpp_python*.whl
 pip3 show llama-cpp-python
 
 python3 -c 'import llama_cpp'
 python3 -m llama_cpp.server --help
 
-twine upload --verbose /opt/llama_cpp_python*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
+twine upload --verbose /opt/wheels/llama_cpp_python*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
