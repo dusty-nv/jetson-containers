c9c4a2861e5e782c8f73c9f0624623e2cbcba785
Dustin Franklin <dustinf@nvidia.com>
2024-04-04 22:33:09 -0400

    added tarfile install method

--- packages/tensorrt/config.py
+++ packages/tensorrt/config.py
@@ -1,7 +1,26 @@
+from jetson_containers import L4T_VERSION, CUDA_VERSION, update_dependencies
+from packaging.version import Version
+
 import os
 
+package['depends'] = ['cuda', 'cudnn', 'python']
+package['test'] = ['test.sh']
 
-def tensorrt_package(version, url, deb, packages=None, requires=None, default=False):
+if 'TENSORRT_VERSION' in os.environ and len(os.environ['TENSORRT_VERSION']) > 0:
+    TENSORRT_VERSION = Version(os.environ['TENSORRT_VERSION'])
+else:
+    if L4T_VERSION.major >= 36:
+        if CUDA_VERSION >= Version('12.4'):
+            TENSORRT_VERSION = Version('10.0')
+        else:
+            TENSORRT_VERSION = Version('8.6')
+    elif L4T_VERSION.major >= 34:
+        TENSORRT_VERSION = Version('8.5')
+    elif L4T_VERSION.major >= 32:
+        TENSORRT_VERSION = Version('8.2')
+        
+        
+def tensorrt_deb(version, url, deb, cudnn=None, packages=None, requires=None):
     """
     Generate containers for a particular version of TensorRT installed from debian packages
     """
@@ -9,22 +28,51 @@ def tensorrt_package(version, url, deb, packages=None, requires=None, default=Fa
         packages = os.environ.get('TENSORRT_PACKAGES', 'tensorrt tensorrt-libs python3-libnvinfer-dev')
     
     tensorrt = package.copy()
+    
     tensorrt['name'] = f'tensorrt:{version}'
+    tensorrt['dockerfile'] = 'Dockerfile.deb'
+    
     tensorrt['build_args'] = {
         'TENSORRT_URL': url,
         'TENSORRT_DEB': deb,
         'TENSORRT_PACKAGES': packages,
     }
 
-    if default:
+    if Version(version) == TENSORRT_VERSION:
         tensorrt['alias'] = 'tensorrt'
     
+    if cudnn:
+        tensorrt['depends'] = update_dependencies(tensorrt['depends'], f"cudnn:{cudnn}")
+         
     if requires:
         tensorrt['requires'] = requires
 
     return tensorrt
 
 
+def tensorrt_tar(version, url, cudnn=None, requires=None):
+    """
+    Generate containers for a particular version of TensorRT installed from tar.gz file
+    """
+    tensorrt = package.copy()
+    
+    tensorrt['name'] = f'tensorrt:{version}'
+
+    tensorrt['dockerfile'] = 'Dockerfile.tar'
+    tensorrt['build_args'] = {'TENSORRT_URL': url}
+
+    if Version(version) == TENSORRT_VERSION:
+        tensorrt['alias'] = 'tensorrt'
+    
+    if cudnn:
+        tensorrt['depends'] = update_dependencies(tensorrt['depends'], f"cudnn:{cudnn}")
+         
+    if requires:
+        tensorrt['requires'] = requires
+
+    return tensorrt
+    
+    
 def tensorrt_builtin(version=None, requires=None, default=False):
     """
     Backwards-compatability for when TensorRT already installed in base container (like l4t-jetpack)
@@ -43,14 +91,15 @@ def tensorrt_builtin(version=None, requires=None, default=False):
     if requires:
         passthrough['requires'] = requires
         
-    del passthrough['dockerfile']
+    #del passthrough['dockerfile']
     return passthrough
 
     
 package = [
     # JetPack 6
-    tensorrt_package('8.6', 'https://nvidia.box.com/shared/static/hmwr57hm88bxqrycvlyma34c3k4c53t9.deb', 'nv-tensorrt-local-repo-l4t-8.6.2-cuda-12.2', requires='==36.*', default=True), 
-
+    tensorrt_deb('8.6', 'https://nvidia.box.com/shared/static/hmwr57hm88bxqrycvlyma34c3k4c53t9.deb', 'nv-tensorrt-local-repo-l4t-8.6.2-cuda-12.2', cudnn='8.9', requires='==36.*'), 
+    tensorrt_tar('10.0', 'https://nvidia.box.com/shared/static/n00by5242vvf800jbhzjioj77vd7fqu7.gz', cudnn='9.0', requires='==36.*'), 
+    
     # JetPack 4-5 (TensorRT installed in base container)
     tensorrt_builtin(requires='<36', default=True),
 ]
