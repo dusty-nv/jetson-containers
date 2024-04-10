0e842b5dadc78c5936149da53063003f19419d3f
Dustin Franklin <dustinf@nvidia.com>
2024-04-10 11:39:17 -0400

    fixed exllama for JetPack 5

--- packages/llm/exllama/config.py
+++ packages/llm/exllama/config.py
@@ -1,9 +1,12 @@
 
-def exllama(version, branch=None, default=False):
+def exllama(version, branch=None, requires=None, default=False):
     pkg = package.copy()
 
     pkg['name'] = f'exllama:{version}'
 
+    if requires:
+        pkg['requires'] = requires
+        
     if default:
         pkg['alias'] = 'exllama'
     
@@ -18,6 +21,7 @@ def exllama(version, branch=None, default=False):
     return pkg
 
 package = [
-    #exllama('0.0.16', default=True),
-    exllama('0.0.15', default=True),
+    #exllama('0.0.16', requires='>=36', default=True),
+    exllama('0.0.15', requires='>=36', default=True),
+    exllama('0.0.14', requires='==35.*', default=True),
 ]
