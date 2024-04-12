11f6b7e292f27bf6960e179d05277c4052d27949
Dustin Franklin <dustinf@nvidia.com>
2024-04-12 12:00:37 -0400

    added flash-attention container

---
+++ packages/llm/flash-attention/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -ex
+
+echo "Building FlashAttention ${FLASH_ATTENTION_VERSION}"
+
+git clone --depth=1 --branch=v${FLASH_ATTENTION_VERSION} https://github.com/Dao-AILab/flash-attention /opt/flash-attention
+
+cd /opt/flash-attention
+
+git apply /tmp/flash-attention/patch.diff
+git diff
+git status
+    
+FLASH_ATTENTION_FORCE_BUILD=1 \
+FLASH_ATTENTION_FORCE_CXX11_ABI=0 \
+FLASH_ATTENTION_SKIP_CUDA_BUILD=0 \
+python3 setup.py --verbose bdist_wheel --dist-dir /opt
+
+cd /
+
+pip3 install --no-cache-dir --verbose /opt/flash-attn*.whl
+pip3 show flash-attn && python3 -c 'import flash_attn'
+
+twine upload --verbose /opt/flash-attn*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
