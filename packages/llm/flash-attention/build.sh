aeae01f2cdfe5e88916b5ee8d098c0d7802c1f8e
Dustin Franklin <dustinf@nvidia.com>
2024-04-13 00:02:10 -0400

    fixed flash-attention build for JP6

--- packages/llm/flash-attention/build.sh
+++ packages/llm/flash-attention/build.sh
@@ -10,15 +10,17 @@ cd /opt/flash-attention
 git apply /tmp/flash-attention/patch.diff
 git diff
 git status
-    
+ 
 FLASH_ATTENTION_FORCE_BUILD=1 \
 FLASH_ATTENTION_FORCE_CXX11_ABI=0 \
 FLASH_ATTENTION_SKIP_CUDA_BUILD=0 \
+MAX_JOBS=$(nproc) \
 python3 setup.py --verbose bdist_wheel --dist-dir /opt
 
+ls /opt
 cd /
 
-pip3 install --no-cache-dir --verbose /opt/flash-attn*.whl
-pip3 show flash-attn && python3 -c 'import flash_attn'
+pip3 install --no-cache-dir --verbose /opt/flash_attn*.whl
+#pip3 show flash-attn && python3 -c 'import flash_attn'
 
-twine upload --verbose /opt/flash-attn*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
+twine upload --verbose /opt/flash_attn*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
