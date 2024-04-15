e9869c238ea40add5d4c2df920d98f6c007dfdf3
Dustin Franklin <dustinf@nvidia.com>
2024-04-15 10:12:12 -0400

    updated NanoLLM container config

--- packages/llm/nano_llm/config.py
+++ packages/llm/nano_llm/config.py
@@ -1,5 +1,30 @@
 
 from jetson_containers import L4T_VERSION
 
-if L4T_VERSION.major >= 36:
-    package['depends'].append('xtts')
+def NanoLLM(version, branch=None, requires=None, default=False):
+    pkg = package.copy()
+  
+    pkg['name'] = f"nano_llm:{version}"
+
+    if default:
+        pkg['alias'] = 'nano_llm'
+        
+    if requires:
+        pkg['requires'] = requires   
+
+    if L4T_VERSION.major >= 36:
+        package['depends'] = package['depends'] + ['xtts']
+    
+    if not branch:
+        branch = version
+        
+    pkg['build_args'] = {'NANO_LLM_BRANCH': branch}
+
+    return pkg
+
+package = [
+    NanoLLM('main', default=True),
+    NanoLLM('24.4'),
+]
+
+
