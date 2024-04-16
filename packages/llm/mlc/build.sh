a2750e0ccdc014f867ccebe74bcc11a05545465a
Dustin Franklin <dustinf@nvidia.com>
2024-04-16 19:15:07 -0400

    updated MLC containers

--- packages/llm/mlc/build.sh
+++ packages/llm/mlc/build.sh
@@ -8,17 +8,17 @@ echo "Building MLC ${MLC_VERSION} (commit=${MLC_COMMIT})"
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 ./llvm.sh ${LLVM_VERSION} all
-ln -s /usr/bin/llvm-config-* /usr/bin/llvm-config
+ln -sf /usr/bin/llvm-config-* /usr/bin/llvm-config
 
 # could NOT find zstd (missing: zstd_LIBRARY zstd_INCLUDE_DIR)
 apt-get update
-apt-get install -y --no-install-recommends libzstd-dev
+apt-get install -y --no-install-recommends libzstd-dev ccache
 rm -rf /var/lib/apt/lists/*
 apt-get clean
 	
 # clone the sources
 git clone https://github.com/mlc-ai/mlc-llm /opt/mlc-llm
-cd /opt/mlc-llm && \
+cd /opt/mlc-llm
 git checkout ${MLC_COMMIT}
 git submodule update --init --recursive
     
@@ -32,6 +32,9 @@ git diff --submodule=diff
 
 # add extras to the source
 cp /tmp/mlc/benchmark.py /opt/mlc-llm/
+
+# flashinfer build references 'python'
+ln -sf /usr/bin/python3 /usr/bin/python
 
 # disable pytorch: https://github.com/apache/tvm/issues/9362
 # -DUSE_LIBTORCH=$(pip3 show torch | grep Location: | cut -d' ' -f2)/torch
@@ -57,8 +60,8 @@ cmake -G Ninja \
 	
 ninja
 
-rm -rf CMakeFiles tvm/CMakeFiles
-rm -rf tokenizers/CMakeFiles tokenizers/release
+#rm -rf CMakeFiles tvm/CMakeFiles
+#rm -rf tokenizers/CMakeFiles tokenizers/release
 
 # build TVM python module
 cd /opt/mlc-llm/3rdparty/tvm/python
@@ -66,28 +69,30 @@ cd /opt/mlc-llm/3rdparty/tvm/python
 TVM_LIBRARY_PATH=/opt/mlc-llm/build/tvm \
 python3 setup.py --verbose bdist_wheel --dist-dir /opt
 
-pip3 install --no-cache-dir --verbose tvm*.whl
-pip3 show tvm && python3 -c 'import tvm'
-
-rm -rf dist build
+pip3 install --no-cache-dir --verbose /opt/tvm*.whl
+#pip3 show tvm && python3 -c 'import tvm'
+#rm -rf dist build
 
 # build mlc-llm python module
 cd /opt/mlc-llm
-python3 setup.py --verbose bdist_wheel --dist-dir /opt
+
+if [ -f setup.py ]; then
+	python3 setup.py --verbose bdist_wheel --dist-dir /opt
+fi
 
 cd python
 python3 setup.py --verbose bdist_wheel --dist-dir /opt
 
-pip3 install --no-cache-dir --verbose mlc*.whl
+pip3 install --no-cache-dir --verbose /opt/mlc*.whl
 
 # make sure it loads
-cd /
-pip3 show mlc_llm
-python3 -m mlc_llm.build --help
-python3 -c "from mlc_chat import ChatModule; print(ChatModule)"
+#cd /
+#pip3 show mlc_llm
+#python3 -m mlc_llm.build --help
+#python3 -c "from mlc_chat import ChatModule; print(ChatModule)"
     
 # make the CUTLASS sources available for model builder
-ln -s /opt/mlc-llm/3rdparty/tvm/3rdparty /usr/local/lib/python${PYTHON_VERSION}/tvm/3rdparty
+ln -s /opt/mlc-llm/3rdparty/tvm/3rdparty /usr/local/lib/python${PYTHON_VERSION}/dist-packages/tvm/3rdparty
 
 # upload wheels
 twine upload --verbose /opt/tvm*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
