e014dfe478ebfc039df47feee78b2980d09d4073
Mieszko Syty <mieszko@ms1design.pl>
2024-04-22 18:45:48 +0200

    feat(wyoming-openwakeword): Refactor, independent rootfs, config.py

--- packages/smart-home/wyoming/openwakeword/build.sh
+++ packages/smart-home/wyoming/openwakeword/build.sh
@@ -13,33 +13,11 @@ pip3 install --no-cache-dir -U \
    setuptools \
    wheel
    
-echo "wyoming-openwakeword: ${OPENWAKEWORD_VERSION}"
+echo "wyoming-openwakeword: ${WYOMING_OPENWAKEWORD_VERSION}"
 
+# TODO: This still uses tflite on CPU / Need to build from scratch on onnxruntime-gpu
 pip3 install --no-cache-dir \
    --extra-index-url https://www.piwheels.org/simple \
-   "wyoming-openwakeword @ https://github.com/rhasspy/wyoming-openwakeword/archive/refs/tags/v${OPENWAKEWORD_VERSION}.tar.gz"
-   
-git clone --depth=1 https://github.com/home-assistant/addons /tmp/addons
-git -C /tmp/addons sparse-checkout set --no-cone openwakeword/ '!*/openwakeword'
-
-# Fix: Adjust reading config data from config.yaml file when not using HA Supervisor
-sed -i \
-   -e "s/bashio::config.true 'debug_logging'/[[ \"$OPENWAKEWORD_DEBUG\" == \"true\" ]]/g" \
-   -e "s/--threshold.*/--threshold \"$OPENWAKEWORD_THRESHOLD\" \\\\/g" \
-   -e "s/--trigger-level.*/--trigger-level \"$OPENWAKEWORD_TRIGGER_LEVEL\" \${flags[@]}/g" \
-   /tmp/addons/openwakeword/rootfs/etc/s6-overlay/s6-rc.d/openwakeword/run
-
-# Fix: Disable native Discovery handled by HA Supervisor 
-cat /tmp/addons/openwakeword/rootfs/etc/s6-overlay/s6-rc.d/discovery/run
-sed -i '$d' /tmp/addons/openwakeword/rootfs/etc/s6-overlay/s6-rc.d/discovery/run
-sed -i '$d' /tmp/addons/openwakeword/rootfs/etc/s6-overlay/s6-rc.d/discovery/run
-sed -i '$d' /tmp/addons/openwakeword/rootfs/etc/s6-overlay/s6-rc.d/discovery/run
-sed -i '$d' /tmp/addons/openwakeword/rootfs/etc/s6-overlay/s6-rc.d/discovery/run
-sed -i '$d' /tmp/addons/openwakeword/rootfs/etc/s6-overlay/s6-rc.d/discovery/run
-cat /tmp/addons/openwakeword/rootfs/etc/s6-overlay/s6-rc.d/discovery/run
-
-cp -r /tmp/addons/openwakeword/rootfs/* /
-cp /tmp/addons/openwakeword/config.yaml /etc/s6-overlay/s6-rc.d/openwakeword/config.yaml
-rm -rf /tmp/addons
+   "wyoming-openwakeword @ https://github.com/rhasspy/wyoming-openwakeword/archive/refs/tags/v${WYOMING_OPENWAKEWORD_VERSION}.tar.gz"
 
 python3 -c 'import wyoming_openwakeword; print(wyoming_openwakeword.__version__);'
