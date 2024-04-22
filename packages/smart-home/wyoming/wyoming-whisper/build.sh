7a42baca973611d81967b7dd49f91b80fb3a582f
Mieszko Syty <mieszko@ms1design.pl>
2024-04-22 18:46:56 +0200

    feat(wyoming-whisper): Refactor, independent rootfs, config.py

--- packages/smart-home/wyoming/wyoming-whisper/build.sh
+++ packages/smart-home/wyoming/wyoming-whisper/build.sh
@@ -13,37 +13,11 @@ pip3 install --no-cache-dir -U \
    wheel
 
 # Clone wyoming-faster-whisper layer
-git clone https://github.com/rhasspy/wyoming-faster-whisper /tmp/wyoming-faster-whisper
+git clone --branch=v${WYOMING_WHISPER_VERSION} https://github.com/rhasspy/wyoming-faster-whisper /tmp/wyoming-faster-whisper
 pip3 install --no-cache-dir --verbose -r /tmp/wyoming-faster-whisper/requirements.txt
 pip3 wheel --wheel-dir=/opt/ --no-deps --verbose /tmp/wyoming-faster-whisper
 pip3 install --no-cache-dir --verbose /opt/wyoming_faster_whisper*.whl
 pip3 show wyoming_faster_whisper
 rm -rf /tmp/wyoming-faster-whisper
-
-# Clone rootfs
-git clone --depth=1 https://github.com/home-assistant/addons /tmp/addons
-git -C /tmp/addons sparse-checkout set --no-cone whisper/ '!*/whisper'
-
-# Fix: Adjust reading config data from config.yaml file when not using HA Supervisor
-sed -i \
-   -e "s/bashio::config.true 'debug_logging'/[[ \"$WHISPER_DEBUG\" == \"true\" ]]/g" \
-   -e "s/model=\".*/model=\"${WHISPER_MODEL}\"/g" \
-   -e "s/--beam-size.*/--beam-size \"$WHISPER_BEAM_SIZE\" \\\\/g" \
-   -e "s/--language.*/--language \"$WHISPER_LANGUAGE\" \\\\/g" \
-   -e "s/--initial-prompt.*/--initial-prompt \"$WHISPER_INITIAL_PROMPT\" \\\\/g" \
-   /tmp/addons/whisper/rootfs/etc/s6-overlay/s6-rc.d/whisper/run
-
-# Fix: Disable native Discovery handled by HA Supervisor
-cat /tmp/addons/whisper/rootfs/etc/s6-overlay/s6-rc.d/discovery/run
-sed -i '$d' /tmp/addons/whisper/rootfs/etc/s6-overlay/s6-rc.d/discovery/run
-sed -i '$d' /tmp/addons/whisper/rootfs/etc/s6-overlay/s6-rc.d/discovery/run
-sed -i '$d' /tmp/addons/whisper/rootfs/etc/s6-overlay/s6-rc.d/discovery/run
-sed -i '$d' /tmp/addons/whisper/rootfs/etc/s6-overlay/s6-rc.d/discovery/run
-sed -i '$d' /tmp/addons/whisper/rootfs/etc/s6-overlay/s6-rc.d/discovery/run
-cat /tmp/addons/whisper/rootfs/etc/s6-overlay/s6-rc.d/discovery/run
-
-cp -r /tmp/addons/whisper/rootfs/* /
-cp /tmp/addons/whisper/config.yaml /etc/s6-overlay/s6-rc.d/whisper/config.yaml
-rm -rf /tmp/addons
 
 python3 -c 'import wyoming_faster_whisper; print(wyoming_faster_whisper.__version__);'
