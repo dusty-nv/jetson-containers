a51a1287ede4b5a04cc24d4aa76a5e6c2125ad43
Chitoku YATO <cyato@nvidia.com>
2024-05-11 09:31:55 -0700

    Update openai-triton package to the new builder-upload-to-PIP-server style

--- packages/openai-triton/config.py
+++ packages/openai-triton/config.py
@@ -1,14 +1,42 @@
-from jetson_containers import find_container
+def openai_triton(version, requires=None, default=False):
+    pkg = package.copy()
+
+    if requires:
+        pkg['requires'] = requires   
+
+    pkg['name'] = f'openai-triton:{version}'
+    
+    pkg['build_args'] = {
+        'OPENAITRIRON_VERSION': version,
+    }
+    
+    builder = pkg.copy()
+    
+    builder['name'] = f'openai-triton:{version}-builder'
+
+    if default:
+        pkg['alias'] = 'openai-triton'
+        builder['alias'] = 'openai-triton:builder'
+        
+    return pkg, builder
+
+package = [
+    openai_triton('2.1.0', default=True),
+]
+
+# from jetson_containers import find_container
 
 
-builder = package.copy()
-runtime = package.copy()
+# builder = package.copy()
+# runtime = package.copy()
 
-builder['name'] = 'openai-triton:builder'
-builder['dockerfile'] = 'Dockerfile.builder'
+# builder['name'] = 'openai-triton:builder'
+# builder['dockerfile'] = 'Dockerfile.builder'
 
-runtime['build_args'] = {
-    'BUILD_IMAGE': find_container(builder['name']),
-}
+# print(" ============== [openai-triton/config.py] =============== ")
 
-package = [builder, runtime]
+# runtime['build_args'] = {
+#     'BUILD_IMAGE': find_container(builder['name']),
+# }
+
+# package = [builder, runtime]
