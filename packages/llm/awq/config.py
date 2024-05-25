e2d017d537ba8a68f51509ce4565ea90f5961136
Dustin Franklin <dustinf@nvidia.com>
2024-05-24 22:41:06 -0400

    updated AWQ container config with builder image

--- packages/llm/awq/config.py
+++ packages/llm/awq/config.py
@@ -3,19 +3,26 @@ def awq(version, kernels=None, branch=None, default=False):
     pkg = package.copy()
     pkg['name'] = f'awq:{version}'
 
-    if default:
-        pkg['alias'] = 'awq'
-    
     if not branch:
         branch = version
         
     pkg['build_args'] = {
+        'AWQ_REPO': 'mit-han-lab/llm-awq',
         'AWQ_BRANCH': branch,
         'AWQ_VERSION': version,
         'AWQ_KERNEL_VERSION': kernels,
     }
     
-    return pkg
+    builder = pkg.copy()
+    
+    builder['name'] = f'awq:{version}-builder'
+    builder['build_args'] = {**pkg['build_args'], **{'FORCE_BUILD': 'on'}}
+
+    if default:
+        pkg['alias'] = 'awq'
+        builder['alias'] = 'awq:builder'
+
+    return pkg, builder
 
 package = [
     awq('0.1.0', kernels='0.0.0', branch='main', default=True),
