49df6bc4c67822d1a7b52c2f169be73645a27aa0
Dustin Franklin <dustinf@nvidia.com>
2024-06-08 12:18:34 -0400

    updated audiocraft and voicecraft

--- packages/openai-triton/install.sh
+++ packages/openai-triton/install.sh
@@ -2,8 +2,8 @@
 set -ex
 
 if [ "$FORCE_BUILD" == "on" ]; then
-	echo "Forcing build of openai_triron ${OPENAITRIRON_VERSION}"
+	echo "Forcing build of openai_triton ${OPENAITRITON_VERSION}"
 	exit 1
 fi
 
-pip3 install --no-cache-dir --verbose openai_triron==${OPENAITRIRON_VERSION}
+pip3 install --no-cache-dir --verbose triton==${OPENAITRITON_VERSION}
