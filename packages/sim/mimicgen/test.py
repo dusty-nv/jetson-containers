d50933d85ea9b13cc4dc6c32a859e8fa2a5f7e2d
Dustin Franklin <dustinf@nvidia.com>
2024-07-29 11:12:07 -0400

    updated sim tests

--- packages/sim/mimicgen/test.py
+++ packages/sim/mimicgen/test.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+import os
 import time
+import json
+import pprint
 import imageio
+import argparse
 import mimicgen
+import multiprocessing
+
 import numpy as np
 import robosuite as rs
+
 
 print('MIMICGEN    ', mimicgen.__version__)
 print('ROBOSUITE   ', rs.__version__)
@@ -12,38 +19,128 @@ print('CONTROLLERS ', list(rs.ALL_CONTROLLERS))
 print('ROBOTS      ', list(rs.ALL_ROBOTS))
 print('GRIPPERS    ', list(rs.ALL_GRIPPERS), '\n')
 
-# create environment instance
-env = rs.make(
-    env_name="Stack_D0", 
-    robots="Panda",
-    gripper_types="PandaGripper",
-    has_renderer=False,
-    has_offscreen_renderer=True,
-    use_camera_obs=True,
-    camera_names="frontview",
-    camera_widths=512,
-    camera_heights=512,
-)
 
-# reset the environment
-env.reset()
-frames = []
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    
+parser.add_argument('--tasks', type=str, nargs='+', default=['Stack_D0', 'Stack_D1', 'Square_D0', 'Square_D1'], choices=list(rs.ALL_ENVIRONMENTS), help="one or more tasks to generate episodes for")
+parser.add_argument('--robots', type=str, nargs='+', default=['Panda', 'IIWA', 'UR5e'], choices=list(rs.ALL_ROBOTS), help="one or more robots to generate episodes for")
+parser.add_argument('--grippers', type=str, nargs='+', default=['PandaGripper'], choices=list(rs.ALL_GRIPPERS), help="one or more grippers to generate episodes for")
 
-for i in range(10):
+parser.add_argument('--cameras', type=str, nargs='+', default=['agentview', 'frontview', 'robot0_eye_in_hand'], help="one or more camera views to render")
+parser.add_argument('--camera-width', type=int, default=512, help="the width (in pixels) of the camera")
+parser.add_argument('--camera-height', type=int, default=512, help="the height (in pixels) of the camera")
+
+parser.add_argument('--frames', type=int, default=60, help="the number of frames to generate per configuration")
+parser.add_argument('--parallel', type=int, default=4, help="the number of parallel processes to run")
+parser.add_argument('--output', type=str, default="/data/sim/mimicgen/test", help="output directory of the demo videos")
+
+args = parser.parse_args()
+print(args)
+
+
+def generate(task='Stack_D0', robot='Panda', gripper='PandaGripper', 
+             camera='frontview', camera_width=512, camera_height=512, 
+             frames=20, output=None, **kwargs):
+    """
+    Generate a sample video of the environment
+    """
+    env = rs.make(
+        env_name=task, 
+        robots=robot,
+        gripper_types=gripper,
+        has_renderer=False,
+        has_offscreen_renderer=True,
+        use_camera_obs=True,
+        camera_names=camera,
+        camera_widths=camera_width,
+        camera_heights=camera_height,
+    )
+    
+    env.reset()
+    images = []
+
     time_begin = time.perf_counter()
-    action = np.random.randn(env.robots[0].dof) # sample random action
-    obs, reward, done, info = env.step(action)  # take action in the environment
-    render_time = time.perf_counter() - time_begin
-    ##env.render()  # render on display
-    image = obs.get('frontview_image')
-    #image = np.flip(image, axis=0)
-    #frames.append(image)
-    print(f"frame {i}  ({render_time*1000:.2f} ms, {1.0/render_time:.1f} fps)")
-    print(f"  observations:", list(obs.keys()))
-    print(f"  image:", image.shape, image.dtype)
-    print(f"  reward:  {reward}")
-    print(f"  done:    {done}")
-    print(f"  state:   {info}")
+    
+    for i in range(frames):
+        action = np.random.randn(env.robots[0].dof) # sample random action
+        obs, reward, done, info = env.step(action)  # take action in the environment
+        ##env.render()  # render on display
+        #image = env.sim.render(mode='offscreen', width=camera_width, height=camera_height, camera_name=camera)
+        image = obs.get(f'{camera}_image') 
+        if any(x in camera for x in ['agentview', 'frontview', 'sideview']):
+            image = np.flip(image, axis=0)
+        images.append(image)
+     
+    render_time = (time.perf_counter() - time_begin) / frames
+    
+    stats = dict(
+        task = task,
+        robot = robot,
+        gripper = gripper,
+        camera = camera,
+        render_fps = 1/render_time,
+        render_time = render_time,
+        observations = list(obs.keys()),
+        image = images[-1].shape,
+        reward = reward,
+        done = done,
+        info = info
+    )
+    
+    stats_name = f"{task}-{robot}-{gripper}-{camera}" 
+    
+    if output:
+        stats['output'] = os.path.join(output, stats_name)
+    
+    print(f"\nProcess {os.getpid()} - done generating scene:\n\n{pprint.pformat(stats, indent=2, sort_dicts=False)}")
+    
+    if not output:
+        return
 
-#imageio.mimsave("robosuite.mp4", np.stack(frames), fps=15)
-print("\nIgnore:  OpenGL.raw.EGL._errors.EGLError: <exception str() failed>")
+    stats_path = os.path.join(output, stats_name + '.json')
+    image_path = os.path.join(output, stats_name + '.jpg')
+    video_path = os.path.join(output, stats_name + '.mp4')
+
+    os.makedirs(output, exist_ok=True)
+    
+    with open(stats_path, 'w') as f:
+        json.dump(stats, f, indent=2)
+        
+    imageio.imwrite(image_path, images[0])
+    imageio.mimsave(video_path, np.stack(images), fps=20)
+    
+
+for key in ['tasks', 'robots', 'grippers', 'cameras']:
+    arg = getattr(args, key)
+    if isinstance(arg, str):
+        setattr(args, key, [arg])
+
+if args.parallel:
+    pool = multiprocessing.Pool(args.parallel)
+    pool_results = []
+    
+generations = 0
+
+for task in args.tasks:
+    for robot in args.robots:
+        for gripper in args.grippers:
+            for camera in args.cameras:
+                config = dict(task=task, robot=robot, gripper=gripper, camera=camera, **vars(args))
+                if args.parallel:
+                    pool_results.append(pool.apply_async(
+                        generate, kwds=config
+                    ))
+                else:
+                    generate(**config)
+                generations += 1
+
+if args.parallel:
+    for result in pool_results:
+        result.wait()
+                      
+print(f"\nMIMICGEN SCENE GENERATIONS COMPLETED ({generations}/{generations})\n")
+print(f"   * tasks={args.tasks}  robots={args.robots}  grippers={args.grippers}  cameras={args.cameras}")
+print(f"   * images/video saved to:  {args.output}")
+print(f"   * ignore EGL errors on exit below\n")
+
+
