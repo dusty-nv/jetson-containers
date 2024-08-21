e6cfc8a9be16c1db52ffca26fa67d159e1032443
johnnynunez <johnnynuca14@gmail.com>
2024-08-21 16:48:40 +0200

    update version pytorch

--- packages/pytorch/config.py
+++ packages/pytorch/config.py
@@ -81,7 +81,7 @@ package = [
     pytorch_pip('2.0', requires='==35.*'),
     pytorch_pip('2.1', requires='>=35'),
     pytorch_pip('2.2', requires='>=35'),
-    pytorch_pip('2.3', requires='==36.*'),
+    pytorch_pip('2.4', requires='==36.*'),
 
     # JetPack 4
     pytorch_whl('1.10', 'torch-1.10.0-cp36-cp36m-linux_aarch64.whl', 'https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl', '==32.*'),
--- packages/pytorch/torchaudio/config.py
+++ packages/pytorch/torchaudio/config.py
@@ -42,7 +42,7 @@ package = [
     torchaudio('2.0.1', pytorch='2.0', requires='==35.*'),
     torchaudio('2.1.0', pytorch='2.1', requires='>=35'),
     torchaudio('2.2.2', pytorch='2.2', requires='>=35'),
-    torchaudio('2.3.0', pytorch='2.3', requires='==36.*'),
+    torchaudio('2.4.0', pytorch='2.4', requires='==36.*'),
 
     # JetPack 4
     torchaudio('0.10.0', pytorch='1.10', requires='==32.*'),
