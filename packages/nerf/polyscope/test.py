7544c283e0e69a657003d2c4d19533bd6af12d04
johnnynunez <johnnynuca14@gmail.com>
2024-08-27 16:04:41 +0200

    polyscope test running

--- packages/nerf/polyscope/test.py
+++ packages/nerf/polyscope/test.py
@@ -1,9 +1,27 @@
-#!/usr/bin/env python3
-print('testing polyscope...')
+import os
+import time
+import polyscope as ps
+import numpy as np
 
-import polyscope
-import polyscope.info
+# Start Xvfb for headless rendering
+os.system("Xvfb :99 -screen 0 1024x768x24 &")
+os.environ["DISPLAY"] = ":99"
 
-polyscope.info.print_info()
+# Wait for Xvfb to initialize
+time.sleep(3)
 
-print('polyscope OK\n')
+# Initialize polyscope
+ps.init()
+
+# Register a point cloud
+my_points = np.random.randn(100, 3)
+ps.register_point_cloud("my points", my_points)
+
+# Register a mesh
+num_verts = 100
+num_faces = 200
+verts = np.random.rand(num_verts, 3)
+faces = np.random.randint(0, num_verts, size=(num_faces, 3))
+ps.register_surface_mesh("my mesh", verts, faces, smooth_shade=True)
+
+print("Polyscope test completed successfully!")
