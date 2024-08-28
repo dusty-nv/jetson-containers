e7f3eee1d5d8170ad5064d682868b1a384dc892f
johnnynunez <johnnynuca14@gmail.com>
2024-08-29 00:06:03 +0200

    change to build pymeshlab

--- packages/nerf/pymeshlab/build.sh
+++ packages/nerf/pymeshlab/build.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-echo "Building pymeshlab ${PYMESHLAB_VERSION}"
-
-git clone --branch=v${PYMESHLAB_VERSION} --depth=1 --recursive https://github.com/cnr-isti-vclab/PyMeshLab /opt/pymeshlab
-
-cd /opt/pymeshlab/src/meshlab/resources/linux
-# remove all content in the linux folder
-rm -rf linuxdeploy linuxdeploy-plugin-appimage linuxdeploy-plugin-qt
-cp -r /tmp/PYMESHLAB/extra/* /opt/pymeshlab/src/meshlab/resources/linux
-chmod +x linuxdeploy linuxdeploy-plugin-appimage linuxdeploy-plugin-qt
-
 echo "Setting up environment for pymeshlab ${PYMESHLAB_VERSION}"
 
 echo "Building pymeshlab ${PYMESHLAB_VERSION}"
@@ -19,11 +9,21 @@ cd /opt/pymeshlab
 # Run the 1_build.sh script to build PyMeshLab
 sh scripts/Linux/1_build.sh -j$(nproc)
 
-echo "Building Python wheel for pymeshlab ${PYMESHLAB_VERSION}"
+
+echo "Deploying for pymeshlab ${PYMESHLAB_VERSION}"
+# sh scripts/Linux/2_deploy.sh
 
 # Run the make_wheel.sh script to create a Python wheel
-sh scripts/Linux/make_wheel.sh
+sh scripts/Linux/make_wheel.sh this use linuxdeploy
 
+echo "Building Python wheel for pymeshlab ${PYMESHLAB_VERSION}"
+# pip3 wheel . -w /opt/pymeshlab/wheels
 cd /
 
 pip3 install --no-cache-dir --verbose /opt/pymeshlab/wheels/pymeshlab*.whl
+
+# run here to test if it works
+python3 -c "import pymeshlab; print(pymeshlab.version())"
+
+echo "Pymeshlab OK\n"
+
--- packages/nerf/pymeshlab/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-print('testing pymeshlab...')
-
-import pymeshlab
-ms = pymeshlab.MeshSet()
-
-print('pymeshlab OK\n')
