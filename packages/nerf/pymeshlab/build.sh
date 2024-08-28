8889f97f9c3d0d964b673aad1f9796bc39c3b8c9
johnnynunez <johnnynuca14@gmail.com>
2024-08-29 00:10:48 +0200

    change to build pymeshlab

--- packages/nerf/pymeshlab/build.sh
+++ packages/nerf/pymeshlab/build.sh
@@ -9,16 +9,16 @@ cd /opt/pymeshlab
 # Run the 1_build.sh script to build PyMeshLab
 sh scripts/Linux/1_build.sh -j$(nproc)
 
-
-echo "Deploying for pymeshlab ${PYMESHLAB_VERSION}"
-# sh scripts/Linux/2_deploy.sh
-
-# Run the make_wheel.sh script to create a Python wheel
-sh scripts/Linux/make_wheel.sh this use linuxdeploy
-
-echo "Building Python wheel for pymeshlab ${PYMESHLAB_VERSION}"
-# pip3 wheel . -w /opt/pymeshlab/wheels
-cd /
+# Check if make_wheel.sh exists, and use it if available
+if [ -f scripts/Linux/make_wheel.sh ]; then
+    echo "Using make_wheel.sh to create the Python wheel"
+    sh scripts/Linux/make_wheel.sh
+else
+    echo "make_wheel.sh not found, using pip3 wheel as fallback"
+    echo "Deploying for pymeshlab ${PYMESHLAB_VERSION}"
+    sh scripts/Linux/2_deploy.sh
+    pip3 wheel . -w /opt/pymeshlab/wheels
+fi
 
 pip3 install --no-cache-dir --verbose /opt/pymeshlab/wheels/pymeshlab*.whl
 
