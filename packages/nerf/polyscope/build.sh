eb2b9e7ba166c44bff7a81308d054fa72bfc1467
johnnynunez <johnnynuca14@gmail.com>
2024-08-29 20:32:53 +0200

    .

--- packages/nerf/polyscope/build.sh
+++ packages/nerf/polyscope/build.sh
@@ -13,6 +13,6 @@ python3 setup.py --verbose bdist_wheel --dist-dir /opt
 ls /opt
 cd /
 
-pip3 install --no-cache-dir --verbose /opt/polyscope*.whl
+pip3 install --no-cache-dir --verbose /opt/wheels/polyscope*.whl
 
-twine upload --verbose /opt/polyscope*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
+twine upload --verbose /opt/wheels/polyscope*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
