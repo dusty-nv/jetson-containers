23ed3cd66637773ea9d2a90993f671d205db6ae8
johnnynunez <johnnynuca14@gmail.com>
2024-08-30 04:28:04 +0200

    some fixes

--- packages/nerf/meshlab/build.sh
+++ packages/nerf/meshlab/build.sh
@@ -3,15 +3,6 @@ set -ex
 
 echo "Building meshlab ${MESHLAB_VERSION}"
 
-git clone --branch=${MESHLAB_VERSION} --depth=1 --recursive https://github.com/cnr-isti-vclab/meshlab /opt/meshlab
-
-cd /opt/meshlab/resources/linux
-# remove all content in the linux folder
-rm -rf linuxdeploy linuxdeploy-plugin-appimage linuxdeploy-plugin-qt
-# copy /tmp/PYMESHLAB/extra/ al content in /opt/meshlab/resources/linux
-cp -r /tmp/MESHLAB/extra/* /opt/meshlab/resources/linux
-chmod +x linuxdeploy linuxdeploy-plugin-appimage linuxdeploy-plugin-qt
-
 echo "Setting up environment for meshlab ${MESHLAB_VERSION}"
 
 echo "Building meshlab ${MESHLAB_VERSION}"
