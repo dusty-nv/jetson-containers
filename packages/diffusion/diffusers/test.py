322dea257fdb794dae624d6968209ee6121b86e3
johnnynunez <johnnynuca14@gmail.com>
2024-08-30 17:59:40 +0200

    fix diffusers

--- packages/diffusion/diffusers/test.py
+++ packages/diffusion/diffusers/test.py
@@ -4,9 +4,5 @@ print('testing diffusers...')
 from diffusers import DiffusionPipeline
 import torch
 
-pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
-pipeline.to("cuda")
-# pipeline("An image of a squirrel in Picasso style").images[0]
-
 print('diffusers OK\n')
 
