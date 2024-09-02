c310ef290000a25de65a3031d18e8a82bb810483
johnnynunez <johnnynuca14@gmail.com>
2024-09-02 02:14:34 +0200

    .

--- packages/nerf/fruitNerf/build.sh
+++ packages/nerf/fruitNerf/build.sh
@@ -17,15 +17,13 @@ wget https://huggingface.co/lkeab/hq-sam/resolve/main/sam_hq_vit_h.pth
 pip3 install -e segment_anything 
 pip3 install --no-build-isolation -e GroundingDINO
 pip3 install --upgrade diffusers[torch]
-pip3 install -U opencv-python pycocotools matplotlib onnxruntime onnx ipykernel
+pip3 install -U opencv-python pycocotools matplotlib onnxruntime onnx onnxruntime-gpu ipykernel
 pip3 install -U segment-anything-hq
 
 cd /opt/fruitnerf
 pip3 install -e .
 pip3 install 'numpy<2'
-ns-install-cli
 
-wget https://zenodo.org/records/10869455/files/FruitNeRF_Real.zip -P /datasets/
 wget https://zenodo.org/records/10869455/files/FruitNeRF_Synthetic.zip -P /datasets/
 unzip /datasets/FruitNeRF_Real.zip -d /datasets/FruitNeRF_Real
 unzip /datasets/FruitNeRF_Synthetic.zip -d /datasets/FruitNeRF_Synthetic
