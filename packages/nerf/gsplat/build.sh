889cdaf420c17e6f5a65d3c6b495645c5ba521d8
johnnynunez <johnnynuca14@gmail.com>
2024-09-04 11:29:09 +0200

    fix

--- packages/nerf/gsplat/build.sh
+++ packages/nerf/gsplat/build.sh
@@ -2,8 +2,8 @@
 set -ex
 
 # Clone the repository if it doesn't exist
-git clone --branch=v${GSPLAT_VERSION} --depth=1 --recursive https://github.com/nerfstudio-project/gsplat/opt/gsplat || \
-git clone --depth=1 --recursive https://github.com/nerfstudio-project/gsplat/opt/gsplat /opt/gsplat
+git clone --branch=v${GSPLAT_VERSION} --depth=1 --recursive https://github.com/nerfstudio-project/gsplat /opt/gsplat || \
+git clone --depth=1 --recursive https://github.com/nerfstudio-project/gsplat /opt/gsplat
 
 # Navigate to the directory containing PyMeshLab's setup.py
 cd /opt/gsplat
@@ -18,3 +18,5 @@ pip3 install --no-cache-dir --verbose /opt/hloc/gsplat/gsplat*.whl
 
 cd /opt/gsplat
 pip3 install 'numpy<2'
+# Optionally upload to a repository using Twine
+twine upload --verbose /opt/gsplat/wheels/gsplat*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
