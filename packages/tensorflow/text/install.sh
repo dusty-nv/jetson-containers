b99384926b4b12695d4996bb888307573282d8cb
johnnynunez <johnnynuca14@gmail.com>
2024-09-18 07:13:08 +0200

    .

--- packages/tensorflow/text/install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-pip3 install --verbose --no-cache-dir tensorflow-text==${TENSORFLOW_TEXT_VERSION}
--- packages/tensorflow/text/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-print('testing TensorFlow Text...')
-import tensorflow_text as text
-
-print(text.case_fold_utf8(['Everything not saved will be lost.']))
-print(text.normalize_utf8(['Äffin']))
-print(text.normalize_utf8(['Äffin'], 'nfkd'))
-
-print("Tensorflow-Text OK\n")
