337c9d3bd53ac6bc38fd828d05391ec333051a82
johnnynunez <johnnynuca14@gmail.com>
2024-09-18 20:49:20 +0200

    fix

--- packages/tensorflow/build.sh
+++ packages/tensorflow/build.sh
@@ -14,9 +14,6 @@ echo "Building TensorFlow for Jetson"
 git clone --branch "v${TENSORFLOW_VERSION}" --depth=1 https://github.com/tensorflow/tensorflow.git /opt/tensorflow || \
 git clone --depth=1 https://github.com/tensorflow/tensorflow.git /opt/tensorflow 
 
-wget https://github.com/bazelbuild/bazelisk/releases/download/v1.21.0/bazelisk-linux-arm64
-chmod +x bazelisk-linux-arm64
-mv bazelisk-linux-arm64 /usr/local/bin/bazel
 cd /opt/tensorflow
 
 # Set up environment variables for the configure script
@@ -26,6 +23,8 @@ export TF_NEED_CUDA=1
 export TF_CUDA_CLANG=1
 # Set Clang path for CUDA
 export CLANG_CUDA_COMPILER_PATH=/usr/bin/clang
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
 # Set Clang path for CPU
 export CLANG_COMPILER_PATH=/usr/bin/clang
 export HERMETIC_CUDA_VERSION=12.6.0
@@ -33,9 +32,9 @@ export HERMETIC_CUDNN_VERSION=9.3.0
 export HERMETIC_CUDA_COMPUTE_CAPABILITIES=8.7
 export HERMETIC_PYTHON_VERSION="${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}"
 
-
+bazel clean --expunge
 # Build the TensorFlow pip package
-bazel build //tensorflow/tools/pip_package:wheel --repo_env=WHEEL_NAME=tensorflow --config=cuda --config=cuda_wheel --config=nonccl --copt=-Wno-gnu-offsetof-extensions
+bazel build //tensorflow/tools/pip_package:wheel --repo_env=WHEEL_NAME=tensorflow --config=cuda --config=cuda_wheel --copt=-Wno-gnu-offsetof-extensions
 
 # Upload the wheels to mirror
 twine upload --verbose /opt/tensorflow/bazel-bin/tensorflow/tools/pip_package/wheel_house/tensorflow*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
