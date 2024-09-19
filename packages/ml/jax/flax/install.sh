7ef3341d584053def8013a4c0685220deaeee776
johnnynunez <johnnynuca14@gmail.com>
2024-09-20 00:34:24 +0200

    .

--- packages/ml/jax/flax/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-if [ "$FORCE_BUILD" == "on" ]; then
-	echo "Forcing build of mamba ${MAMBA}"
-	exit 1
-fi
-
-pip3 install --no-cache-dir --verbose mamba_ssm==${MAMBA_VERSION}
