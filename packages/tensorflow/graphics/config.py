98f4287545fb25ab9ccbcf05ae12c9738d94bbe5
johnnynunez <johnnynuca14@gmail.com>
2024-09-20 00:33:08 +0200

    fix name

--- packages/tensorflow/graphics/config.py
+++ packages/tensorflow/graphics/config.py
@@ -1,29 +1,42 @@
-from jetson_containers import CUDA_ARCHITECTURES
+from jetson_containers import update_dependencies
+from packaging.version import Version
+from ..tensorflow.version import TENSORFLOW_VERSION
 
-def tensorflow_graphics(version, requires=None, default=False):
+def tensorflow_graphics(version, tensorflow=None, requires=None):
     pkg = package.copy()
-
+    
+    pkg['name'] = f"tensorflow_graphics:{version.split('-')[0]}"  # remove any -rc* suffix
+    
+    if tensorflow:
+        pkg['depends'] = update_dependencies(pkg['depends'], f"tensorflow2:{tensorflow}")
+    else:
+        tensorflow = TENSORFLOW_VERSION  
+        
     if requires:
-        pkg['requires'] = requires   
-
-    pkg['name'] = f'tensorflow_graphics:{version}'
-
+        pkg['requires'] = requires
+   
+    if len(version.split('.')) < 3:
+        version = version + '.0'
+        
     pkg['build_args'] = {
-        'CUDAARCHS': ';'.join([str(x) for x in CUDA_ARCHITECTURES]),
         'TENSORFLOW_GRAPHICS_VERSION': version,
     }
+    
+    builder = pkg.copy()
+    builder['name'] = builder['name'] + '-builder'
+    builder['build_args'] = {**builder['build_args'], 'FORCE_BUILD': 'on'}
+    
+    if not isinstance(tensorflow, Version):
+        tensorflow = Version(tensorflow)
 
-    #builder = pkg.copy()
-
-    #builder['name'] = f'tensorflow_graphics:{version}-builder'
-    #builder['build_args'] = {**pkg['build_args'], **{'FORCE_BUILD': 'on'}}
-
-    if default:
+    if tensorflow == TENSORFLOW_VERSION:
         pkg['alias'] = 'tensorflow_graphics'
-        #builder['alias'] = 'tensorflow_graphics:builder'
+        builder['alias'] = 'tensorflow_graphics:builder'
 
-    return pkg #, builder
-
+    return pkg, builder
+    
+ 
 package = [
-    tensorflow_graphics('2.0', default=True)
+    # JetPack 5/6
+    tensorflow_graphics('2.18.0', tensorflow='2.18.0', requires='==36.*'),
 ]
