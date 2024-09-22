5508752dfa0ddfdcd054129e79da6dc085a16371
johnnynunez <johnnynuca14@gmail.com>
2024-09-22 03:33:52 +0200

    fix

--- packages/tensorflow/graphics/test.py
+++ packages/tensorflow/graphics/test.py
@@ -1,17 +1,39 @@
 #!/usr/bin/env python3
 print('testing tensorflow_graphics...')
 
-import tensorflow_graphics as tfg
 import tensorflow as tf
 import numpy as np
-
-# Create a 2D image of a cube
-image = np.zeros((128, 128, 3), dtype=np.float32)
-image = tfg.image.draw_cuboid(image, [32, 32], [96, 96], [0.5, 0.5, 0.5], thickness=1)
-
-print('image shape:', image.shape)
 print('TensorFlow version: ' + str(tf.__version__))
 print('Num GPUs Available: ', len(tf.config.experimental.list_physical_devices('GPU')))
-print('tensorflow.graphics.__version__', tfg.__version__)
+import numpy as np
+import tensorflow as tf
+import trimesh
 
+import tensorflow_graphics.geometry.transformation as tfg_transformation
+from tensorflow_graphics.notebooks import threejs_visualization
+
+# Download the mesh.
+import requests
+
+url = 'https://storage.googleapis.com/tensorflow-graphics/notebooks/index/cow.obj'
+file_name = 'cow.obj'
+
+response = requests.get(url)
+
+# Save the file locally
+with open(file_name, 'wb') as file:
+    file.write(response.content)
+
+print(f"{file_name} has been downloaded.")
+
+# Load the mesh.
+mesh = trimesh.load("cow.obj")
+mesh = {"vertices": mesh.vertices, "faces": mesh.faces}
+
+# Set the axis and angle parameters.
+axis = np.array((0., 1., 0.))  # y axis.
+angle = np.array((np.pi / 4.,))  # 45 degree angle.
+# Rotate the mesh.
+mesh["vertices"] = tfg_transformation.axis_angle.rotate(mesh["vertices"], axis,
+                                                        angle).numpy()
 print('Tensorflow graphics OK\n')
