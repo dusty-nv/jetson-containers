d4efae2027087668cb7d06f73ad282ce02ded921
johnnynunez <johnnynuca14@gmail.com>
2024-09-24 19:28:47 +0200

    octo

--- packages/robots/octo/test.py
+++ packages/robots/octo/test.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
+import os
+import logging
 
-from octo.model import OctoModel
+logging.basicConfig(level=logging.DEBUG)
 
-model = OctoModel.load_pretrained("hf://rail-berkeley/octo-small-1.5")
-task = model.create_tasks(texts=["pick up the spoon"])
-action = model.sample_actions(observation, task, rng=jax.random.PRNGKey(0))
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
+
+import jax
+# https://jax.readthedocs.io/en/latest/persistent_compilation_cache.html
+jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
+jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+
+from crossformer.model.crossformer_model import CrossFormerModel
+#model = CrossFormerModel.load_pretrained("hf://rail-berkeley/crossformer")
+#print(model.get_pretty_spec())
