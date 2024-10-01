13f2fbdef5aba74609d540ba6707a96a2b67db03
Chitoku YATO <cyato@nvidia.com>
2024-10-01 11:08:34 -0700

    Fix issues with multiple CSI cameras

--- run.sh
+++ run.sh
@@ -84,11 +84,12 @@ if [[ "$csi_to_webcam_conversion" == true ]]; then
 	sudo modprobe v4l2loopback devices=${#csi_indexes[@]} exclusive_caps=1 card_label="Converted from CSI camera"
 
 	# Get all new /dev/video devices created by v4l2loopback
-	new_devices=$(v4l2-ctl --list-devices | grep -A 1 "v4l2loopback" | grep '/dev/video' | awk '{print $1}')
+	new_devices=($(v4l2-ctl --list-devices | grep -A 1 "v4l2loopback" | grep '/dev/video' | awk '{print $1}'))
+	echo "###### new_devices: ${new_devices[@]}"
 
 	# add the created v4l2loopback devices
-	if [[ -n "$new_devices" ]]; then
-		for converted_device in $new_devices; do
+	if [[ -n "${new_devices[@]}" ]]; then
+		for converted_device in ${new_devices[@]}; do
 			V4L2_DEVICES="$V4L2_DEVICES --device $converted_device "
 		done
 	else
