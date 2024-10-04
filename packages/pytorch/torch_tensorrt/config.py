bf7322ba307a2972a37400d7ed73f51bf2842c1f
Johnny <johnnync13@gmail.com>
2024-10-04 11:56:59 +0200

    Update config.py

--- packages/pytorch/torch_tensorrt/config.py
+++ packages/pytorch/torch_tensorrt/config.py
@@ -4,7 +4,7 @@ from jetson_containers import PYTHON_VERSION, JETPACK_VERSION
 if JETPACK_VERSION.major >= 5:
     TORCH_TRT_VERSION = 'v1.4.0'  # build setup has changed > 1.4.0 (still ironing it out on aarch64)
 else:
-    TORCH_TRT_VERSION = 'v2.4.0'  # compatability with TensorRT 8.2 and PyTorch 1.10
+    TORCH_TRT_VERSION = 'v2.4.0'  # compatability with PyTorch 2.4, CUDA 12.4, TensorRT 10.1, Python 3.12
     
 package['build_args'] = {
     'PYTHON_VERSION': PYTHON_VERSION,
