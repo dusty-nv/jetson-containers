e266f86019294159a3c93dff24b9866e96bdbe64
Dustin Franklin <dustinf@nvidia.com>
2024-10-04 08:26:59 -0400

    updated AutoGPTQ for JetPack 6.1

--- packages/llm/auto_gptq/build.sh
+++ packages/llm/auto_gptq/build.sh
@@ -6,12 +6,12 @@ echo "Building AutoGPTQ ${AUTOGPTQ_VERSION}"
 git clone --branch=v${AUTOGPTQ_BRANCH} --depth=1 https://github.com/PanQiWei/AutoGPTQ.git
 
 cd AutoGPTQ
-python3 setup.py --verbose bdist_wheel --dist-dir /opt
+python3 setup.py --verbose bdist_wheel --dist-dir /opt/wheels
 
 cd /
 rm -rf AutoGPTQ
 
-pip3 install --no-cache-dir --verbose /opt/auto_gptq*.whl
+pip3 install --no-cache-dir --verbose /opt/wheels/auto_gptq*.whl
 pip3 show auto-gptq && python3 -c 'import auto_gptq'
 
-twine upload --verbose /opt/auto_gptq*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
+twine upload --verbose /opt/wheels/auto_gptq*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
