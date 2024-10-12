99fee1e0f5bd4d940c1b80e646eb0f7b4f6f6c01
johnnynunez <johnnynuca14@gmail.com>
2024-10-12 15:50:19 +0200

    .

--- packages/ml/jax/build.sh
+++ packages/ml/jax/build.sh
@@ -35,7 +35,7 @@ twine upload --verbose /opt/wheels/jax-*.whl || echo "failed to upload wheel to 
 
 # Install them into the container
 cd /opt/wheels/
-pip3 install --verbose --no-cache-dir jaxlib*.whl jax_cuda12_plugin*.whl opt_einsum*.whl
-pip3 install --verbose --no-cache-dir --no-dependencies jax*.whl
+pip3 install --verbose --no-cache-dir jaxlib*.whl jax_cuda12_plugin*.whl opt_einsum
+pip3 install --verbose --no-cache-dir --no-dependencies jax*.whl 
 
 cd /opt/jax
