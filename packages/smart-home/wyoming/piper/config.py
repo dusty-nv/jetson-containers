033c6b3ae7f33a788da9f2e1ec1ebdc3409841ca
Mieszko Syty <mieszko@ms1design.pl>
2024-11-20 12:59:38 +0100

    wyoming-piper builder

--- packages/smart-home/wyoming/piper/config.py
+++ packages/smart-home/wyoming/piper/config.py
@@ -1,21 +1,39 @@
 from jetson_containers import handle_text_request
 
+from packaging.version import Version
 
-def create_package(version, default=False) -> list:
+
+def create_package(version, branch=None, default=False) -> list:
     pkg = package.copy()
-    url = 'https://raw.githubusercontent.com/rhasspy/wyoming-piper/master/wyoming_piper/VERSION'
-    wanted_version = handle_text_request(url) if version == 'latest' else version
-    pkg['name'] = f'wyoming-piper:{version}'
+
+    if not branch:
+        branch = f'v{version}'
+
+    wanted_version = handle_text_request(f'https://raw.githubusercontent.com/rhasspy/wyoming-piper/{branch}/wyoming_piper/VERSION')
+
+    # FIXME: wyoming-piper on branch v1.5.2 has incorrect version set in VERSION file.
+    if Version(version) != Version(wanted_version):
+        print(f"wyoming-piper versions mismatch ({version} vs {wanted_version}) resolved version: {wanted_version}")
+        wanted_version = version
+
+    pkg['name'] = f'wyoming-piper:{wanted_version}'
 
     pkg['build_args'] = {
         'WYOMING_PIPER_VERSION': wanted_version,
+        'WYOMING_PIPER_BRANCH': branch,
     }
+
+    builder = pkg.copy()
+    builder['name'] = f'wyoming-piper:{wanted_version}-builder'
+    builder['build_args'] = {**pkg['build_args'], **{'FORCE_BUILD': 'on'}}
 
     if default:
         pkg['alias'] = 'wyoming-piper'
+        builder['alias'] = 'wyoming-piper:builder'
 
-    return pkg
+    return pkg, builder
 
 package = [
-    create_package("master", default=True),
+    create_package("1.5.2", default=True),
+    create_package("1.5.0", default=False),
 ]
