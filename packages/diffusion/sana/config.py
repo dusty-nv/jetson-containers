51086210a77d8d16bb0b176835970c364a0219b1
johnnynunez <johnnynuca14@gmail.com>
2024-11-26 10:15:51 +0100

    SANA

---
+++ packages/diffusion/sana/config.py
@@ -0,0 +1,29 @@
+from jetson_containers import CUDA_ARCHITECTURES
+
+def sana(version, requires=None, default=False):
+    pkg = package.copy()
+
+    if requires:
+        pkg['requires'] = requires   
+
+    pkg['name'] = f'sana:{version}'
+
+    pkg['build_args'] = {
+        'CUDAARCHS': ';'.join([str(x) for x in CUDA_ARCHITECTURES]),
+        'SANA_VERSION': version,
+    }
+
+    builder = pkg.copy()
+
+    builder['name'] = f'sana:{version}-builder'
+    builder['build_args'] = {**pkg['build_args'], **{'FORCE_BUILD': 'on'}}
+
+    if default:
+        pkg['alias'] = 'sana'
+        builder['alias'] = 'sana:builder'
+
+    return pkg, builder
+
+package = [
+    sana('0.1', default=True)
+]
