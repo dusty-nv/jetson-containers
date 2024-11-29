56fc09ffc6f79b87927b8eaa7e14cf95a1af3b57
Mieszko Syty <mieszko@ms1design.pl>
2024-11-29 18:40:35 +0100

    extend transformers config.py

--- packages/llm/transformers/config.py
+++ packages/llm/transformers/config.py
@@ -1,10 +1,11 @@
 from jetson_containers import handle_json_request, github_latest_tag
+from packaging.version import parse
 
-def transformers(version, source=None, requires='>=34', default=False):
+
+def _transformers(version, codename=None, source=None, requires='>=34', default=False):
     pkg = package.copy()
 
-    if version:
-        pkg['name'] += f':{version}'
+    pkg['name'] = f'{package["name"]}:{codename or version}'
 
     pkg['build_args'] = {
         'TRANSFORMERS_PACKAGE': source,
@@ -15,43 +16,46 @@ def transformers(version, source=None, requires='>=34', default=False):
         pkg['requires'] = requires
 
     if default:
-        pkg['alias'] = f'transformers'
+        pkg['alias'] = package['name']
 
     return pkg
 
 
 def transformers_pypi(version, **kwargs):
-    releases = f'https://pypi.org/pypi/transformers/json'
-
     if version == 'latest':
-        data = handle_json_request(releases)
-        version = data['releases'].keys()[-1]
+        data = handle_json_request('https://pypi.org/pypi/transformers/json')
+        # Sort the version keys using `parse` for proper semantic version sorting
+        sorted_versions = sorted(data['releases'].keys(), key=parse)
+        version = sorted_versions[-1]
 
-        print(f'releases: {data['releases'].keys()}')
-        print(f'latest: {version}')
-
-    return transformers(
+    return _transformers(
         version,
-        source=f'transformers',
+        source=f'transformers=={version}',
         **kwargs
     )
 
 
-def transformers_git(version, repo='huggingface/transformers', branch='main', **kwargs):
+def transformers_git(version, repo='huggingface/transformers', branch=None, **kwargs):
     version = github_latest_tag(repo) if version == 'latest' else version
-    return transformers(
+
+    if version.startswith('v'):
+        version = version[1:]
+
+    if branch is None:
+        branch = f'v{version}'
+
+    return _transformers(
         version,
-        source=f'git+https://github.com/{repo}',
+        source=f'git+https://github.com/{repo}@{branch}',
         **kwargs
     )
-
 
 # 11/3/23 - removing 'bitsandbytes' and 'auto_gptq' due to circular dependency and increased load times of
 # anything using transformers if you want to use load_in_8bit/load_in_4bit or AutoGPTQ quantization 
 # built-into transformers, use the 'bitsandbytes' or 'auto_gptq' containers directly instead of transformers container
 package = [
-    transformers_pypi('latest', default=True),                  # will always resolve to the latest pypi version
-    transformers_pypi('4.35.0'),                                # For compatibility with AutoAWQ
-    transformers_git('git'),
-    transformers_git('nvgpt', repo='ertkonuk/transformers'),
+    transformers_pypi('latest', default=True),                                  # will always resolve to the latest pypi version
+    transformers_pypi('4.41.2'),                                                # For compatibility with AutoAWQ
+    transformers_git('latest', codename='git'),                                                 # will always resolve to the latest git version from huggingface/transformers
+    transformers_git('latest', codename='nvgpt', repo='ertkonuk/transformers'),     # will always resolve to the latest git version from ertkonuk/transformers
 ]
