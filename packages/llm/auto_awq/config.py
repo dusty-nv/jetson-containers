2793e0fa8e07fb30b7df29ac49c37fc3f8b7ba37
Mieszko Syty <mieszko@ms1design.pl>
2024-11-29 13:42:37 +0100

    Fix issues with auto_awq, fix transformers dependency version

--- packages/llm/auto_awq/config.py
+++ packages/llm/auto_awq/config.py
@@ -4,18 +4,25 @@ def AutoAWQ(version, kernels_version, default=False):
     pkg = package.copy()
 
     pkg['name'] = f'auto_awq:{version}'
-
-    if default:
-        pkg['alias'] = 'auto_awq'
-    
     pkg['build_args'] = {
         'AUTOAWQ_VERSION': version,
         'AUTOAWQ_KERNELS_VERSION': kernels_version,
-        'AUTOAWQ_CUDA_ARCH': ','.join([str(x) for x in CUDA_ARCHITECTURES])
+        'COMPUTE_CAPABILITIES': ','.join([str(x) for x in CUDA_ARCHITECTURES]),
     }
+
+    builder = pkg.copy()
     
-    return pkg
+    builder['name'] = f'auto_awq:{version}-builder'
+    builder['build_args'] = {**pkg['build_args'], **{'FORCE_BUILD': 'on'}}
+
+    if default:
+        pkg['alias'] = 'auto_awq'
+        builder['alias'] = 'auto_awq:builder'
+    
+    return pkg, builder
 
 package = [
-    AutoAWQ('0.2.4', '0.0.6', default=True),
+    AutoAWQ('0.2.7.post2', '0.0.9', default=False),
+    AutoAWQ('0.2.6', '0.0.8', default=True),
+    AutoAWQ('0.2.4', '0.0.6', default=False),
 ]
