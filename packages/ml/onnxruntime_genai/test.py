a169d428fcd4398cacaf83545e01df93e4121ed3
johnnynunez <johnnynuca14@gmail.com>
2025-01-03 16:26:44 +0100

    fix

--- packages/ml/onnxruntime_genai/test.py
+++ packages/ml/onnxruntime_genai/test.py
@@ -9,8 +9,8 @@ import requests
 import numpy as np
 
 from packaging.version import Version
-
 import onnxruntime as ort
+
 print('onnxruntime version: ' + str(ort.__version__))
 
 ort_version = Version(ort.__version__)
@@ -18,36 +18,39 @@ ort_version = Version(ort.__version__)
 if ort_version > Version('1.10'):
     print(ort.get_build_info())
 
-# verify execution providers
+# Verify execution providers
 providers = ort.get_available_providers()
+print(f'Execution providers: {providers}')
 
-print(f'execution providers:  {providers}')
-
-print('testing onnxruntime_genai...')
+print('Testing onnxruntime_genai...')
 import onnxruntime_genai as og
 print('onnxruntime_genai version: ' + str(og.__version__))
 
+# Execute Hugging Face CLI
+ios.system("huggingface-cli download microsoft/Phi-3-mini-4k-instruct-onnx --include cpu_and_mobile/cpu-int4-rtn-block-32-acc-level-4/* --local-dir .")
+
+# Initialize model and tokenizer
 model = og.Model('cpu_and_mobile/cpu-int4-rtn-block-32-acc-level-4')
 tokenizer = og.Tokenizer(model)
 tokenizer_stream = tokenizer.create_stream()
 
-# Set the max length to something sensible by default,
-# since otherwise it will be set to the entire context length
-search_options = {}
-search_options['max_length'] = 2048
-search_options['batch_size'] = 1
+# Set default search options
+search_options = {
+    'max_length': 2048,
+    'batch_size': 1
+}
 
 chat_template = '<|user|>\n{input} <|end|>\n<|assistant|>'
 
 text = "Input: Hello, I'm in jetson-containers"
 if not text:
     print("Error, input cannot be empty")
-    exit
+    exit()
 
 prompt = f'{chat_template.format(input=text)}'
-
 input_tokens = tokenizer.encode(prompt)
 
+# Initialize generator
 params = og.GeneratorParams(model)
 params.set_search_options(**search_options)
 generator = og.Generator(model, params)
@@ -58,11 +61,10 @@ try:
     generator.append_tokens(input_tokens)
     while not generator.is_done():
         generator.generate_next_token()
-
         new_token = generator.get_next_tokens()[0]
         print(tokenizer_stream.decode(new_token), end='', flush=True)
 except KeyboardInterrupt:
-    print("  --control+c pressed, aborting generation--")
+    print("  --Control+C pressed, aborting generation--")
 
 print()
 del generator
