a5784677ceabbeb02ecd9f3ac1d5505d6f15f00f
Johnny <johnnynuca14@gmail.com>
2025-01-10 16:12:02 +0100

    update test transformer_engine

--- packages/ml/transformer-engine/test.py
+++ packages/ml/transformer-engine/test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 print ('Transformer Engine Test')
-import torch
+"""import torch
 import transformer_engine.pytorch as te
 from transformer_engine.common import recipe
 
@@ -21,6 +21,30 @@ with te.fp8_autocast(enabled=True, fp8_recipe=fp8_recipe):
     out = model(inp)
 
 loss = out.sum()
+loss.backward()"""
+
+# AMPERE not has fp8 support
+#!/usr/bin/env python3
+print('Transformer Engine Test (FP16 with Ampere)')
+import torch
+import transformer_engine.pytorch as te
+
+# Set dimensions.
+in_features = 768
+out_features = 3072
+hidden_size = 2048
+
+# Initialize model and inputs on the GPU.
+model = te.Linear(in_features, out_features, bias=True).cuda()
+inp = torch.randn(hidden_size, in_features, device="cuda")
+
+# Enable autocasting for the forward pass using FP16
+with torch.cuda.amp.autocast():
+    out = model(inp)
+
+loss = out.sum()
 loss.backward()
+
+print('Transformer Engine with FP16 OK\n')
 
 print('Transformer Engine OK\n')
