17b8b394207d323db52bd03b268a524760928221
Johnny <johnnynuca14@gmail.com>
2025-01-10 16:58:31 +0100

    update test transformer_engine

--- packages/ml/transformer-engine/test.py
+++ packages/ml/transformer-engine/test.py
@@ -15,7 +15,7 @@ model = te.Linear(in_features, out_features, bias=True).cuda()
 inp = torch.randn(hidden_size, in_features, device="cuda")
 
 # --- FP8 Attempt ---
-try:
+"""try:
     print("Trying FP8...")
     # Create an FP8 recipe.
     fp8_recipe = recipe.DelayedScaling(margin=0, fp8_format=recipe.Format.E4M3)
@@ -30,36 +30,36 @@ try:
 except RuntimeError as e:
     print(f"FP8 Failed: {e} or not supported (Ampere)")
 
-    # --- FP16 Attempt ---
+    # --- FP16 Attempt ---"""
+try:
+    print("Trying FP16...")
+    # Reset gradients
+    model.zero_grad()
+
+    # Enable autocasting for the forward pass using FP16
+    with torch.cuda.amp.autocast():
+        out = model(inp)
+    loss = out.sum()
+    loss.backward()
+    print("FP16 Successful!\n")
+
+except RuntimeError as e:
+    print(f"FP16 Failed: {e}")
+
+    # --- FP32 Attempt ---
     try:
-        print("Trying FP16...")
+        print("Trying FP32...")
         # Reset gradients
         model.zero_grad()
 
-        # Enable autocasting for the forward pass using FP16
-        with torch.cuda.amp.autocast():
-            out = model(inp)
+        # Run forward and backward pass in FP32
+        out = model(inp)
         loss = out.sum()
         loss.backward()
-        print("FP16 Successful!\n")
+        print("FP32 Successful!\n")
 
-    except RuntimeError as e:
-        print(f"FP16 Failed: {e}")
-
-        # --- FP32 Attempt ---
-        try:
-            print("Trying FP32...")
-            # Reset gradients
-            model.zero_grad()
-
-            # Run forward and backward pass in FP32
-            out = model(inp)
-            loss = out.sum()
-            loss.backward()
-            print("FP32 Successful!\n")
-
-        except Exception as e:
-            print(f"FP32 Failed: {e}")
-            print("All attempts failed.\n")
+    except Exception as e:
+        print(f"FP32 Failed: {e}")
+        print("All attempts failed.\n")
 
 print('Transformer Engine OK\n')
