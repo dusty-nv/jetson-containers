13e0a49bd6f220e2c96425c1638791b658562e7d
Dustin Franklin <dustinf@nvidia.com>
2025-01-30 16:56:45 -0500

    updated paths

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # launcher for jetson_containers/build.py (see docs/build.md)
-ROOT="$(dirname "$(readlink -f "$0")")"
+ROOT="$(dirname $(dirname "$(readlink -f "$0")") )"
 PYTHONPATH="$PYTHONPATH:$ROOT" python3 -m jetson_containers.build "$@"
