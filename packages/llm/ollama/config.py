e6f1087b0de1c8f3378342873c285e12cd185126
Dustin Franklin <dustinf@nvidia.com>
2025-01-30 22:48:31 -0500

    Revert "Copy nv_tegra_release to let Ollama installer install GPU-enabled one"

--- packages/llm/ollama/config.py
+++ packages/llm/ollama/config.py
@@ -1,4 +1,36 @@
-import shutil
 
-shutil.copyfile("/etc/nv_tegra_release", "./packages/llm/ollama/nv_tegra_release")
-print("##### /etc/nv_tegra_release file copied!")
+from jetson_containers import CUDA_ARCHITECTURES, CUDA_VERSION, JETPACK_VERSION
+
+def ollama(version, golang='1.22.8', cmake='3.22.1', branch=None, requires=None, default=False):
+    pkg = package.copy()
+
+    if not branch:
+        branch = version
+
+    pkg['name'] = f'ollama:{version}'
+
+    if branch[0].isnumeric():
+        branch = 'v' + branch
+        
+    pkg['build_args'] = {
+        'OLLAMA_REPO': 'ollama/ollama',
+        'OLLAMA_BRANCH': branch,
+        'GOLANG_VERSION': golang,
+        'CMAKE_VERSION': cmake,
+        'JETPACK_VERSION': str(JETPACK_VERSION),
+        'CUDA_VERSION_MAJOR' : CUDA_VERSION.major,
+        'CMAKE_CUDA_ARCHITECTURES': ';'.join([str(x) for x in CUDA_ARCHITECTURES]),
+    }   
+     
+    if default:
+        pkg['alias'] = 'ollama'
+
+    return pkg
+    
+package = [
+    ollama('main'),
+    ollama('0.4.0'),
+    ollama('0.5.1'),
+    ollama('0.5.5', branch='0.5.5-rc0'),
+    ollama('0.5.7', default=True),
+]
