44db6996eb53a16e77345e017e2f5be74b94cbf2
Johnny <johnnynuca14@gmail.com>
2025-02-04 21:57:28 +0100

    Update config.py

--- packages/ml/jax/config.py
+++ packages/ml/jax/config.py
@@ -43,6 +43,7 @@ package = [
     jax('0.4.30', requires='>=35'),
     jax('0.4.32', requires='>=35'),
     jax('0.4.35', requires='>=35'),
-    jax('0.4.38', requires='>=35', default=True), # It works from jetpack 5 11.8 Cuda & 8.6 Cudnn
+    jax('0.4.38', requires='>=35'), # It works from jetpack 5 11.8 Cuda & 8.6 Cudnn
+    jax('0.5.1', requires='>=36', default=True), # Blackwell
     
 ]
