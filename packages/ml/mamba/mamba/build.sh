9e625fedb82eaae4b88c31cbc5a244b7ed4b7f33
johnnynunez <johnnynuca14@gmail.com>
2025-02-09 18:18:25 +0100

    fix triton

--- packages/ml/mamba/mamba/build.sh
+++ packages/ml/mamba/mamba/build.sh
@@ -11,12 +11,16 @@ git apply /tmp/MAMBA/patch.diff
 git diff
 git status
 
+sed -i '/torch/d' pyproject.toml
+sed -i '/triton/d' pyproject.toml
+
 pip3 install lm-eval
 pip3 install --upgrade setuptools einops
+pip3 install --no-cache-dir --verbose triton
 export MAX_JOBS=$(nproc)
 export MAMBA_FORCE_BUILD=TRUE
 export MAMBA_SKIP_CUDA_BUILD=FALSE
-pip3 wheel --no-build-isolation --wheel-dir=/opt/mamba/wheels .
+pip3 wheel --no-build-isolation --wheel-dir=/opt/mamba/wheels . --verbose
 pip3 install --no-cache-dir --verbose /opt/mamba/wheels/mamba_ssm*.whl
 
 cd /opt/mamba
