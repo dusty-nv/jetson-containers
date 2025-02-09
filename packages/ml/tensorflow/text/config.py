f7314ecdd072b6b1af5bb972750ff1b96ec309a8
Johnny <johnnynuca14@gmail.com>
2025-02-09 12:10:53 +0100

    Fix tensorflow (#835)
    Fix jax and tensorflow

--- packages/ml/tensorflow/text/config.py
+++ packages/ml/tensorflow/text/config.py
@@ -41,4 +41,5 @@ def tensorflow_text(version, tensorflow=None, requires=None):
 package = [
     # JetPack 5/6
     tensorflow_text('2.18.0', tensorflow='2.18.0', requires='==36.*'),
+    tensorflow_text('2.19.0', tensorflow='2.19.0', requires='==36.*'),
 ]
