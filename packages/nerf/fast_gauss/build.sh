d7b5a878603c7725ad35bcd7102bae2ff090ab80
johnnynunez <johnnynuca14@gmail.com>
2025-02-24 15:56:55 +0100

    test

--- packages/nerf/fast_gauss/build.sh
+++ packages/nerf/fast_gauss/build.sh
@@ -9,11 +9,11 @@ git clone --depth=1 --recursive https://github.com/dendenxu/fast-gaussian-raster
 cd /opt/fast_gauss
 
 MAX_JOBS=$(nproc) \
-pip3 wheel . -w /opt/fast_gauss/wheels --verbose
+pip3 wheel . -w /opt/wheels --verbose
 
-pip3 install --no-cache-dir --verbose /opt/fast_gauss/wheels/fast_gauss*.whl
+pip3 install --no-cache-dir --verbose /opt/wheels/fast_gauss*.whl
 
 cd /opt/fast_gauss
 
 # Optionally upload to a repository using Twine
-twine upload --verbose /opt/fast_gauss/wheels/fast_gauss*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
+twine upload --verbose /opt/wheels/fast_gauss*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
