be5c8304a580851ec7943dce57fdd984fb59c001
Johnny <johnnynuca14@gmail.com>
2025-02-26 02:55:26 +0100

    fix sglang

--- packages/llm/sglang/build.sh
+++ packages/llm/sglang/build.sh
@@ -8,6 +8,7 @@ git clone --branch=v${SGLANG_VERSION} --recursive --depth=1 https://github.com/f
 git clone --recursive --depth=1 https://github.com/flashinfer-ai/flashinfer /opt/flashinfer
 cd /opt/flashinfer
 
+export MAX_JOBS=$(nproc)
 python3 setup.py --verbose bdist_wheel --dist-dir /opt/flashinfer/wheels/ && \
 pip3 install --verbose /opt/flashinfer/wheels/flashinfer_python-*.whl
 
@@ -22,14 +23,11 @@ sed -i '/sgl-kernel>=0.0.2.post14/d' python/pyproject.toml && \
 sed -i '/flashinfer==0.1.6/d' python/pyproject.toml && \
 sed -i '/xgrammar>=0.1.10/d' python/pyproject.toml && \
 cd sgl-kernel && \
-python3 setup.py bdist_wheel && pip3 install ./dist/*.whl && \
+python3 setup.py --verbose bdist_wheel --dist-dir /opt/sglang/wheels/ && pip3 install /opt/sglang/wheels/*.whl && \
 cd .. && \
 sed -i '/return min(memory_values)/s/.*/        return None/' python/sglang_backup/srt/utils.py  && \
 sed -i '/if not memory_values:/,+1d' python/sglang_backup/srt/utils.py && \
 pip3 install -e "python[all]"
-
-pip3 wheel -v --wheel-dir=/opt/sglang/wheels .
-pip3 install --no-cache-dir --verbose /opt/sglang/wheels/sglang*.whl
 
 cd /opt/sglang
 
