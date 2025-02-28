2858502215c86188ec105697eece82f1b4efb2a3
Dustin Franklin <dustinf@nvidia.com>
2025-02-28 06:01:14 -0500

    OpenCV patches for 24.04

--- packages/opencv/install_deps.sh
+++ packages/opencv/install_deps.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 # this script installs build dependencies for compiling OpenCV
-
 set -e -x
 
 ARCH=$(uname -i)
-echo "ARCH:  $ARCH"
-	 
-if [ $(lsb_release --codename --short) != "jammy" ]; then
+DISTRO=$(lsb_release -rs)
+
+echo "ARCH:   $ARCH"
+echo "DISTRO: $DISTRO"
+
+if [[ $DISTRO == "18.04" || $DISTRO == "20.04" ]]; then
 	EXTRAS="libavresample-dev libdc1394-22-dev"
 fi
-	 
+
+if [[ $DISTRO == "24.04" ]]; then
+  EXTRAS="libtbbmalloc2 libtbb-dev $EXTRAS"
+else
+  EXTRAS="libtbb2 libtbb2-dev liblapacke-dev $EXTRAS"
+fi
+
 apt-get update
 apt-get install -y --no-install-recommends \
         build-essential \
-	   gfortran \
+        gfortran \
         cmake \
         git \
-	   file \
-	   tar \
+        file \
+        tar \
         libatlas-base-dev \
         libavcodec-dev \
         libavformat-dev \
@@ -32,26 +40,23 @@ apt-get install -y --no-install-recommends \
         libjpeg8-dev \
         libjpeg-turbo8-dev \
         liblapack-dev \
-        liblapacke-dev \
         libopenblas-dev \
         libpng-dev \
         libpostproc-dev \
         libswscale-dev \
-        libtbb-dev \
-        libtbb2 \
         libtesseract-dev \
         libtiff-dev \
         libv4l-dev \
         libxine2-dev \
         libxvidcore-dev \
         libx264-dev \
-	   libgtkglext1 \
-	   libgtkglext1-dev \
+        libgtkglext1 \
+        libgtkglext1-dev \
         pkg-config \
         qv4l2 \
         v4l-utils \
         zlib1g-dev \
-	   $EXTRAS
+        $EXTRAS
 
 # on x86, the python dev packages are already installed in the NGC containers under conda
 # and installing them again from apt messes up their proper detection, so skip doing that
@@ -59,11 +64,14 @@ apt-get install -y --no-install-recommends \
 if [ $ARCH != "x86_64" ]; then
 	echo "detected $ARCH, installing python3 dev packages..."
 
+  if [[ $DISTRO != "24.04" ]]; then
+    DIST_EXTRAS="python3-distutils python3-setuptools"
+  fi
+
 	apt-get install -y --no-install-recommends \
 		python3-pip \
 		python3-dev \
-		python3-distutils \
-		python3-setuptools
+    $DIST_EXTRAS
 
 	python3 -c 'import numpy; print("NumPy version before installation:", numpy.__version__)' 2>/dev/null
 
