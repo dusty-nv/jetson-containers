10850262e0e3b8df7752fcab6eb5a4158e2ad0bf
johnnynunez <johnnynuca14@gmail.com>
2025-03-06 19:29:00 +0100

    refactor

--- packages/nerf/manifold/test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-print('testing manifold...')
-
-import manifold3d
-
-print('manifold OK\n')
--- packages/nerf/vhacdx/test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-print('testing vhacdx...')
-
-import vhacdx
-
-print('vhacdx OK\n')
