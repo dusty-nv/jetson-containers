d5e311ae3c631cd2b4a69e587f91c14a12183a75
johnnynunez <johnnynuca14@gmail.com>
2025-03-06 19:10:53 +0100

    fix

--- packages/nerf/vhacdx/config.py
+++ packages/nerf/vhacdx/config.py
@@ -25,6 +25,5 @@ def vhacdx(version, requires=None, default=False):
     return pkg, builder
 
 package = [
-    vhacdx('0.0.8.post1'),
-    vhacdx('0.0.9.post1', default=True)
+    vhacdx('0.0.8.post2', default=True)
 ]
