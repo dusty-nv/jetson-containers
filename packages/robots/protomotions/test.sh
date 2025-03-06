c9b770608022b12e3245c5cac415040633d57fe0
Johnny <johnnynuca14@gmail.com>
2025-03-06 16:59:57 +0100

    fix protomotions (#912)
    * fix
    
    * fix

--- packages/robots/protomotions/test.sh
+++ packages/robots/protomotions/test.sh
@@ -6,6 +6,13 @@ import genesis as gs
 import torch
 from protomotions.simulator.genesis.config import GenesisSimulatorConfig, GenesisSimParams
 from protomotions.simulator.genesis.simulator import GenesisSimulator
+from protomotions.simulator.base_simulator.config import (
+    RobotConfig,
+    RobotAssetConfig,
+    InitState,
+    ControlConfig,
+    ControlType,
+)
 
 
 print('ProtoMotions OK\\n')
