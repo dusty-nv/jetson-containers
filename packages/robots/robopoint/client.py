abea6f1b96a81dc596623c4c3423ee2af68d5fba
Manuel Schweiger <schweigermanuel@icloud.com>
2025-03-08 07:14:19 +0100

    adapt request
    Signed-off-by: Manuel Schweiger <schweigermanuel@icloud.com>

--- packages/robots/robopoint/client.py
+++ packages/robots/robopoint/client.py
@@ -13,22 +13,26 @@ parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFo
 parser.add_argument('--server', type=str, default='http://0.0.0.0:7860/')
 parser.add_argument('--model', type=str, default='robopoint-v1-vicuna-v1.5-13b')
 parser.add_argument('--image', type=str, default=os.path.join(os.path.dirname(__file__), 'examples/sink.jpg'))
+parser.add_argument('--request', type=str, required=True)
 
 args = parser.parse_args()
 
 print(f"{args}\n")
 print(f"Connecting to RoboPoint server @ {args.server}")
 
+#connect to the server
 client = Client(args.server)
 
+#load the model list
 result = client.predict(
     api_name="/load_demo_refresh_model_list"
 )
 
 print(f"Loading {args.image}\n")
 
+#load the request text from the cli
 result = client.predict(
-    text="<<<request>>>",
+    text=args.request,
     image=handle_file(args.image),
     image_process_mode="Pad",
     api_name="/add_text_1"
@@ -37,6 +41,7 @@ result = client.predict(
 print(f"Generating output with {args.model}\n")
 time_begin = time.perf_counter()
 
+# generate the 2D action point prediction
 result = client.predict(
     model_selector=args.model,
     temperature=1,
