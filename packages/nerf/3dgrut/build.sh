37546adaafc3b56c002c995171910f477e26bb9e
johnnynunez <johnnynuca14@gmail.com>
2025-03-22 09:26:26 +0100

    fix

--- packages/nerf/3dgrut/build.sh
+++ packages/nerf/3dgrut/build.sh
@@ -13,12 +13,18 @@ cd /opt/3DGRUT
 RUN update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 110 && \
     update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 110
 
+sed -i '/^--find-links/d' requirements.txt
+sed -i 's/kaolin==0.17.0/kaolin>=0.17.0/' requirements.txt
+
+pip install -r requirements.txt
+pip install -e .
+
 MAX_JOBS=$(nproc) \
 pip3 wheel . -w /opt/3DGRUT/wheels --verbose
 
-pip3 install /opt/3DGRUT/wheels/3DGRUT*.whl
+# pip3 install /opt/3DGRUT/wheels/threedgrut-*.whl
 
 cd /opt/3DGRUT
 
 # Optionally upload to a repository using Twine
-twine upload --verbose /opt/3DGRUT/wheels/3DGRUT*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
+twine upload --verbose /opt/3DGRUT/wheels/threedgrut-*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
---
+++ packages/nerf/3dgrut/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python3 - <<EOF
+print('testing 3DGRUT...')
+from threedgrut.render import Renderer
+
+print('3DGRUT OK\\n')
+EOF
