4f7b046125c387bd663fcf270fa95672140dbcf4
Mister K <678459+kairin@users.noreply.github.com>
2025-04-02 06:13:18 +0800

    Update build.sh

--- buildx/build.sh
+++ buildx/build.sh
@@ -3,8 +3,14 @@
 # Docker Hub username
 DOCKER_USERNAME=kairin
 
-# Image name
+# Base image name
 IMAGE_NAME=my-custom-image
+
+# Get the current date and time formatted as YYYYMMDD-HHMMSS
+CURRENT_DATE_TIME=$(date +"%Y%m%d-%H%M%S")
+
+# Create the tag with the current date and time and append number 1
+TAG="${DOCKER_USERNAME}/001:${CURRENT_DATE_TIME}-1"
 
 # Remove any existing builder instance
 docker buildx rm mybuilder || true
@@ -14,8 +20,10 @@ docker buildx create --name mybuilder --use
 
 # Build the Docker image using buildx and push to Docker Hub
 docker buildx build --platform linux/amd64,linux/arm64 \
-    -t $DOCKER_USERNAME/$IMAGE_NAME:latest \
+    -t $TAG \
     --push .
 
 # Remove the builder instance
 docker buildx rm mybuilder
+
+echo "Docker image tagged and pushed as $TAG"
