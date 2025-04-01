b65356b51c34d3b3b9e92d598402fe617d7d626c
Mister K <678459+kairin@users.noreply.github.com>
2025-04-02 05:13:30 +0800

    Create build.sh

---
+++ buildx/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set the builder instance
+docker buildx create --name mybuilder --use
+
+# Build the Docker image using buildx
+docker buildx build --platform linux/amd64,linux/arm64 \
+    -t my-custom-image:latest .
+
+# Remove the builder instance
+docker buildx rm mybuilder
