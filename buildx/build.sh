ef97757e1ca6b7d431180a9f1d8084b725680552
Mister K <678459+kairin@users.noreply.github.com>
2025-04-02 06:01:58 +0800

    Update build.sh

--- buildx/build.sh
+++ buildx/build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# Docker Hub username
+DOCKER_USERNAME=kairin
+
+# Image name
+IMAGE_NAME=my-custom-image
+
 # Remove any existing builder instance
 docker buildx rm mybuilder || true
 
 # Set the builder instance
 docker buildx create --name mybuilder --use
 
-# Build the Docker image using buildx
+# Build the Docker image using buildx and push to Docker Hub
 docker buildx build --platform linux/amd64,linux/arm64 \
-    -t my-custom-image:latest .
+    -t $DOCKER_USERNAME/$IMAGE_NAME:latest \
+    --push .
 
 # Remove the builder instance
 docker buildx rm mybuilder
