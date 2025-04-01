a702eb0927cc70bb852ccd3aca1e151eceb83222
Mister K <678459+kairin@users.noreply.github.com>
2025-04-02 05:20:49 +0800

    Update build.sh

--- buildx/build.sh
+++ buildx/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Remove any existing builder instance
+docker buildx rm mybuilder || true
+
 # Set the builder instance
 docker buildx create --name mybuilder --use
 
 # Change permissions of the installer file
-chmod +x /media/kkk/Apps/qt-online-installer-linux-arm64-4.9.0.run
+chmod +x /media/kkk/jetc/buildx/qt-online-installer-linux-arm64-4.9.0.run
 
 # Build the Docker image using buildx
 docker buildx build --platform linux/amd64,linux/arm64 \
