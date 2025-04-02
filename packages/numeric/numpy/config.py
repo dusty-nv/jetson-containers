fad8474a3a70a2ab175aa063c7f23033a2c11dff
Dustin Franklin <dustinf@nvidia.com>
2025-04-02 09:41:55 -0400

    add NUMPY_VERSION_MAJOR to env

--- packages/numeric/numpy/config.py
+++ packages/numeric/numpy/config.py
@@ -2,10 +2,14 @@
 from jetson_containers import CUDA_VERSION
 from packaging.version import Version
 
-NUMPY_PACKAGE = 'numpy<2' \
-  if CUDA_VERSION <= Version('12.6') \
-    else 'numpy'
+if CUDA_VERSION <= Version('12.6'):
+  NUMPY_PACKAGE = 'numpy<2'
+  NUMPY_VERSION_MAJOR = 1
+else:
+  NUMPY_PACKAGE = 'numpy'
+  NUMPY_VERSION_MAJOR = 2
 
 package['build_args'] = {
-  'NUMPY_PACKAGE': NUMPY_PACKAGE
+  'NUMPY_PACKAGE': NUMPY_PACKAGE,
+  'NUMPY_VERSION_MAJOR': NUMPY_VERSION_MAJOR,
 }
