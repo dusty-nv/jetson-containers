7c9a7bd82f0f5f8137673cdeb62b950529c656e0
Johnny <johnnynuca14@gmail.com>
2025-04-08 02:40:43 +0200

    fix

--- packages/llm/exllama/build.sh
+++ packages/llm/exllama/build.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 set -ex
 
-echo "Building ExLlamaV2 ${EXLLAMA_VERSION}"
+echo "Building ExLlamaV3 ${EXLLAMA_VERSION}"
 
-cd /opt/exllamav2
+cd /opt/exllamav3
 
 sed 's|torch.*|torch|g' -i requirements.txt
 sed 's|"torch.*"|"torch"|g' -i setup.py
 sed 's|\[\"cublas\"\] if windows else \[\]|\[\"cublas\"\]|g' -i setup.py
 sed 's|-mavx2||g' -i setup.py
-sed 's|-mavx2||g' -i exllamav2/ext.py
-sed 's|#define USE_AVX2||g' -i exllamav2/exllamav2_ext/cpp/sampling.cpp
+sed 's|-mavx2||g' -i exllamav3/ext.py
+sed 's|#define USE_AVX2||g' -i exllamav3/exllamav3_ext/cpp/sampling.cpp
 
 #pip3 wheel --wheel-dir=/opt --verbose .
 python3 setup.py --verbose bdist_wheel --dist-dir /opt
 
 cd /
-#rm -rf exllamav2
+#rm -rf exllamav3
 ls -ll /opt
 
-pip3 install /opt/exllamav2*.whl
-pip3 show exllamav2 && python3 -c 'import exllamav2'
+pip3 install /opt/exllamav3*.whl
+pip3 show exllamav3 && python3 -c 'import exllamav3'
 
-twine upload --verbose /opt/exllamav2*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
+twine upload --verbose /opt/exllamav3*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
