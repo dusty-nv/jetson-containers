86dc91542cc6aa9d129db270dd7ea479f2cb60bf
Johnny <johnnynuca14@gmail.com>
2025-04-08 21:42:39 +0200

    fix cuda-python (#1030)
    * fix
    
    * fix cuda-python

--- packages/cuda/cuda-python/build.sh
+++ packages/cuda/cuda-python/build.sh
@@ -3,17 +3,19 @@ set -ex
 
 echo "Building cuda-python ${CUDA_PYTHON_VERSION}"
    
-git clone --branch v${CUDA_PYTHON_VERSION} --depth=1 https://github.com/NVIDIA/cuda-python
-cd cuda-python
+git clone --branch v${CUDA_PYTHON_VERSION} --depth=1 https://github.com/NVIDIA/cuda-python /opt/cuda-python
+cd /opt/cuda-python
 
+# NOW CUDA-PYTHON HAS 3 STRUCTURES
+cd cuda-python
 sed 's|^numpy.=.*|numpy|g' -i requirements.txt
 sed 's|^numba.=.*|numba|g' -i requirements.txt
 
 pip3 install -r requirements.txt
 python3 setup.py bdist_wheel --verbose --dist-dir /opt
 
-cd ../
-rm -rf cuda-python
+cd /opt/
+rm -rf /opt/cuda-python
 
 pip3 install /opt/cuda*.whl
 pip3 show cuda-python && python3 -c 'import cuda; print(cuda.__version__)'
