23c64636c0f9f0cdc59f158dc96fbffca328d3a0
johnnynunez <johnnynuca14@gmail.com>
2025-04-09 14:19:10 +0200

    support new CUDA PYTHON

--- packages/cuda/cuda-python/build.sh
+++ packages/cuda/cuda-python/build.sh
@@ -7,17 +7,17 @@ git clone --branch v${CUDA_PYTHON_VERSION} --depth=1 https://github.com/NVIDIA/c
 cd /opt/cuda-python
 
 # NOW CUDA-PYTHON HAS 3 STRUCTURES
-cd cuda-python
-sed 's|^numpy.=.*|numpy|g' -i requirements.txt
-sed 's|^numba.=.*|numba|g' -i requirements.txt
+cd cuda_core
+pip3 wheel . --no-deps --wheel-dir /opt --verbose
 
-pip3 install -r requirements.txt
-python3 setup.py bdist_wheel --verbose --dist-dir /opt
+cd /opt/cuda-python
+cd cuda_bindings
+pip3 wheel . --no-deps --wheel-dir /opt --verbose
 
 cd /opt/
 rm -rf /opt/cuda-python
 
 pip3 install /opt/cuda*.whl
-pip3 show cuda-python && python3 -c 'import cuda; print(cuda.__version__)'
+pip3 show cuda_core && pip3 show cuda_bindings && python3 -c 'import cuda; print(cuda.__version__)'
 
 twine upload --verbose /opt/cuda*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
