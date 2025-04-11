5c08fbfe7de52bd4b9b616cd7a108a0b83166558
Johnny <johnnynuca14@gmail.com>
2025-04-12 01:11:29 +0200

    back

--- packages/llm/sglang/install.sh
+++ packages/llm/sglang/install.sh
@@ -6,5 +6,5 @@ if [ "$FORCE_BUILD" == "on" ]; then
 	exit 1
 fi
 
-uv pip3 install sglang==${SGLANG_VERSION} || \
-uv pip install sglang==${SGLANG_VERSION_SPEC}
+pip3 install sglang==${SGLANG_VERSION} || \
+pip3 install sglang==${SGLANG_VERSION_SPEC}
