ca5bb0135700db6604341fea9bf3a98dc37b9492
Dustin Franklin <dustinf@nvidia.com>
2025-04-15 23:53:17 -0400

    updated pyav to use ffmpeg:git dependency

--- packages/multimedia/pyav/test.sh
+++ packages/multimedia/pyav/test.sh
@@ -2,9 +2,9 @@
 echo "testing PyAV..."
 set -ex
 
-$HOME/bin/ffmpeg -version
-$HOME/bin/ffmpeg -decoders
-$HOME/bin/ffmpeg -decoders | grep av1
+ffmpeg -version
+ffmpeg -decoders
+ffmpeg -decoders | grep av1
 
 pip3 show pyav
 python3 -c 'import av; print("pyav version:", av.__version__)'
