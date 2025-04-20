f7858a992e3bc6e98ba6f2bf8604a6293159804c
Johnny <johnnynuca14@gmail.com>
2025-04-20 23:11:20 +0200

    Update config.py

--- packages/llm/flash-infer/config.py
+++ packages/llm/flash-infer/config.py
@@ -26,6 +26,7 @@ def flash_infer(version, version_spec=None, requires=None, default=False):
     return pkg, builder
 
 package = [
-    flash_infer('0.2.3', '0.2.2.post1', default=False),
+    flash_infer('0.2.2', '0.2.2', default=False),
+    flash_infer('0.2.2.post1', '0.2.2.post1', default=False),
     flash_infer('0.2.6', '0.2.5', default=True),
 ]
