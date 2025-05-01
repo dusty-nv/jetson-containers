c0db566fcbb14219916807405b2b26b9b07aa6dd
johnnynunez <johnnynuca14@gmail.com>
2025-05-01 11:29:57 +0200

    refactor: update build script for mamba installation and packaging

--- packages/ml/mamba/mamba/build.sh
+++ packages/ml/mamba/mamba/build.sh
@@ -2,8 +2,11 @@
 set -ex
 
 # Clone the repository if it doesn't exist
-git clone --branch=v${MAMBA_VERSION} --depth=1 --recursive https://github.com/johnnynunez/mamba /opt/mamba || \
-git clone --depth=1 --recursive https://github.com/johnnynunez/mamba /opt/mamba
+git clone --branch=v${CASUALCONV1D_VERSION} --depth=1 --recursive https://github.com/johnnynunez/mamba /opt/mamba  || \
+git clone --depth=1 --recursive https://github.com/johnnynunez/mamba  /opt/mamba
+
+# Navigate to the directory containing mamba's setup.py
+cd /opt/mamba
 
 # Generate the diff dynamically
 python3 /tmp/mamba/generate_diff.py
@@ -14,16 +17,13 @@ git status
 sed -i '/torch/d' pyproject.toml
 sed -i '/triton/d' pyproject.toml
 
-pip3 install lm-eval
-pip3 install --upgrade setuptools einops
-pip3 install triton
-export MAX_JOBS=$(nproc)
-export MAMBA_FORCE_BUILD=TRUE
-export MAMBA_SKIP_CUDA_BUILD=FALSE
-pip3 wheel --no-build-isolation --wheel-dir=/opt/mamba/wheels . --verbose
-pip3 install /opt/mamba/wheels/mamba_ssm*.whl
+MAX_JOBS=16 \
+MAMBA_FORCE_BUILD="TRUE" \
+MAMBA_SKIP_CUDA_BUILD="FALSE" \
+python3 setup.py bdist_wheel --dist-dir=/opt/mamba/wheels
+pip3 install /opt/mamba/wheels/mamba*.whl
 
 cd /opt/mamba
 
 # Optionally upload to a repository using Twine
-twine upload --verbose /opt/mamba/wheels/mamba_ssm*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
+twine upload --verbose /opt/mamba/wheels/mamba*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
