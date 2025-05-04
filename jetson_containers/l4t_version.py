e5f15cfd18ec16dffa716c7bb166dbd1f2491d90
Johnny <johnnynuca14@gmail.com>
2025-05-04 16:03:29 +0200

    [UPDATE] Add support for CUDA 12.9 and TensorRT 10.9 in configuration

--- jetson_containers/l4t_version.py
+++ jetson_containers/l4t_version.py
@@ -246,16 +246,17 @@ def get_cuda_version(version_file: str = "/usr/local/cuda/version.json", l4t_ver
                 print("-- unable to extract CUDA version number")
         else:
             l4t_version = get_l4t_version(l4t_version=l4t_version)
-            if l4t_version.major >= 36:
+            if l4t_version.major >= 37:
+                if l4t_version == Version('37.0'):
+                    cuda_version = '13.0'
+            elif l4t_version.major >= 36:
                 # L4T r36.x (JP 6.x) and above does not require having CUDA installed on host
                 # When CUDA is not installed on host, users can specify which version of
                 # CUDA (and matching version cuDNN and TensorRT) in container by
                 # executing, for example, `export CUDA_VERSION=12.8`.
                 # If the env variable is not set, set the CUDA_VERSION to be the CUDA version
                 # that made available with the release of L4T_VERSION
-                if l4t_version == Version('37.0'):
-                    cuda_version = '13.0'
-                elif l4t_version == Version('36.4') or l4t_version == Version('36.4.2') or l4t_version == Version('36.4.3'):
+                if l4t_version == Version('36.4') or l4t_version == Version('36.4.2') or l4t_version == Version('36.4.3'):
                     cuda_version = '12.6'
                 elif l4t_version == Version('36.3'):
                     cuda_version = '12.4'
