0952b92786e53abd71a4e85894e306bbe025f881
Johnny <johnnynuca14@gmail.com>
2025-05-07 14:05:11 +0200

    update CUDA and cuDNN package requirements to use '>=36' instead of '>=36.*'

--- packages/cuda/cudnn/config.py
+++ packages/cuda/cudnn/config.py
@@ -95,9 +95,9 @@ if IS_TEGRA and IS_CONFIG:
         cudnn_package('9.0',f'{CUDNN_URL}/9.0.0/local_installers/cudnn-local-tegra-repo-ubuntu2204-9.0.0_1.0-1_arm64.deb', cuda='12.4', requires='==36.*'),
         cudnn_package('9.3',f'{CUDNN_URL}/9.3.0/local_installers/cudnn-local-tegra-repo-ubuntu2204-9.3.0_1.0-1_arm64.deb', cuda='12.6', requires='==36.*'),
         cudnn_package('9.4',f'{CUDNN_URL}/9.4.0/local_installers/cudnn-local-tegra-repo-ubuntu2204-9.4.0_1.0-1_arm64.deb', cuda='12.6', requires='==36.*'),
-        cudnn_package('9.8',f'{CUDNN_URL}/9.8.0/local_installers/cudnn-local-tegra-repo-ubuntu2404-9.8.0_1.0-1_arm64.deb', cuda='12.8', requires='>=36.*', packages="libcudnn9-cuda-12 libcudnn9-dev-cuda-12 libcudnn9-samples"),
-        cudnn_package('9.9',f'{CUDNN_URL}/9.9.0/local_installers/cudnn-local-tegra-repo-ubuntu2404-9.9.0_1.0-1_arm64.deb', cuda='12.9', requires='>=36.*', packages="libcudnn9-cuda-12 libcudnn9-dev-cuda-12 libcudnn9-samples"),
-        cudnn_package('10.0',f'{CUDNN_URL}/10.0.0/local_installers/cudnn-local-tegra-repo-ubuntu2404-10.0.0_1.0-1_arm64.deb', cuda='13.0', requires='>=36.*', packages="libcudnn9-cuda-13 libcudnn9-dev-cuda-13 libcudnn9-samples"),
+        cudnn_package('9.8',f'{CUDNN_URL}/9.8.0/local_installers/cudnn-local-tegra-repo-ubuntu2404-9.8.0_1.0-1_arm64.deb', cuda='12.8', requires='>=36', packages="libcudnn9-cuda-12 libcudnn9-dev-cuda-12 libcudnn9-samples"),
+        cudnn_package('9.9',f'{CUDNN_URL}/9.9.0/local_installers/cudnn-local-tegra-repo-ubuntu2404-9.9.0_1.0-1_arm64.deb', cuda='12.9', requires='>=36', packages="libcudnn9-cuda-12 libcudnn9-dev-cuda-12 libcudnn9-samples"),
+        cudnn_package('10.0',f'{CUDNN_URL}/10.0.0/local_installers/cudnn-local-tegra-repo-ubuntu2404-10.0.0_1.0-1_arm64.deb', cuda='13.0', requires='>=36', packages="libcudnn9-cuda-13 libcudnn9-dev-cuda-13 libcudnn9-samples"),
 
         # JetPack 4-5 (cuDNN installed in base container)
         cudnn_builtin(requires='<36', default=True),
