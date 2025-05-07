cdb603aae089292d8480b57751a666f014e7603b
Johnny <johnnynuca14@gmail.com>
2025-05-07 17:39:08 +0200

    Update build scripts and Dockerfile for Open3D installation

--- packages/cv/open3d/build.sh
+++ packages/cv/open3d/build.sh
@@ -24,21 +24,24 @@ fi
 
 cd "${REPO_DIR}" || exit 1
 
-bash util/install_deps_ubuntu.sh
 mkdir build
 cd build
-export LD_PRELOAD=/usr/local/lib/libOpen3D.so.0.19
+
 cmake -DBUILD_CUDA_MODULE=ON \
       -DBUILD_SHARED_LIBS=ON \
       -DGLIBCXX_USE_CXX11_ABI=OFF \
-      -DBUILD_PYTORCH_OPS=ON \
+      -DBUILD_PYTORCH_OPS=OFF \
       -DBUILD_TENSORFLOW_OPS=OFF \
-      -DBUNDLE_OPEN3D_ML=ON \
-      -DOPEN3D_ML_ROOT=https://github.com/isl-org/Open3D-ML.git \
       ..
-make install # install open3d C++
-make -j$(nproc) pip-package
 
+# -DBUNDLE_OPEN3D_ML=ON \
+# -DOPEN3D_ML_ROOT=https://github.com/isl-org/Open3D-ML.git \
+
+make install # install open3d C++
+echo "Installed Open3D C++"
+echo "Building Open3D Python wheel"
+make -j$(nproc) pip-package
+echo "Done Open3D Python wheel"
 # Try uploading; ignore failure
 twine upload --verbose "opt/open3d/open3d"*.whl \
   || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL:-<unset>}"
