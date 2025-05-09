0b14f2103fef7e40b436863c0efe66e68dcbb5db
johnnynunez <johnnynuca14@gmail.com>
2025-05-10 00:36:04 +0200

    feat: add Dockerfile and configuration for cuDNN installation and testing

--- packages/llm/distributed/dynamo/nixl/install.sh
+++ packages/llm/distributed/dynamo/nixl/install.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 set -ex
-RUN apt-get update -y && \
-    apt-get install -y --no-install-recommends \
-        build-essential devscripts debhelper fakeroot pkg-config \
-        cmake etcd etcd-server etcd-client \
-        libgrpc-dev libgrpc++-dev libprotobuf-dev protobuf-compiler-grpc \
-        libcpprest-dev && \
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
+apt-get update -y && \
+apt-get install -y --no-install-recommends \
+  ninja-build \
+  pybind11-dev \
+  libclang-dev \
+  cmake \
+  libgflags-dev \
+  libgrpc-dev \
+  libgrpc++-dev \
+  libprotobuf-dev \
+  libaio-dev \
+  liburing-dev \
+  protobuf-compiler-grpc \
+  libcpprest-dev \
+  etcd-server \
+  etcd-client \
+  autotools-dev \
+  automake \
+  libtool \
+  libz-dev \
+  flex \
+  build-essential && \
+  apt-get clean && \
+  rm -rf /var/lib/apt/lists/*
 
 if [ "$FORCE_BUILD" == "on" ]; then
 	echo "Forcing build of nixl ${NIXL_VERSION}"
