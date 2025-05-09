5cf7711560b72fa6e37bd7613673862239929962
johnnynunez <johnnynuca14@gmail.com>
2025-05-09 21:21:40 +0200

    feat: update build script to create wheels for mooncake and remove isolation flag

--- packages/llm/distributed/mooncake/build.sh
+++ packages/llm/distributed/mooncake/build.sh
@@ -19,6 +19,11 @@ ldconfig
 
 cd /opt/mooncake/mooncake-wheel
 sed -i 's/manylinux_2_35_x86_64/manylinux_2_35_aarch64/' setup.py
+
+cd /opt/mooncake/scripts
+sed -i 's/--plat manylinux_2_35_x86_64/--plat manylinux_2_35_aarch64/' build.sh
+
+
 mkdir /opt/mooncake/wheels
 bash build_wheel.sh /opt/mooncake/wheels
 pip3 install /opt/mooncake/wheels/mooncake_transfer_engine*.whl
