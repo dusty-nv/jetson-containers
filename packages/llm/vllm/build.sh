3fd9e8ecee58a202eb5d50dd53a40258b837a9b1
Johnny <johnnynuca14@gmail.com>
2025-05-11 01:06:37 +0200

    refactor: Simplify vllm function and clean up build script

--- packages/llm/vllm/build.sh
+++ packages/llm/vllm/build.sh
@@ -1,22 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-# Install dependencies of XGrammar
 pip3 install pre-commit nanobind==2.5.0
-
-# Clone the repository if it doesn't exist
-git clone --branch=v${XGRAMMAR_VERSION} --recursive --depth=1 https://github.com/mlc-ai/xgrammar /opt/xgrammar ||
-git clone --recursive --depth=1 https://github.com/mlc-ai/xgrammar /opt/xgrammar
-
-# Create the wheel
-cd /opt/xgrammar
-pip3 wheel . --wheel-dir /opt/xgrammar/wheels/ --verbose
-
-# Install the wheel
-# Warning: version number is 0.1.5 even if actual version is 0.1.8, or 0.1.9 due to version.py not being adapted yet: https://github.com/mlc-ai/xgrammar/blob/main/python/xgrammar/version.py
-pip3 install /opt/xgrammar/wheels/xgrammar*.whl
-
-
 # Clone the repository if it doesn't exist
 git clone --branch=${VLLM_BRANCH} --recursive --depth=1 https://github.com/vllm-project/vllm /opt/vllm || 
 git clone --recursive --depth=1 https://github.com/vllm-project/vllm /opt/vllm
@@ -60,4 +45,3 @@ pip3 install compressed-tensors
 
 # Optionally upload to a repository using Twine
 twine upload --verbose /opt/vllm/wheels/vllm*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
-twine upload --verbose /opt/xgrammar/wheels/xgrammar*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
--- packages/llm/vllm/config.py
+++ packages/llm/vllm/config.py
@@ -1,21 +1,20 @@
 from jetson_containers import CUDA_VERSION, IS_SBSA
 from packaging.version import Version
 
-def vllm(vllm_version, xgrammar_version, branch=None, requires=None, default=False):
+def vllm(version, branch=None, requires=None, default=False):
     pkg = package.copy()
 
     if requires:
         pkg['requires'] = requires   
 
-    suffix = branch if branch else vllm_version
-    branch = branch if branch else f'v{vllm_version}'
+    suffix = branch if branch else version
+    branch = branch if branch else f'v{version}'
 
     pkg['name'] = f'vllm:{suffix}'
 
     pkg['build_args'] = {
-        'VLLM_VERSION': vllm_version,
+        'VLLM_VERSION': version,
         'VLLM_BRANCH': branch,
-        'XGRAMMAR_VERSION': xgrammar_version,
         'IS_SBSA': IS_SBSA
     }
 
@@ -32,8 +31,8 @@ def vllm(vllm_version, xgrammar_version, branch=None, requires=None, default=Fal
 
 package = [
     # 0.6.5 compatible with jetson https://github.com/vllm-project/vllm/pull/9735
-    vllm(vllm_version='0.7.4', xgrammar_version='0.1.15', default=False),
-    vllm(vllm_version='0.8.4', xgrammar_version='0.1.18', default=False),
-    vllm(vllm_version='0.8.5', xgrammar_version='0.1.18', default=False),
-    vllm(vllm_version='0.8.6', xgrammar_version='0.1.19', default=True),
+    vllm(version='0.7.4', default=False),
+    vllm(version='0.8.4', default=False),
+    vllm(version='0.8.5', default=False),
+    vllm(version='0.8.6', default=True),
 ]
