f54f7137bb47909bb6f36f80b53d6b125f4d87e7
johnnynunez <johnnynuca14@gmail.com>
2025-05-10 19:20:56 +0200

    Add installation scripts and configuration for xgrammar

---
+++ packages/llm/xgrammar/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -ex
+
+# Install dependencies of XGrammar
+pip3 install pre-commit nanobind==2.5.0
+
+# Clone the repository if it doesn't exist
+git clone --branch=v${XGRAMMAR_VERSION} --recursive --depth=1 https://github.com/mlc-ai/xgrammar /opt/xgrammar ||
+git clone --recursive --depth=1 https://github.com/mlc-ai/xgrammar /opt/xgrammar
+
+# Create the wheel
+export MAX_JOBS=$(nproc) # this is for AGX (max 4 working on Orin NX)
+export USE_CUDNN=1
+export VERBOSE=1
+export CUDA_HOME=/usr/local/cuda
+export PATH="${CUDA_HOME}/bin:$PATH"
+cd /opt/xgrammar
+pip3 wheel . --wheel-dir /opt/xgrammar/wheels/ --verbose
+
+# Install the wheel
+# Warning: version number is 0.1.5 even if actual version is 0.1.8, or 0.1.9 due to version.py not being adapted yet: https://github.com/mlc-ai/xgrammar/blob/main/python/xgrammar/version.py
+pip3 install /opt/xgrammar/wheels/xgrammar*.whl
+pip3 install compressed-tensors
+
+# Optionally upload to a repository using Twine
+twine upload --verbose /opt/xgrammar/wheels/xgrammar*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
---
+++ packages/llm/xgrammar/test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+print('testing xgrammar...')
+import xgrammar as xgr
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
+
+device = "cuda"  # Or "cpu", etc.
+model_name = "meta-llama/Llama-3.2-1B-Instruct"
+model = AutoModelForCausalLM.from_pretrained(
+    model_name, torch_dtype=torch.float32, device_map=device
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+config = AutoConfig.from_pretrained(model_name)
+
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Introduce yourself in JSON briefly."},
+]
+texts = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+model_inputs = tokenizer(texts, return_tensors="pt").to(model.device)
+    
+print('xgrammar OK\n')
