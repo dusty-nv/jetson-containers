f7660c5139472d59ec9c8aa93cbfb3eebdee62c5
Johnny <johnnynuca14@gmail.com>
2025-05-10 20:20:17 +0200

    ai-dynamo vllm docker and xgrammar docker (#1101)
    * ai-dynamo-vllm
    
    * Update config.py
    
    * Update Dockerfile
    
    * Update config.py
    
    * Update build.sh
    
    * Update build.sh
    
    * Update Dockerfile
    
    * Update build.sh
    
    * Add installation scripts and configuration for xgrammar
    
    * Update Dockerfile to include xgrammar as a dependency
    
    * Update generate_diff.py to change project name from vLLM to dynamo_vllm
    
    * Update generate_diff.py to change project name from vLLM to dynamo_vllm
    
    * Update generate_diff.py to change project name from vLLM to dynamo_vllm
    
    * Update generate_diff.py to change project name from vLLM to dynamo_vllm
    
    * Update generate_diff.py to change project name from vLLM to dynamo_vllm

---
+++ packages/llm/dynamo/ai-dynamo-vllm/config.py
@@ -0,0 +1,37 @@
+from jetson_containers import CUDA_VERSION, IS_SBSA
+from packaging.version import Version
+
+def dynamo_vllm(version, version_spec=None, requires=None, default=False):
+    pkg = package.copy()
+
+    if requires:
+        pkg['requires'] = requires   
+
+    if not version_spec:
+        version_spec = version
+
+    pkg['name'] = f'dynamo_vllm:{version}'
+     
+    if version:
+        vllm_ref = version.split('.post')[0]
+        
+    pkg['build_args'] = {
+        'IS_SBSA': IS_SBSA,
+        'VLLM_REF': vllm_ref,
+        'VLLM_PATCHED_PACKAGE_VERSION': version,
+    }
+
+    builder = pkg.copy()
+
+    builder['name'] = f'dynamo_vllm:{version}-builder'
+    builder['build_args'] = {**pkg['build_args'], **{'FORCE_BUILD': 'on'}}
+
+    if default:
+        pkg['alias'] = 'dynamo_vllm'
+        builder['alias'] = 'dynamo_vllm:builder'
+
+    return pkg, builder
+
+package = [
+    dynamo_vllm('0.8.4.post1', default=True),
+]
