9627327a346349b2d66a0a8897bb59f6d5b4c916
Johnny <johnnync13@gmail.com>
2025-05-10 18:37:32 +0200

    Update build.sh

--- packages/llm/dynamo/dynamo/build.sh
+++ packages/llm/dynamo/dynamo/build.sh
@@ -5,11 +5,27 @@ set -ex
 git clone --branch=v${DYNAMO_VERSION} --depth=1 --recursive https://github.com/ai-dynamo/dynamo /opt/dynamo || \
 git clone --depth=1 --recursive https://github.com/ai-dynamo/dynamo /opt/dynamo
 
-cd /opt/dynamo
-echo "Building ai-dynamo version ${DYNAMO_VERSION}..."
-export CARGO_BUILD_JOBS=$(nproc)
+
 export MAX_JOBS=$(nproc)
 
+echo "Building building vllm version ${VLLM_VERSION}..."
+git clone --branch v"${VLLM_REF}"  --depth 1 https://github.com/vllm-project/vllm.git /tmp/vllm/vllm-"${VLLM_REF}" 
+cd /tmp/vllm/vllm-"${VLLM_REF}/" 
+# Patch vLLM source with dynamo additions
+patch -p1 < "/opt/dynamo/container/deps/vllm/${VLLM_PATCH}" || echo "Failed to apply patch ${VLLM_PATCH}"
+sleep 5
+sed -i 's/version("ai_dynamo_vllm")/version("vllm")/g' vllm/platforms/__init__.py 
+sleep 5
+python3 use_existing_torch.py || echo "skipping vllm/use_existing_torch.py" 
+pip3 install -r requirements/build.txt -v 
+python3 -m setuptools_scm || echo "skipping vllm/setuptools_scm" 
+pip3 wheel --no-build-isolation -v --wheel-dir=/opt/ . 
+pip3 install  /opt/ai-dynamo-runtime*.whl \
+twine upload --verbose /opt/ai-dynamo-runtime*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
+
+echo "Building ai-dynamo version ${DYNAMO_VERSION}..."
+export CARGO_BUILD_JOBS=$(nproc)
+cd /opt/dynamo
 cargo build --release --features cuda,python
 
 echo "Building bindings for Python"
