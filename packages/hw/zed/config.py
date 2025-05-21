d10565a5244e564321c3de55c5edee7f9bc330a4
none <none>
2025-05-20 22:10:57 -0400

    updated container for x86

--- packages/hw/zed/config.py
+++ packages/hw/zed/config.py
@@ -1,8 +1,10 @@
 
-from jetson_containers import L4T_VERSION
+from jetson_containers import L4T_VERSION, CUDA_VERSION, LSB_RELEASE, IS_TEGRA
 from packaging.version import Version
 
-def zed(version, l4t_version, requires=None, default=False):
+from ..robots.ros import ros_container
+
+def zed(version, l4t_version=None, requires=None, default=False):
     """
     Container for Stereolabs ZED SDK from:
 
@@ -12,10 +14,12 @@ def zed(version, l4t_version, requires=None, default=False):
     This defines a version released to Stereolabs website.
     """
     sdk = package.copy()
-    url = f"https://download.stereolabs.com/zedsdk/{version}/l4t{l4t_version}/jetsons"
 
-    sdk['name'] = f'zed:{version}'
-
+    if l4t_version and IS_TEGRA:
+        url = f"https://download.stereolabs.com/zedsdk/{version}/l4t{l4t_version}/jetsons"
+    else:
+        url = f"https://download.stereolabs.com/zedsdk/{version}/cu{CUDA_VERSION.major}/ubuntu{LSB_RELEASE.split('.')[0]}"
+    
     sdk['build_args'] = {
         'ZED_URL': url,
         'L4T_MAJOR_VERSION': L4T_VERSION.major,
@@ -23,13 +27,26 @@ def zed(version, l4t_version, requires=None, default=False):
         'L4T_PATCH_VERSION': L4T_VERSION.micro,
     }
 
+    sdk['name'] = f'zed:{version}'
+
     if default:
         sdk['alias'] = 'zed'
 
     if requires:
         sdk['requires'] = requires
 
-    return sdk
+    return [
+        sdk,
+        ros_container({
+            **sdk,
+            'name': sdk['name'] + '-${ROS_DISTRO}',
+            'alias': sdk['alias'] + '-${ROS_DISTRO}',
+            }, 
+            'https://github.com/stereolabs/zed-ros2-interfaces',
+            'https://github.com/stereolabs/zed-ros2-wrapper',
+            distros=['humble', 'jazzy'], base_packages='desktop'
+        )
+    ]
 
 package = [
     zed('5.0', '35.4', requires='<=35', default=True),  # JetPack 5
