d6ff664edc822a2aec9ad5c3519a339a6c44fad0
Gianfranco <gianpa@gmail.com>
2025-06-01 01:06:03 +0200

    upgrade to ollama 0.9

--- packages/llm/ollama/config.py
+++ packages/llm/ollama/config.py
@@ -4,8 +4,9 @@ from jetson_containers import JETPACK_VERSION, nv_tegra_release
 
 nv_tegra_release( # ollama uses /etc/nv_tegra_release
     dst=os.path.join(package['path'], 'nv_tegra_release')
-) 
+)
 
+# grammer
 def ollama(version, default=False):
     pkg = package.copy()
 
@@ -14,13 +15,13 @@ def ollama(version, default=False):
     pkg['build_args'] = {
         'OLLAMA_VERSION': version,
         'JETPACK_VERSION_MAJOR': JETPACK_VERSION.major,
-    }   
-     
+    }
+
     if default:
         pkg['alias'] = 'ollama'
 
     return pkg
-    
+
 package = [
     #ollama('main'),
     ollama('0.4.0'),
@@ -28,5 +29,7 @@ package = [
     ollama('0.5.5'), #, branch='0.5.5-rc0'),
     ollama('0.5.7'),
     ollama('0.6.7'),
-    ollama('0.7.0', default=True)
+    ollama('0.7.0'),
+    ollama('0.8.0'),
+    ollama('0.9.0', default=True)
 ]
