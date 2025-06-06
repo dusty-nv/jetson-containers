3655d0b0f2793be87652537f065f6cc2f53faf83
Johnny <johnnynuca14@gmail.com>
2025-06-06 20:08:23 +0200

    Update build.sh to install generated wheel after building

--- packages/rag/memvid/build.sh
+++ packages/rag/memvid/build.sh
@@ -11,5 +11,5 @@ cd /opt/memvid
 sed -i 's/==/>=/g' requirements.txt
 pip3 install -U -r requirements.txt
 pip3 wheel . -v --no-deps -w /opt/memvid/wheels/
-
+pip3 install /opt/memvid/wheels/memvid*.whl
 twine upload --verbose /opt/memvid/wheels/memvid*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
