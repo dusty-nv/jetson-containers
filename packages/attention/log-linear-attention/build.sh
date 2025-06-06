9adf31012edc578d4ab5efabea1b967db38b42f9
Johnny <johnnynuca14@gmail.com>
2025-06-06 20:31:41 +0200

    Update build.sh to clone log-linear-attention repository recursively

--- packages/attention/log-linear-attention/build.sh
+++ packages/attention/log-linear-attention/build.sh
@@ -3,8 +3,8 @@ set -ex
 
 echo "Building LOG_LINEAR_ATTN_VERSION ${LOG_LINEAR_ATTN_VERSION}"
 
-git clone --depth=1 --branch=v${LOG_LINEAR_ATTN_VERSION} https://github.com/HanGuo97/log-linear-attention /opt/log-linear-attention ||
-git clone --depth=1 https://github.com/HanGuo97/log-linear-attention /opt/log-linear-attention
+git clone --recursive --depth=1 --branch=v${LOG_LINEAR_ATTN_VERSION} https://github.com/HanGuo97/log-linear-attention /opt/log-linear-attention ||
+git clone --recursive --depth=1 https://github.com/HanGuo97/log-linear-attention /opt/log-linear-attention
 
 cd /opt/log-linear-attention
 # export MAX_JOBS="$(nproc)" this breaks with actual log-linear-attention
@@ -17,7 +17,7 @@ export CMAKE_BUILD_PARALLEL_LEVEL=$MAX_JOBS
 echo "Building with MAX_JOBS=$MAX_JOBS and CMAKE_BUILD_PARALLEL_LEVEL=$CMAKE_BUILD_PARALLEL_LEVEL"
 
 pip3 install -r requirements.txt
-pip3 install -e flame/
+pip3 install -e /opt/log-linear-attention/flame/
 pip3 install -r flame/3rdparty/torchtitan/requirements.txt
 
 MAX_JOBS=$MAX_JOBS \
