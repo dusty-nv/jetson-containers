765a457779de5ee08f745dcf6b15dd09307e036f
johnnynunez <johnnynuca14@gmail.com>
2025-06-10 20:11:06 +0200

    Normalize package naming to lowercase in config.py and Dockerfile

--- packages/nerf/4k4d/config.py
+++ packages/nerf/4k4d/config.py
@@ -4,7 +4,7 @@ def fourk4D(version, requires=None, default=False):
     if requires:
         pkg['requires'] = requires   
 
-    pkg['name'] = f'4k4D:{version}'
+    pkg['name'] = f'4k4d:{version}'
 
     pkg['build_args'] = {
         '4k4D_VERSION': version,
@@ -12,12 +12,12 @@ def fourk4D(version, requires=None, default=False):
 
     builder = pkg.copy()
 
-    builder['name'] = f'4k4D:{version}-builder'
+    builder['name'] = f'4k4d:{version}-builder'
     builder['build_args'] = {**pkg['build_args'], **{'FORCE_BUILD': 'on'}}
 
     if default:
-        pkg['alias'] = '4k4D'
-        builder['alias'] = '4k4D:builder'
+        pkg['alias'] = '4k4d'
+        builder['alias'] = '4k4d:builder'
 
     return pkg, builder
 
