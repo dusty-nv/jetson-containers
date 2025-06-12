00573bf434c28e9c85a5e36262c7ff16f3854bc9
johnnynunez <johnnynuca14@gmail.com>
2025-06-12 14:27:43 +0200

    Update CUDA architectures for SBSA compatibility by removing reference to Spark 121

--- jetson_containers/l4t_version.py
+++ jetson_containers/l4t_version.py
@@ -322,7 +322,7 @@ def get_cuda_arch(l4t_version: str=None, cuda_version: str=None, format=list):
             elif l4t_version.major == 32:  # JetPack 4
                 cuda_architectures = [53, 62, 72]
         elif IS_SBSA:
-            cuda_architectures = [87, 90, 100, 101]  # Ampere Orin, Hopper GH200 90, Blackwell GB200 100, Thor 101, Spark 121
+            cuda_architectures = [87, 90, 100, 101]  # Ampere Orin, Hopper GH200 90, Blackwell GB200 100, Thor 101
             if cuda_version >= Version('13.0'):
                 cuda_architectures += [103, 110, 121] # Thor 110, Spark 121
     else:
