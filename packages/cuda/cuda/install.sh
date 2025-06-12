9db5470c2ed1b3d0980c1acd94ac33856a57ec6c
johnnynunez <johnnynuca14@gmail.com>
2025-06-12 13:34:53 +0200

    Enhance installation scripts to improve architecture detection and add NVPL support for ARM64

--- packages/cuda/cuda/install.sh
+++ packages/cuda/cuda/install.sh
@@ -1,8 +1,17 @@
-
 #!/usr/bin/env bash
 set -ex
 
-IS_SBSA=$([ -z "$(nvidia-smi --query-gpu=name --format=csv,noheader | grep nvgpu)" ] && echo 1 || echo 0)
+ARCH=$(uname -m)
+ARCH_TYPE=$ARCH
+
+# Detectar si es Tegra
+if [[ "$ARCH" == "aarch64" ]]; then
+    if uname -a | grep -qi tegra; then
+        ARCH_TYPE="tegra-aarch64"
+    fi
+fi
+
+echo "Detected architecture: ${ARCH_TYPE}"
 
 apt-get update
 apt-get install -y --no-install-recommends \
@@ -15,7 +24,7 @@ echo "Downloading ${CUDA_DEB}"
 mkdir -p /tmp/cuda
 cd /tmp/cuda
 
-if [[ -z $IS_SBSA ]]; then
+if [[ "$ARCH_TYPE" == "tegra-aarch64" ]]; then
     # Jetson (Tegra)
     wget $WGET_FLAGS \
         https://developer.download.nvidia.com/compute/cuda/repos/${DISTRO}/arm64/cuda-${DISTRO}.pin \
@@ -32,19 +41,18 @@ dpkg -i *.deb
 cp /var/cuda-*-local/cuda-*-keyring.gpg /usr/share/keyrings/
 
 # Tegra (Jetson)
-if [[ -z $IS_SBSA ]]; then
+if [[ "$ARCH_TYPE" == "tegra-aarch64" ]]; then
     ar x /var/cuda-tegra-repo-ubuntu*-local/cuda-compat-*.deb
     tar xvf data.tar.xz -C /
 fi
 
+# ARM64 SBSA (Grace) NVIDIA Performance Libraries (NVPL)
+# NVPL allows you to easily port HPC applications to NVIDIA Grace™ CPU platforms to achieve industry-leading performance and efficiency.
+if [[ "$ARCH_TYPE" == "aarch64" ]]; then
+    sudo apt-get -y install nvpl
+fi
 apt-get update
 apt-get install -y --no-install-recommends ${CUDA_PACKAGES}
-
-# if ARM64 SBSA (Grace)
-if [[ ! -z $IS_SBSA ]]; then
-    sudo apt-get -y --no-install-recommends install nvpl
-fi
-
 rm -rf /var/lib/apt/lists/*
 apt-get clean
 
