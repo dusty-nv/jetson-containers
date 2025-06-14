8ee7f8476c771e925aed6c16e4de8e53b0fb4cfd
johnnynunez <johnnynuca14@gmail.com>
2025-06-14 20:33:01 +0200

    Rename cosmos-predict1 to cosmos-predict2 and update related Dockerfile and README

--- packages/multimedia/decord/build.sh
+++ packages/multimedia/decord/build.sh
@@ -2,8 +2,8 @@
 set -ex
 
 # Clone the repository if it doesn't exist
-git clone --branch=v${DECORD_VERSION} --depth=1 --recursive https://github.com/johnnynunez/decord /opt/decord || \
-git clone --depth=1 --recursive https://github.com/johnnynunez/decord /opt/decord
+git clone --branch=v${DECORD_VERSION} --depth=1 --recursive https://github.com/johnnynunez/decord2 /opt/decord || \
+git clone --depth=1 --recursive https://github.com/johnnynunez/decord2 /opt/decord
 
 cd /opt/decord
 mkdir build && cd build
