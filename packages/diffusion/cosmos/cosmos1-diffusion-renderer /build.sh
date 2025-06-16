c2e4f5cf8062a836e8e711a462f19e83a3e0c22b
johnnynunez <johnnynuca14@gmail.com>
2025-06-16 10:09:44 +0200

    Update build script to install requirements after modifying dependencies

--- packages/diffusion/cosmos/cosmos1-diffusion-renderer /build.sh
+++ packages/diffusion/cosmos/cosmos1-diffusion-renderer /build.sh
@@ -10,6 +10,7 @@ cd /opt/cosmos1-diffusion-renderer
 sed -i '/decord==0.6.0/d' requirements.txt
 sed -i 's/==/>=/g' requirements.txt
 pip3 install decord2
+pip3 install -r requirements.txt
 cosmos1-diffusion-renderer_MORE_DETAILS=1 MAX_JOBS=$(nproc) \
 python3 setup.py --verbose bdist_wheel --dist-dir /opt/cosmos1-diffusion-renderer/wheels/
 
