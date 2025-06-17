43605fd318ad0f5460051dfdce7a7f2be013f24d
johnnynunez <johnnynuca14@gmail.com>
2025-06-17 12:20:09 +0200

    fix: update CUDA version and architectures for compatibility with L4T versions

--- jetson_containers/l4t_version.py
+++ jetson_containers/l4t_version.py
@@ -250,7 +250,7 @@ def get_cuda_version(version_file: str = "/usr/local/cuda/version.json", l4t_ver
             l4t_version = get_l4t_version(l4t_version=l4t_version)
             if l4t_version.major >= 38:
                 if l4t_version == Version('38.1'):
-                    cuda_version = '12.9'
+                    cuda_version = '13.0'
             elif l4t_version.major >= 36:
                 # L4T r36.x (JP 6.x) and above does not require having CUDA installed on host
                 # When CUDA is not installed on host, users can specify which version of
@@ -322,7 +322,7 @@ def get_cuda_arch(l4t_version: str=None, cuda_version: str=None, format=list):
             elif l4t_version.major == 32:  # JetPack 4
                 cuda_architectures = [53, 62, 72]
         elif IS_SBSA:
-            cuda_architectures = [87, 90, 100, 101]  # Ampere Orin, Hopper GH200 90, Blackwell GB200 100, Thor 101
+            cuda_architectures = [87, 90, 100]  # Ampere Orin, Hopper GH200 90, Blackwell GB200 100
             if cuda_version >= Version('13.0'):
                 cuda_architectures += [103, 110, 121] # Thor 110, Spark 121
     else:
@@ -330,7 +330,7 @@ def get_cuda_arch(l4t_version: str=None, cuda_version: str=None, format=list):
             80, 86,  # Ampere
             89,  # Ada
             90,  # Hopper
-            100, 101, 120  # Blackwell
+            100, 120  # Blackwell
         ]
 
         if cuda_version >= Version('12.9'):
