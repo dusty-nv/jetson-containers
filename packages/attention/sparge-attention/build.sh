a2ad813f9b72b752925ccc045e8a4686978d3c4e
johnnynunez <johnnynuca14@gmail.com>
2025-06-20 17:01:40 +0200

    fix: update build script to clone SpargeAttention from new repository and clean up test script

--- packages/attention/sparge-attention/build.sh
+++ packages/attention/sparge-attention/build.sh
@@ -3,11 +3,13 @@ set -ex
 
 echo "Building SpargeAttention ${SPARGE_ATTENTION_VERSION}"
 
-git clone --depth=1 --branch=v${SPARGE_ATTENTION_VERSION} https://github.com/thu-ml/SpargeAttn /opt/sparge-attention ||
-git clone --depth=1 https://github.com/thu-ml/SpargeAttn /opt/sparge-attention
+git clone --depth=1 --branch=v${SPARGE_ATTENTION_VERSION} https://github.com/johnnynunez/SpargeAttn /opt/sparge-attention ||
+git clone --depth=1 https://github.com/johnnynunez/SpargeAttn /opt/sparge-attention
 
 cd /opt/sparge-attention
-pip3 install ninja
+
+sed -i '1i#include <assert.h>' /usr/local/cuda/include/cuda_fp8.hpp
+
 export MAX_JOBS="$(nproc)"
 export CMAKE_BUILD_PARALLEL_LEVEL=$MAX_JOBS
 echo "Building with MAX_JOBS=$MAX_JOBS and CMAKE_BUILD_PARALLEL_LEVEL=$CMAKE_BUILD_PARALLEL_LEVEL"
