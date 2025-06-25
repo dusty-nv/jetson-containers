5f1603200116eabdffbf91e66fe3efbc1b668e5e
Johnny <johnnynuca14@gmail.com>
2025-06-26 00:47:48 +0200

    speedup

--- packages/cuda/cuda-python/build.sh
+++ packages/cuda/cuda-python/build.sh
@@ -5,9 +5,12 @@ set -ex
 SRC=/opt/cuda-python
 WHL=/opt/wheels
 
-export MAX_JOBS=$(nproc) 
+export MAX_JOBS=$(nproc)
 
 git clone --branch v$CUDA_PYTHON_VERSION --depth=1 https://github.com/NVIDIA/cuda-python $SRC
+export MAX_JOBS="$(nproc)"
+export CMAKE_BUILD_PARALLEL_LEVEL=$MAX_JOBS
+echo "Building with MAX_JOBS=$MAX_JOBS and CMAKE_BUILD_PARALLEL_LEVEL=$CMAKE_BUILD_PARALLEL_LEVEL"
 
 # NOW CUDA-PYTHON HAS 3 STRUCTURES
 if [ $(vercmp $CUDA_PYTHON_VERSION "12.6") -gt 0 ]; then
@@ -35,4 +38,4 @@ pip3 install $WHL/cuda*.whl
 twine upload --verbose $WHL/cuda*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
 
 python3 -c 'import cuda; print(cuda.__version__)'
-pip3 show cuda_core cuda_bindings || pip3 show cuda-python 
+pip3 show cuda_core cuda_bindings || pip3 show cuda-python
