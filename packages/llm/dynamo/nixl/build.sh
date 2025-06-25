e67e1674a0ac720e712a2691ad96d631a0d57606
Johnny <johnnynuca14@gmail.com>
2025-06-25 12:44:26 +0200

    fix: update build script to change installation paths and improve dependency management

--- packages/llm/dynamo/nixl/build.sh
+++ packages/llm/dynamo/nixl/build.sh
@@ -2,14 +2,19 @@
 set -ex
 
 # Clone the repository if it doesn't exist
-git clone --branch=v${NIXL_VERSION} --depth=1 --recursive https://github.com/ai-dynamo/nixl /opt/nixl || \
-git clone --depth=1 --recursive https://github.com/ai-dynamo/nixl /opt/nixl
+git clone --branch=v${NIXL_VERSION} --depth=1 --recursive https://github.com/ai-dynamo/nixl /usr/local || \
+git clone --depth=1 --recursive https://github.com/ai-dynamo/nixl /usr/local/
 
+# -----------------------------------------------------------------------------
 cd /opt/
-
+echo "Installing dependencies for NixL build..."
 export MAX_JOBS=$(nproc)
 ARCH=$(uname -m)
+export CPATH=/usr/local/cuda/include:$CPATH
+export LIBRARY_PATH=/usr/local/cuda/lib64:$LIBRARY_PATH
 
+# -----------------------------------------------------------------------------
+echo "UCX INSTALLATION STARTED"
 cd /usr/local/src
 git clone https://github.com/openucx/ucx.git && \
 cd ucx && \
@@ -30,17 +35,50 @@ git checkout v1.19.x && \
 make -j && \
 make -j install-strip && \
 ldconfig
+echo "UCX INSTALLATION COMPLETED"
 
+
+# -----------------------------------------------------------------------------
+echo "ETCD INSTALLATION..."
 # Navigate to the directory containing nixl's setup.py
-cd /opt/nixl
+cd /opt/
+git clone https://github.com/etcd-cpp-apiv3/etcd-cpp-apiv3.git &&\
+	cd etcd-cpp-apiv3 && mkdir build && cd build && \
+	cmake .. && make -j$(nproc) && make install
+ldconfig
+echo "ETCD INSTALLATION COMPLETED"
 
-pip3 install --upgrade meson pybind11 patchelf auditwheel
+# -----------------------------------------------------------------------------
+echo "Installing DPDK and DOCA SDK dependencies..."
+cd /opt/
+ARCH_SUFFIX=$(if [ "${ARCH}" = "aarch64" ]; then echo "arm64-sbsa"; else echo "${ARCH}"; fi) && \
+export PKG_CONFIG_PATH="/opt/mellanox/doca/lib/${ARCH_SUFFIX}-linux-gnu/pkgconfig:/opt/mellanox/dpdk/lib/${ARCH_SUFFIX}-linux-gnu/pkgconfig:$PKG_CONFIG_PATH" && \
+curl -fsSL https://linux.mellanox.com/public/repo/doca/3.0.0/ubuntu24.04/${ARCH_SUFFIX}/GPG-KEY-Mellanox.pub | \
+gpg --dearmor | tee /usr/share/keyrings/mellanox-archive-keyring.gpg && \
+echo "deb [signed-by=/usr/share/keyrings/mellanox-archive-keyring.gpg] https://linux.mellanox.com/public/repo/doca/3.0.0/ubuntu24.04/${ARCH_SUFFIX} ./" | \
+tee /etc/apt/sources.list.d/mellanox.list && \
+DEBIAN_FRONTEND=noninteractive apt update -y && \
+apt install -y --no-install-recommends \
+    mlnx-dpdk mlnx-dpdk-dev \
+    doca-sdk-common doca-sdk-dma doca-sdk-dpdk-bridge \
+    doca-sdk-eth doca-sdk-flow doca-sdk-rdma doca-all \
+    doca-sdk-gpunetio libdoca-sdk-gpunetio-dev
 
+echo "DPDK and DOCA SDK dependencies installed successfully."
+
+# -----------------------------------------------------------------------------
+echo "Installing Python dependencies..."
+cd /usr/local
+uv pip install --upgrade meson pybind11 patchelf auditwheel
+# -----------------------------------------------------------------------------
+echo "Building NixL..."
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
+export CMAKE_PREFIX_PATH=/usr/local:$CMAKE_PREFIX_PATH
 export MESON_ARGS="-Ddisable_mooncake_backend=false"
-rm -rf build && \
-mkdir build && \
-meson setup nixl-build -Ducx_path=/opt/ucx/ --prefix=/usr/local/nixl && \
-cd build && \
+rm -rf nixl-build && \
+mkdir nixl-build && \
+meson setup nixl-build --prefix=/usr/local/nixl --buildtype=release && \
+cd nixl-build && \
 ninja && \
 ninja install
 
@@ -49,7 +87,7 @@ export LD_LIBRARY_PATH=/usr/local/nixl/lib64/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/nixl/lib64/plugins:$LD_LIBRARY_PATH
 export NIXL_PLUGIN_DIR=/usr/local/nixl/lib64/plugins
 
-ARCH=$(uname -m)
+# -----------------------------------------------------------------------------
 echo "System architecture detected: ${ARCH}"
 LIB_DIR="/usr/local/nixl/lib/${ARCH}-linux-gnu"
 if [ -d "$LIB_DIR" ]; then
@@ -66,22 +104,20 @@ else
   echo "   Checked path: ${LIB_DIR}"
   echo "   No configuration changes were made."
 fi
+ln -s /opt/nvidia/nvda_nixl/lib/aarch64-linux-gnu/plugins/libplugin_UCX.so /usr/lib64/
 ldconfig
-
-
-
-cd /opt/nixl/src/bindings/rust && \
+# -----------------------------------------------------------------------------
+cd /usr/local/src/bindings/rust && \
 cargo build --release --locked
-cd /opt/nixl/
-pip3 wheel --wheel-dir=/opt/nixl/wheels . --verbose
-UNREPAIRED_WHEEL=$(find /opt/nixl/wheels -name "nixl-*linux_aarch64.whl")
-PLATFORM="manylinux_2_39_$(uname -m)"
+cd /usr/local/
+uv build --wheel --out-dir=/tmp/wheels . --verbose
+UNREPAIRED_WHEEL=$(find /tmp/wheels/ -name "nixl-*linux_aarch64.whl")
+WHL_PLATFORM="manylinux_2_39_$(uname -m)"
 uv pip install auditwheel
-uv run auditwheel repair --exclude libcuda.so.1 --exclude 'libssl*' --exclude 'libcrypto*' $UNREPAIRED_WHEEL --plat $WHL_PLATFORM --wheel-dir dist && \
-    /opt/nixl/contrib/wheel_add_ucx_plugins.py --ucx-lib-dir /usr/lib64 dist/*.whl
-pip3 install /opt/nixl/wheels/nixl*.whl
+uv run auditwheel repair --exclude libcuda.so.1 --exclude 'libssl*' --exclude 'libcrypto*' $UNREPAIRED_WHEEL --plat $WHL_PLATFORM --wheel-dir /usr/local/wheels/
+pip3 install /usr/local/wheels/nixl*.whl
 
-cd /opt/nixl
+cd /usr/local
 
 # Optionally upload to a repository using Twine
-twine upload --verbose /opt/nixl/wheels/nixl*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
+twine upload --verbose /usr/local/wheels/nixl*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
