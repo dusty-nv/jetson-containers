6341a9316835141dc66c720bfb2b1de72c6e4f5a
Johnny <johnnynuca14@gmail.com>
2025-06-25 18:00:38 +0200

    XFORMERS

--- packages/llm/dynamo/nixl/build.sh
+++ packages/llm/dynamo/nixl/build.sh
@@ -13,8 +13,6 @@ ARCH=$(uname -m)
 export CPATH=/usr/local/cuda/include:$CPATH
 export LIBRARY_PATH=/usr/local/cuda/lib64:$LIBRARY_PATH
 
-
-
 # -----------------------------------------------------------------------------
 echo "ETCD INSTALLATION..."
 # Navigate to the directory containing nixl's setup.py
@@ -53,23 +51,6 @@ ldconfig
 
 echo "UCX INSTALLATION COMPLETED"
 
-# -----------------------------------------------------------------------------
-echo "Installing DPDK and DOCA SDK dependencies..."
-cd /opt/
-ARCH_SUFFIX=$(if [ "${ARCH}" = "aarch64" ]; then echo "arm64-sbsa"; else echo "${ARCH}"; fi) && \
-export PKG_CONFIG_PATH="/opt/mellanox/doca/lib/${ARCH_SUFFIX}-linux-gnu/pkgconfig:/opt/mellanox/dpdk/lib/${ARCH_SUFFIX}-linux-gnu/pkgconfig:$PKG_CONFIG_PATH" && \
-curl -fsSL https://linux.mellanox.com/public/repo/doca/3.0.0/ubuntu24.04/${ARCH_SUFFIX}/GPG-KEY-Mellanox.pub | \
-gpg --dearmor | tee /usr/share/keyrings/mellanox-archive-keyring.gpg && \
-echo "deb [signed-by=/usr/share/keyrings/mellanox-archive-keyring.gpg] https://linux.mellanox.com/public/repo/doca/3.0.0/ubuntu24.04/${ARCH_SUFFIX} ./" | \
-tee /etc/apt/sources.list.d/mellanox.list && \
-DEBIAN_FRONTEND=noninteractive apt update -y && \
-apt install -y --no-install-recommends \
-    mlnx-dpdk mlnx-dpdk-dev \
-    doca-sdk-common doca-sdk-dma doca-sdk-dpdk-bridge \
-    doca-sdk-eth doca-sdk-flow doca-sdk-rdma doca-all \
-    doca-sdk-gpunetio libdoca-sdk-gpunetio-dev
-
-echo "DPDK and DOCA SDK dependencies installed successfully."
 
 # -----------------------------------------------------------------------------
 echo "Installing Python dependencies..."
@@ -89,22 +70,24 @@ ninja install
 # -----------------------------------------------------------------------------
 echo "System architecture detected: ${ARCH}"
 LIB_DIR="/usr/local/nixl/lib/${ARCH}-linux-gnu"
-if [ -d "$LIB_DIR" ]; then
-
-  echo "✅ Found NixL directory for ${ARCH}: ${LIB_DIR}"
-  export PLUGIN_DIR="${LIB_DIR}/plugins"
-  export NIXL_PLUGIN_DIR="/usr/local/nixl/lib/${ARCH}-linux-gnu/plugins"
-  echo "/usr/local/nixl/lib/${ARCH}-linux-gnu" > /etc/ld.so.conf.d/nixl.conf && \
-  echo "/usr/local/nixl/lib/${ARCH}-linux-gnu/plugins" >> /etc/ld.so.conf.d/nixl.conf && \
-  ldconfig
-  echo "NixL configuration for ${ARCH} applied successfully."
-else
-  echo "⚠️  Warning: NixL directory not found for architecture ${ARCH}."
-  echo "   Checked path: ${LIB_DIR}"
-  echo "   No configuration changes were made."
-fi
+echo "✅ Found NixL directory for ${ARCH}: ${LIB_DIR}"
+export PLUGIN_DIR="${LIB_DIR}/plugins"
+export NIXL_PLUGIN_DIR="/usr/local/nixl/lib/${ARCH}-linux-gnu/plugins"
+echo "/usr/local/nixl/lib/${ARCH}-linux-gnu" > /etc/ld.so.conf.d/nixl.conf && \
+echo "/usr/local/nixl/lib/${ARCH}-linux-gnu/plugins" >> /etc/ld.so.conf.d/nixl.conf && \
 ldconfig
 # -----------------------------------------------------------------------------
+
+cp /usr/lib/aarch64-linux-gnu/librdmacm.so.1 /usr/lib/ucx/
+cp /usr/lib/aarch64-linux-gnu/libibverbs.so.1 /usr/lib/ucx/
+cp /usr/lib/aarch64-linux-gnu/libmlx5.so.1 /usr/lib/ucx/
+cp /usr/lib/aarch64-linux-gnu/libnl-3.so.200.26.0 /usr/lib/ucx/
+cp /usr/lib/aarch64-linux-gnu/libnl-route-3.so.200.26.0 /usr/lib/ucx/
+cp /usr/lib/aarch64-linux-gnu/libm.so.6 /usr/lib/ucx/
+cp /usr/lib/aarch64-linux-gnu/libc.so.6 /usr/lib/ucx/
+cp /lib/ld-linux-aarch64.so.1 /usr/lib/ucx/
+
+export NIXL_PLUGIN_DIR="/usr/local/nixl/lib/${ARCH}-linux-gnu/plugins"
 cd /usr/local/nixl/
 rm -rf /tmp/wheels
 rm -rf /usr/local/nixl/wheels/
