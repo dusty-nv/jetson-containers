02cf5ef24bf2caf4bde25f5d5865b690956362bf
Johnny <johnnynuca14@gmail.com>
2025-06-26 21:36:01 +0200

    fix: update Dockerfile and build.sh to modify version constraints for compatibility

--- packages/vlm/videollama/build.sh
+++ packages/vlm/videollama/build.sh
@@ -11,6 +11,7 @@ cd /opt/videollama
 sed -i '/--extra-index-url https:\/\/download.pytorch.org\/whl\/cu118/d; s/==/>=/g' requirements.txt
 pip3 install -U -r requirements.txt
 sed -i 's/==/>=/g' pyproject.toml
+sed -i 's/~=/>=/g' pyproject.toml
 
 export MAX_JOBS="$(nproc)"
 export CMAKE_BUILD_PARALLEL_LEVEL=$MAX_JOBS
