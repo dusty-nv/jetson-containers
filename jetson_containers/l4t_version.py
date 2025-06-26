01e89aaada0cf1f6ff4a31b745295fbf1624c5bf
Johnny <johnnynuca14@gmail.com>
2025-06-26 14:39:53 +0200

    fix: adjust CUDA architectures for improved compatibility with newer versions

--- jetson_containers/l4t_version.py
+++ jetson_containers/l4t_version.py
@@ -326,9 +326,9 @@ def get_cuda_arch(l4t_version: str=None, cuda_version: str=None, format=list):
             if l4t_version.major >= 38:
                 cuda_architectures = [87, 110, 121]  # Ampere Orin, Thor 110, Spark 121
             else:
-                cuda_architectures = [87, 90, 100]  # Ampere Orin, Hopper GH200 90, Blackwell GB200 100
+                cuda_architectures = [87, 90, 100, 121]  # Ampere Orin, Hopper GH200 90, Blackwell GB200 100
                 if cuda_version >= Version('13.0'):
-                    cuda_architectures += [110, 121] # Thor 110, Spark 121
+                    cuda_architectures += [110] # Thor 110, Spark 121
     else:
         cuda_architectures = [
             80, 86,  # Ampere
