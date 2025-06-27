b9171182d74b34692a98d79184694f6a98eaaf3d
johnnynunez <johnnynuca14@gmail.com>
2025-06-27 19:54:57 +0200

    fix: update build.sh to remove branch specification for OpenCV repositories

--- packages/cv/opencv/build.sh
+++ packages/cv/opencv/build.sh
@@ -8,11 +8,11 @@ bash $TMP/install_deps.sh
 
 # clone source repos
 git clone --branch ${OPENCV_VERSION} --recursive https://github.com/opencv/opencv || \
-git clone --branch --recursive https://github.com/opencv/opencv
+git clone --recursive https://github.com/opencv/opencv
 git clone --branch ${OPENCV_VERSION} --recursive https://github.com/opencv/opencv_contrib || \
-git clone --branch --recursive https://github.com/opencv/opencv_contrib
+git clone --recursive https://github.com/opencv/opencv_contrib
 git clone --branch ${OPENCV_PYTHON} --recursive https://github.com/opencv/opencv-python || \
-git clone --branch --recursive https://github.com/opencv/opencv-python
+git clone --recursive https://github.com/opencv/opencv-python
 
 cd /opt/opencv-python/opencv
 git checkout --recurse-submodules ${OPENCV_VERSION}
