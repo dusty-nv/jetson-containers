ad96e1d7dac4e0fc40304bf929c3c82b3d2eeca1
johnnynunez <johnnynuca14@gmail.com>
2025-06-27 13:14:54 +0200

    feat: update config.py and add Dockerfile for NCCL setup

--- packages/cuda/cudnn/config.py
+++ packages/cuda/cudnn/config.py
@@ -108,7 +108,7 @@ elif IS_SBSA and IS_CONFIG:
     package = [
         cudnn_package('9.8',f'{CUDNN_URL}/9.8.0/local_installers/cudnn-local-repo-ubuntu2404-9.8.0_1.0-1_arm64.deb', cuda='12.8', requires='aarch64', packages="libcudnn9-cuda-12 libcudnn9-dev-cuda-12 libcudnn9-samples"),
         cudnn_package('9.10',f'{CUDNN_URL}/9.10.2/local_installers/cudnn-local-repo-ubuntu2404-9.10.2_1.0-1_arm64.deb', cuda='12.9', requires='aarch64', packages="libcudnn9-cuda-12 libcudnn9-dev-cuda-12 libcudnn9-samples"),
-        cudnn_package('10.0',f'{CUDNN_URL}/10.0.0/local_installers/cudnn-local-repo-ubuntu2404-10.0.0_1.0-1_amd64.deb', cuda='13.0', requires='x86_64', packages="libcudnn10-cuda-13 libcudnn10-dev-cuda-13 libcudnn10-samples"),
+        cudnn_package('10.0',f'{CUDNN_URL}/10.0.0/local_installers/cudnn-local-repo-ubuntu2404-10.0.0_1.0-1_amd64.deb', cuda='13.0', requires='aarch64', packages="libcudnn10-cuda-13 libcudnn10-dev-cuda-13 libcudnn10-samples"),
     ]
 elif IS_CONFIG:
     # x86_64
---
+++ packages/cuda/nccl/config.py
@@ -0,0 +1,6 @@
+from jetson_containers import PYTHON_VERSION, IS_SBSA
+from packaging.version import Version
+
+package['build_args'] = {
+    'NCCL_VERSION': '2.27.5',
+}
