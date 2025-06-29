0d6742ac2f65b0375d08a74ba5d11abd77895f02
Johnny <johnnynuca14@gmail.com>
2025-06-29 20:52:54 +0200

    Update config.py

--- packages/llm/sglang/sgl-kernel/config.py
+++ packages/llm/sglang/sgl-kernel/config.py
@@ -28,5 +28,5 @@ def sgl_kernel(version, branch=None, default=False):
     return pkg, builder
 
 package = [
-    sgl_kernel('0.1.9', branch='main', default=True),
+    sgl_kernel('0.2.0', branch='main', default=True),
 ]
