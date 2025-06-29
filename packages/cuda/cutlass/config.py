479e410902a275267f4a8ffd9a69eb27af82164c
johnnynunez <johnnynuca14@gmail.com>
2025-06-30 00:52:26 +0200

    fix: refactor cutlass function to improve parameter handling and simplify package creation

--- packages/cuda/cutlass/config.py
+++ packages/cuda/cutlass/config.py
@@ -1,36 +1,29 @@
-
-from jetson_containers import L4T_VERSION, CUDA_VERSION, update_dependencies
+from jetson_containers import CUDA_VERSION
 from packaging.version import Version
 
-def cutlass(version, cuda=None):
+def cutlass(version, version_spec=None, requires=None, default=False):
     pkg = package.copy()
 
-    pkg['name'] = f"cutlass:{version}"
+    if requires:
+        pkg['requires'] = requires
 
-    if not cuda:
-        cuda = version
+    pkg['name'] = f'cutlass:{version}'
 
-    if len(cuda.split('.')) > 2:
-        cuda = cuda[:-2]
-
-    pkg['depends'] = update_dependencies(pkg['depends'], f"cuda:{cuda}")
-
-    if len(version.split('.')) < 3:
-        version = version + '.0'
-
-    pkg['build_args'] = {'CUTLASS_VERSION': version}
+    pkg['build_args'] = {
+        'CUTLASS_VERSION': version,
+    }
 
     builder = pkg.copy()
-    builder['name'] = builder['name'] + '-builder'
-    builder['build_args'] = {**builder['build_args'], 'FORCE_BUILD': 'on'}
 
-    if Version(version) == CUDA_VERSION:
+    builder['name'] = f'cutlass:{version}-builder'
+    builder['build_args'] = {**pkg['build_args'], **{'FORCE_BUILD': 'on'}}
+
+    if default:
         pkg['alias'] = 'cutlass'
         builder['alias'] = 'cutlass:builder'
 
     return pkg, builder
 
-if L4T_VERSION.major >= 36:    # JetPack 6
-    package = [
-        cutlass('4.0.0'),
-    ]
+package = [
+    cutlass('4.0.0', default=True)
+]
