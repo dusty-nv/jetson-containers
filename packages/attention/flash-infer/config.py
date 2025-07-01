c0b87e633023912d60fa0b3cd405a8a262991c7c
johnnynunez <johnnynuca14@gmail.com>
2025-07-01 11:36:32 +0200

    fix: update flash_infer versions in config.py to include 0.2.8 and adjust default settings for 0.2.7

--- packages/attention/flash-infer/config.py
+++ packages/attention/flash-infer/config.py
@@ -33,5 +33,6 @@ package = [
     flash_infer('0.2.2', '0.2.2', default=False),
     flash_infer('0.2.2.post1', '0.2.2.post1', default=False),
     flash_infer('0.2.6.post1', '0.2.6.post1', default=False),
-    flash_infer('0.2.7', '0.2.7', default=(CUDA_VERSION >= Version('12.6'))),
+    flash_infer('0.2.7', '0.2.7'),
+    flash_infer('0.2.8', '0.2.8', default=(CUDA_VERSION >= Version('12.6'))),
 ]
