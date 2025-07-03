f56ef0ab14e2414a4fc0d93c8f6e9e0731859c7d
johnnynunez <johnnynuca14@gmail.com>
2025-07-03 23:02:54 +0200

    fix: update sgl_kernel version to 0.2.3 in config.py

--- packages/llm/sglang/sgl-kernel/config.py
+++ packages/llm/sglang/sgl-kernel/config.py
@@ -28,5 +28,5 @@ def sgl_kernel(version, branch=None, default=False):
     return pkg, builder
 
 package = [
-    sgl_kernel('0.2.1', branch='main', default=True),
+    sgl_kernel('0.2.3', branch='main', default=True),
 ]
