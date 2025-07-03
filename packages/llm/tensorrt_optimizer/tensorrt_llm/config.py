0c3e6c83b79f6bbe3c204ddac9bee453fc5ddf0f
johnnynunez <johnnynuca14@gmail.com>
2025-07-03 23:15:21 +0200

    fix: update LLAMA_CPP_VERSION_PY to 0.3.10 and default tensorrt_llm version to 0.22.0 in config.py

--- packages/llm/tensorrt_optimizer/tensorrt_llm/config.py
+++ packages/llm/tensorrt_optimizer/tensorrt_llm/config.py
@@ -1,11 +1,11 @@
 from jetson_containers import CUDA_ARCHITECTURES, CUDA_VERSION, update_dependencies
 from packaging.version import Version
-   
+
 def tensorrt_llm(version, branch=None, patch=None, src=None, depends=None, requires=None, default=False):
     trt_llm = package.copy()
 
     trt_llm['name'] = f'tensorrt_llm:{version}'
-    
+
     if not branch:
         branch = 'v' + version
         if len(branch.split('.')) < 3:
@@ -13,10 +13,10 @@ def tensorrt_llm(version, branch=None, patch=None, src=None, depends=None, requi
 
     if not patch:
         patch = 'patches/empty.diff'
-        
+
     if not src:
         src = 'sources/empty.tar.gz'
-        
+
     trt_llm['build_args'] = {
         'TRT_LLM_VERSION': version,
         'TRT_LLM_BRANCH': branch,
@@ -28,24 +28,24 @@ def tensorrt_llm(version, branch=None, patch=None, src=None, depends=None, requi
 
     if depends:
         trt_llm['depends'] = update_dependencies(trt_llm['depends'], depends)
-        
+
     #if tensorrt:
     #    trt_llm['depends'] = [f"tensorrt:{tensorrt}" if x=='tensorrt' else x for x in trt_llm['depends']]
-        
+
     if requires:
         trt_llm['requires'] = requires
-        
+
     builder = trt_llm.copy()
     builder['name'] = builder['name'] + '-builder'
     builder['build_args'] = {**builder['build_args'], 'FORCE_BUILD': 'on'}
-    
+
     if default:
         trt_llm['alias'] = 'tensorrt_llm'
         builder['alias'] = 'tensorrt_llm:builder'
-        
+
     return trt_llm, builder
 
 package = [
     tensorrt_llm('0.12', branch='v0.12.0-jetson', requires='<cu128', default=False),
-    tensorrt_llm('0.20.0', requires='>=cu126', default=True),
+    tensorrt_llm('0.22.0', requires='>=cu126', default=True),
 ]
