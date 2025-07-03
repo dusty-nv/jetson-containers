d9a9e85a08e1459c0c28a42bf29ce55bbb95c283
johnnynunez <johnnynuca14@gmail.com>
2025-07-03 23:10:53 +0200

    fix: update cuDSS package version to 0.6 for different architectures in config.py

--- packages/numeric/cudss/config.py
+++ packages/numeric/cudss/config.py
@@ -1,3 +1,11 @@
+import os
+from packaging.version import Version
+
+from jetson_containers import (
+    L4T_VERSION, JETPACK_VERSION, CUDA_VERSION,
+    CUDA_ARCHITECTURES, LSB_RELEASE, IS_SBSA, IS_TEGRA,
+    SYSTEM_ARM, DOCKER_ARCH, package_requires
+)
 
 def cuDSS(version, url, requires=None, default=False):
     """
@@ -17,15 +25,43 @@ def cuDSS(version, url, requires=None, default=False):
     return pkg
 
 
-package = [
-    cuDSS('0.5',
-        'https://developer.download.nvidia.com/compute/cudss/0.5.0/local_installers/cudss-local-tegra-repo-ubuntu2204-0.5.0_0.5.0-1_arm64.deb',
-        requires=['aarch64', '22.04'],
-        default=True,
-    ),
-    cuDSS('0.5',
-        'https://developer.download.nvidia.com/compute/cudss/0.5.0/local_installers/cudss-local-tegra-repo-ubuntu2404-0.5.0_0.5.0-1_arm64.deb',
-        requires=['aarch64', '24.04'],
-        default=True,
-    ),
-]
+if IS_TEGRA:
+    package = [
+        cuDSS('0.6',
+            'https://developer.download.nvidia.com/compute/cudss/0.6.0/local_installers/cudss-local-tegra-repo-ubuntu2204-0.6.0_0.6.0-1_arm64.deb',
+            requires=['tegra-aarch64', '22.04'],
+            default=True,
+        ),
+        cuDSS('0.6',
+            'https://developer.download.nvidia.com/compute/cudss/0.6.0/local_installers/cudss-local-tegra-repo-ubuntu2404-0.6.0_0.6.0-1_arm64.deb',
+            requires=['aarch64', '24.04'],
+            default=True,
+        ),
+    ]
+elif IS_SBSA:
+    package = [
+        cuDSS('0.6',
+              'https://developer.download.nvidia.com/compute/cudss/0.6.0/local_installers/cudss-local-repo-ubuntu2404-0.6.0_0.6.0-1_arm64.deb',
+              requires=['aarch64', '22.04'],
+              default=True,
+              ),
+        cuDSS('0.6',
+              'https://developer.download.nvidia.com/compute/cudss/0.6.0/local_installers/cudss-local-repo-ubuntu2404-0.6.0_0.6.0-1_arm64.deb',
+              requires=['aarch64', '24.04'],
+              default=True,
+              ),
+    ]
+else:
+    package = [
+        cuDSS('0.6',
+              'https://developer.download.nvidia.com/compute/cudss/0.6.0/local_installers/cudss-local-repo-ubuntu2204-0.6.0_0.6.0-1_amd64.deb',
+              requires=['x86_64', '22.04'],
+              default=True,
+              ),
+        cuDSS('0.6',
+              'https://developer.download.nvidia.com/compute/cudss/0.6.0/local_installers/cudss-local-repo-ubuntu2404-0.6.0_0.6.0-1_amd64.deb',
+              requires=['x86_64', '24.04'],
+              default=True,
+              ),
+    ]
+
