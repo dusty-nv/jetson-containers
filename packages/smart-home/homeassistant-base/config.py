afe79304e5b5ed4d5018459d9de514de477c4672
Mieszko Syty <mieszko@ms1design.pl>
2025-07-04 21:40:20 +0200

    bump `bashio` in `homeassistant-base` package

--- packages/smart-home/homeassistant-base/config.py
+++ packages/smart-home/homeassistant-base/config.py
@@ -2,7 +2,7 @@ def create_package(version, default=False) -> list:
     pkg = package.copy()
     pkg['name'] = f'homeassistant-base:{version}'
     pkg['build_args'] = {
-        'BASHIO_VERSION': '0.16.2',
+        'BASHIO_VERSION': '0.17.0',
         'TEMPIO_VERSION': '2024.11.2',
         'S6_OVERLAY_VERSION': '3.1.6.2'
     }
