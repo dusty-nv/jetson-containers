fb2cd44ebc4ecc7115bf2bcae5824d7d55c32445
johnnynunez <johnnynuca14@gmail.com>
2025-07-07 12:20:45 +0200

    llamacpp

--- packages/llm/llama_cpp/config.py
+++ packages/llm/llama_cpp/config.py
@@ -55,5 +55,5 @@ package = [
     # llama_cpp_python appears abandoned (4/25)
     # so we changed over to llama.cpp branches
     llama_cpp('b5255'),
-    llama_cpp('b5833', default=True)
+    llama_cpp('b5835', default=True)
 ]
