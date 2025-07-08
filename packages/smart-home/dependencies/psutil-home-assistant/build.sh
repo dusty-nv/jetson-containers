5ce65ac87832940139df143267868ffc5cc3f294
Mieszko Syty <mieszko@ms1design.pl>
2025-07-08 19:53:21 +0200

    add missing `wheel` install step to `psutil-home-assistant` build script

--- packages/smart-home/dependencies/psutil-home-assistant/build.sh
+++ packages/smart-home/dependencies/psutil-home-assistant/build.sh
@@ -5,6 +5,7 @@ set -xo pipefail
 git clone --branch=${PSUTIL_HA_BRANCH} https://github.com/home-assistant-libs/psutil-home-assistant /opt/psutil-home-assistant
 
 pip3 wheel --wheel-dir=$PIP_WHEEL_DIR --no-deps --verbose /opt/psutil-home-assistant
+pip3 install $PIP_WHEEL_DIR/psutil_home_assistant*.whl
 rm -rf /opt/psutil-home-assistant
 
 twine upload --skip-existing --verbose $PIP_WHEEL_DIR/psutil_home_assistant*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
