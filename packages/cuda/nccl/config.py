34fc29a7e74f84ee8180480dda6dd75ecbb4155d
johnnynunez <johnnynuca14@gmail.com>
2025-07-19 14:37:04 +0200

    Last commit before vacation

--- packages/cuda/nccl/config.py
+++ packages/cuda/nccl/config.py
@@ -2,5 +2,6 @@ from jetson_containers import PYTHON_VERSION, IS_SBSA
 from packaging.version import Version
 
 package['build_args'] = {
-    'NCCL_VERSION': '2.27.5',
+    'NCCL_VERSION': '2.27.6',
+    'IS_SBSA': IS_SBSA,
 }
