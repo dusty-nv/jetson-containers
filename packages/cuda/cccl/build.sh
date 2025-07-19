6d0bfbc69815c3976b3415e606d362c3c2f1bcf0
johnnynunez <johnnynuca14@gmail.com>
2025-07-19 17:24:30 +0200

    fix cccl

--- packages/cuda/cccl/build.sh
+++ packages/cuda/cccl/build.sh
@@ -15,9 +15,11 @@ export LIBRARY_PATH=${CUDA_HOME}/lib64/stubs:${LIBRARY_PATH}
 
 
 cd $REPO_DIR
-cd cccl/python/cuda_cccl
-pip3 install -U -r requirements.txt
+cmake --preset install -DCMAKE_INSTALL_PREFIX=/usr/local/
+cd build/install
+ninja install
+cd /opt/cuda_cccl/python/cuda_cccl
 pip3 wheel --wheel-dir=$REPO_DIR/wheels --verbose .
-pip3 install /opt/cuda_cccl/wheels/nvidia_cccl-*.whl
+pip3 install /opt/cuda_cccl/wheels/cuda_cccl-*.whl
 
-twine upload --verbose /opt/cuda_cccl/wheels/nvidia_cccl-*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
+twine upload --verbose /opt/cuda_cccl/wheels/cuda_cccl-*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
