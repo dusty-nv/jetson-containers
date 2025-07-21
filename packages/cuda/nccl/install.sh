ba44449d9c1409e2cd944a30ceac8f4b527a0569
Johnny <johnnync13@gmail.com>
2025-07-21 14:18:35 +0200

    Update install.sh

--- packages/cuda/nccl/install.sh
+++ packages/cuda/nccl/install.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -ex
-
-echo "Detected architecture: ${CUDA_ARCH}"
-# NCCL
+echo "Installing NVIDIA NCCL $NCCL_VERSION (NCCL)"
 if [[ "$CUDA_ARCH" != "tegra-aarch64" ]]; then
+  DEB="nccl-local-repo-${DISTRO}-${NCCL_VERSION}-cuda12.9_1.0-1_${CUDA_ARCH}.deb"
+fi
+cd $TMP
+
+if [ ! -f $TXZ ]; then
+  wget $WGET_FLAGS $MULTIARCH_URL/$DEB
+fi
+if [[ "$CUDA_ARCH" != "tegra-aarch64" ]]; then
+    dpkg -i $DEB
     apt-get update
+    dpkg -i $DEB
     apt-get -y install libnccl2 libnccl-dev
 fi
-rm -rf /var/lib/apt/lists/*
-apt-get clean
