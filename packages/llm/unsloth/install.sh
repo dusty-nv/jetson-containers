7a6383c6a5e3095913ca4fe58ea332e57f9fc1de
johnnynunez <johnnynuca14@gmail.com>
2025-07-24 17:02:45 +0200

    unsloth gemma3n

---
+++ packages/llm/unsloth/install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -ex
+
+apt-get update -y
+apt-get install -y --no-install-recommends \
+	libnuma-dev \
+	libsndfile1 \
+	libsndfile1-dev \
+	libprotobuf-dev \
+	libsm6 \
+	libxext6 \
+	libgl1
+
+rm -rf /var/lib/apt/lists/*
+apt-get clean
+
+if [ "$FORCE_BUILD" == "on" ]; then
+	echo "Forcing build of unsloth ${UNSLOTH_VERSION}"
+	exit 1
+fi
+
+pip3 install \
+	unsloth_zoo \
+	cut_cross_entropy \
+	unsloth==${UNSLOTH_VERSION}
---
+++ packages/llm/unsloth/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+print('testing unsloth...')
+
+from unsloth import FastModel
+
+
+print('unsloth OK')
