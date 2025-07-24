7995a174c1324d9e15c01100b4bef30182b7ae6d
Johnny <johnnynuca14@gmail.com>
2025-07-24 19:37:45 +0200

    fix build

--- packages/pytorch/torchao/config.py
+++ packages/pytorch/torchao/config.py
@@ -26,7 +26,7 @@ def torchao(version, requires=None, default=False):
 
 
 package = [
-    torchao('0.12.0', requires='>=36', default=True),
-    torchao('0.13.0', requires='>=36', default=False),
+    torchao('0.12.0', requires='>=36', default=False),
+    torchao('0.13.0', requires='>=36', default=True),
     torchao('0.14.0', requires='>=36', default=False),
 ]
