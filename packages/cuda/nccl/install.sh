3632d02d5a9df5b1b2340eb7caeb2e2bee046b75
Johnny <johnnync13@gmail.com>
2025-08-07 13:29:39 +0800

    Update install.sh

--- packages/cuda/nccl/install.sh
+++ packages/cuda/nccl/install.sh
@@ -2,9 +2,9 @@
 set -ex
 echo "Installing NVIDIA NCCL $NCCL_VERSION (NCCL)"
 if [[ "$CUDA_ARCH" == "aarch64" ]]; then
-  DEB="nccl-local-repo-${DISTRO}-${NCCL_VERSION}-cuda12.9_1.0-1_aarch64.deb"
+  DEB="nccl-local-repo-${DISTRO}-${NCCL_VERSION}-cuda13.0_1.0-1_arm64.deb"
 else
-  DEB="nccl-local-repo-${DISTRO}-${NCCL_VERSION}-cuda12.9_1.0-1_amd64.deb"
+  DEB="nccl-local-repo-${DISTRO}-${NCCL_VERSION}-cuda13.0_1.0-1_amd64.debdeb"
 fi
 cd $TMP
 if [ ! -f $TXZ ]; then
