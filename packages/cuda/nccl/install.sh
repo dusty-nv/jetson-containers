c1d6872e45377af03884271961dab2f584abeaa0
Johnny <johnnynuca14@gmail.com>
2025-08-08 01:18:14 +0800

    Added NCCL for Thor and Spark (#1262)
    * Update Dockerfile
    
    * Update install.sh
    
    * Update install.sh

--- packages/cuda/nccl/install.sh
+++ packages/cuda/nccl/install.sh
@@ -7,11 +7,10 @@ else
   DEB="nccl-local-repo-${DISTRO}-${NCCL_VERSION}-cuda13.0_1.0-1_amd64.debdeb"
 fi
 cd $TMP
-if [ ! -f $TXZ ]; then
-  wget $WGET_FLAGS $MULTIARCH_URL/$DEB
-fi
+wget $WGET_FLAGS $MULTIARCH_URL/$DEB
 if [[ "$CUDA_ARCH" != "tegra-aarch64" ]]; then
     dpkg -i $DEB
+    sudo cp /var/nccl-local-repo-ubuntu2404-2.27.7-cuda13.0/nccl-local-190A5319-keyring.gpg /usr/share/keyrings/
     apt-get update
     dpkg -i $DEB
     apt-get -y install libnccl2 libnccl-dev
