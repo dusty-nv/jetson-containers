6eef688f06ace65d74d6bea2554d0ed16a84e6b7
Mieszko Syty <mieszko@ms1design.pl>
2025-08-07 13:59:02 +0200

    add `flashinfer==0.2.10`

--- packages/attention/flashinfer/config.py
+++ packages/attention/flashinfer/config.py
@@ -34,5 +34,6 @@ package = [
     flash_infer('0.2.2.post1', '0.2.2.post1', default=False),
     flash_infer('0.2.6.post1', '0.2.6.post1', default=(CUDA_VERSION <= Version('12.6'))),
     flash_infer('0.2.7', '0.2.7'),
-    flash_infer('0.2.9', '0.2.9', default=(CUDA_VERSION >= Version('12.8'))),
+    flash_infer('0.2.9', '0.2.9'),
+    flash_infer('0.2.10', '0.2.10', default=(CUDA_VERSION >= Version('12.8'))),
 ]
