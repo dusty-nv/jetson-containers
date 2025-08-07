f73d106ba34c180057f7d0f8416e0c2882147beb
Johnny <johnnync13@gmail.com>
2025-08-07 23:05:42 +0800

    Update install.sh

--- packages/cuda/nccl/install.sh
+++ packages/cuda/nccl/install.sh
@@ -10,6 +10,7 @@ cd $TMP
 wget $WGET_FLAGS $MULTIARCH_URL/$DEB
 if [[ "$CUDA_ARCH" != "tegra-aarch64" ]]; then
     dpkg -i $DEB
+    sudo cp /var/nccl-local-repo-ubuntu2404-2.27.7-cuda13.0/nccl-local-190A5319-keyring.gpg /usr/share/keyrings/
     apt-get update
     dpkg -i $DEB
     apt-get -y install libnccl2 libnccl-dev
