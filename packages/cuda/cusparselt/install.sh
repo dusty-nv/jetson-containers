64619368ff76ccd3a0792f7546fbcf2af8ff2971
Johnny <johnnynuca14@gmail.com>
2025-08-08 11:45:09 +0800

    Update install.sh

--- packages/cuda/cusparselt/install.sh
+++ packages/cuda/cusparselt/install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -ex
-
 echo "Detected architecture: ${CUDA_ARCH}"
 if [[ "$CUDA_ARCH" == "aarch64" ]]; then
   wget $WGET_FLAGS \
@@ -11,10 +10,9 @@ else
   wget $WGET_FLAGS \
   https://developer.download.nvidia.com/compute/cusparselt/${CUSPARSELT_VERSION}/local_installers/cusparselt-local-repo-${DISTRO}-${CUSPARSELT_VERSION}_1.0-1_amd64.deb
 fi
-  dpkg -i cusparselt-local-*.deb
-  cp /var/cusparselt-local-*/cusparselt-*-keyring.gpg /usr/share/keyrings/
-  apt-get update
-  apt-get -y install libcusparselt0 libcusparselt-dev
-fi
+dpkg -i cusparselt-local-*.deb
+cp /var/cusparselt-local-*/cusparselt-*-keyring.gpg /usr/share/keyrings/
+apt-get update
+apt-get -y install libcusparselt0 libcusparselt-dev
 rm -rf /var/lib/apt/lists/*
 apt-get clean
