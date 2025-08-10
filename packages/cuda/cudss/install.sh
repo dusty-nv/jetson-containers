34737808743c742c9a1158ef53cb3083f4090517
Ori Nachum <ori.nachum@gmail.com>
2025-08-10 23:33:09 +0300

    fix(packages): cudss install deb file (#1271)

--- packages/cuda/cudss/install.sh
+++ packages/cuda/cudss/install.sh
@@ -3,13 +3,12 @@ set -ex
 echo "Detected architecture: ${CUDA_ARCH}"
 if [[ "$CUDA_ARCH" == "aarch64" ]]; then
   wget $WGET_FLAGS \
-  https://developer.download.nvidia.com/compute/cudss/${CUDSS_VERSION}/local_installers/cudss-local-repo-${DISTRO}-${CUDSS_VERSION}_*-1_arm64.deb
-else
+  https://developer.download.nvidia.com/compute/cudss/${CUDSS_VERSION}/local_installers/cudss-local-repo-${DISTRO}-${CUDSS_VERSION}_0.6.0-1_arm64.deb
 elif [[ "$CUDA_ARCH" == "tegra-aarch64" ]]; then
-  wget $WGET_FLAGS https://developer.download.nvidia.com/compute/cudss/${CUDSS_VERSION}/local_installers/cudss-local-tegra-repo-${DISTRO}-${CUDSS_VERSION}_*-1_arm64.deb
+  wget $WGET_FLAGS https://developer.download.nvidia.com/compute/cudss/${CUDSS_VERSION}/local_installers/cudss-local-tegra-repo-${DISTRO}-${CUDSS_VERSION}_0.6.0-1_arm64.deb
 else
   wget $WGET_FLAGS \
-  https://developer.download.nvidia.com/compute/cudss/${CUDSS_VERSION}/local_installers/cudss-local-repo-${DISTRO}-${CUDSS_VERSION}_*-1_amd64.deb
+  https://developer.download.nvidia.com/compute/cudss/${CUDSS_VERSION}/local_installers/cudss-local-repo-${DISTRO}-${CUDSS_VERSION}_0.6.0-1_amd64.deb
 fi
 dpkg -i cudss-local-*.deb
 cp /var/cudss-local-*/cudss-*-keyring.gpg /usr/share/keyrings/
