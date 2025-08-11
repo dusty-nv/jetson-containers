064072e40ebea0dbf366e41543677f04344d22a7
MS/1 Design <mieszko@ms1design.pl>
2025-08-11 22:14:03 +0200

    fix `nccl` on `orin`, introduce `build` pattern with `tarpack` (#1274)

--- packages/cuda/nccl/install.sh
+++ packages/cuda/nccl/install.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
-set -ex
-echo "Installing NVIDIA NCCL $NCCL_VERSION (NCCL)"
+set -euxo pipefail
+
+echo "Installing NVIDIA NCCL $NCCL_VERSION"
+
 if [[ "$CUDA_ARCH" == "aarch64" ]]; then
   DEB="nccl-local-repo-${DISTRO}-${NCCL_VERSION}-cuda13.0_1.0-1_arm64.deb"
+
 elif [[ "$CUDA_ARCH" == "tegra-aarch64" ]]; then
-  git clone https://github.com/NVIDIA/nccl
-  cd nccl
-  make -j src.build NVCC_GENCODE="-gencode=arch=compute_87,code=sm_87"
-  sudo apt install build-essential devscripts debhelper fakeroot
-  make pkg.debian.build
-  ls build/pkg/deb/
-  cd ..
+  if [[ "$FORCE_BUILD" == "on" ]]; then
+    echo "Forcing build of NVIDIA NCCL ${NCCL_VERSION}"
+    exit 1
+  fi
+
+  tarpack install "nccl-${NCCL_VERSION}"
+  exit 0
+
 else
   DEB="nccl-local-repo-${DISTRO}-${NCCL_VERSION}-cuda13.0_1.0-1_amd64.deb"
 fi
 
-if [[ "$CUDA_ARCH" != "tegra-aarch64" ]]; then
-    cd $TMP
-    wget $WGET_FLAGS $MULTIARCH_URL/$DEB
-    dpkg -i $DEB
-    sudo cp /var/nccl-local-repo-ubuntu2404-2.27.7-cuda13.0/nccl-local-190A5319-keyring.gpg /usr/share/keyrings/
-    apt-get update
-    dpkg -i $DEB
-    apt-get -y install libnccl2 libnccl-dev
-fi
+cd "$TMP"
+wget $WGET_FLAGS "$MULTIARCH_URL/$DEB"
+dpkg -i "$DEB"
+cp /var/nccl-local-repo-ubuntu2404-"$NCCL_VERSION"-cuda13.0/nccl-local-*-keyring.gpg /usr/share/keyrings/
+apt-get update
+apt-get -y install libnccl2 libnccl-dev
