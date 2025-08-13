96bd94ad251166ddd7c6528f6a4f1320e832c010
Chitoku YATO <cyato@nvidia.com>
2025-08-12 19:43:17 -0700

    Detail cuSPARSELt test

--- packages/cuda/cusparselt/test.sh
+++ packages/cuda/cusparselt/test.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 nvcc /test/test_cusparselt.cu \
+    -v \
     -I/usr/local/cuda/include \
-    -L/usr/lib/aarch64-linux-gnu \
-    -lcusparseLt -lcudart \
-    -Xlinker -rpath=/usr/lib/aarch64-linux-gnu \
+    -L/usr/local/cuda/lib64 -L/usr/lib/aarch64-linux-gnu \
+    -lcusparseLt -lcusparse -lcudart \
+    -Xlinker -rpath=/usr/local/cuda/lib64 -Xlinker -rpath=/usr/lib/aarch64-linux-gnu \
     -Wno-deprecated-gpu-targets \
     -Wno-deprecated-declarations \
     -o /test/test_cusparselt
