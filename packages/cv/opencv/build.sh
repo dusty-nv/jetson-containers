50001e351b3819546ee4657a0462163c3ffffc4a
Johnny <johnnynuca14@gmail.com>
2025-08-19 14:38:55 +0700

    Update build.sh

--- packages/cv/opencv/build.sh
+++ packages/cv/opencv/build.sh
@@ -6,20 +6,23 @@ cd /opt
 # install dependencies
 bash $TMP/install_deps.sh
 
-# clone source repos
-git clone --branch ${OPENCV_VERSION} --recursive https://github.com/opencv/opencv
-git clone --branch ${OPENCV_VERSION} --recursive https://github.com/opencv/opencv_contrib
-git clone --branch ${OPENCV_PYTHON} --recursive https://github.com/opencv/opencv-python
 
-cd /opt/opencv-python/opencv
-git checkout --recurse-submodules ${OPENCV_VERSION}
+git clone --branch "${OPENCV_VERSION}" --recursive https://github.com/opencv/opencv \
+  || git clone --recursive https://github.com/opencv/opencv
+
+git clone --branch "${OPENCV_VERSION}" --recursive https://github.com/opencv/opencv_contrib \
+  || git clone --recursive https://github.com/opencv/opencv_contrib
+
+git clone --branch "${OPENCV_PYTHON}" --recursive https://github.com/opencv/opencv-python \
+  || git clone --recursive https://github.com/opencv/opencv-python
+
+cd /opt/opencv-python/opencv|| git checkout --recurse-submodules origin/4.x
+git checkout --recurse-submodules ${OPENCV_VERSION} || git checkout --recurse-submodules origin/4.x
 cat modules/core/include/opencv2/core/version.hpp
-
 cd ../opencv_contrib
-git checkout --recurse-submodules ${OPENCV_VERSION}
-
+git checkout --recurse-submodules ${OPENCV_VERSION} || git checkout --recurse-submodules origin/4.x
 cd ../opencv_extra
-git checkout --recurse-submodules ${OPENCV_VERSION}
+git checkout --recurse-submodules ${OPENCV_VERSION} || git checkout --recurse-submodules origin/4.x
 
 cd ../
 
