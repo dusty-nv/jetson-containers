eddb2edceac4df3eba9580cf9ff6710e3f4494b3
Khalil BenKhaled <kbenkhaled@nvidia.com>
2025-08-25 09:44:03 -0700

    making sure flashinfer v2.5.0 can be built on orin

--- packages/attention/flashinfer/config.py
+++ packages/attention/flashinfer/config.py
@@ -33,6 +33,8 @@ package = [
     flash_infer('0.2.2', '0.2.2', default=False),
     flash_infer('0.2.2.post1', '0.2.2.post1', default=False),
     flash_infer('0.2.6.post1', '0.2.6.post1', default=False),
+    flash_infer('0.2.5', '0.2.5', default=False),
+    flash_infer('0.2.6.post1', '0.2.6.post1', default=(CUDA_VERSION <= Version('12.6'))),
     flash_infer('0.2.7', '0.2.7'),
     flash_infer('0.2.9', '0.2.9'),
     flash_infer('0.2.15', '0.2.15', default=(CUDA_VERSION >= Version('12.6'))),
