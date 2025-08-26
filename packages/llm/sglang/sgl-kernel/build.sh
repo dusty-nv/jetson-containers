8fff5ee8456e23a8ee5519b4d921a57e2d62b388
johnnynunez <johnnynuca14@gmail.com>
2025-08-26 21:42:39 +0200

    fix sglang

--- packages/llm/sglang/sgl-kernel/build.sh
+++ packages/llm/sglang/sgl-kernel/build.sh
@@ -6,7 +6,7 @@ set -x
 : "${PIP_WHEEL_DIR:?PIP_WHEEL_DIR must be set}"
 
 # Install Python deps
-pip3 install compressed-tensors decord2
+pip3 install compressed-tensors decord2 ninja setuptools wheel numpy uv scikit-build-core
 
 REPO_URL="https://github.com/sgl-project/sglang"
 REPO_DIR="/opt/sglang"
@@ -16,26 +16,7 @@ echo "Building SGLang ${SGL_KERNEL_VERSION}"
 # Clone either the tagged release or fallback to default branch
 echo "Tagged branch not found; cloning default branch"
 git clone --recursive --depth 1 "${REPO_URL}" "${REPO_DIR}"
-
-pip3 install --no-cache-dir ninja setuptools wheel numpy uv scikit-build-core
-echo "Building SGL-KERNEL"
-cd "${REPO_DIR}/sgl-kernel" || exit 1
-sed -i -E 's/(set[[:space:]]*\(ENABLE_BELOW_SM90)[[:space:]]+OFF/\1 ON/' CMakeLists.txt
-sed -i -E 's/(message[[:space:]]*\([[:space:]]*STATUS[[:space:]]*")[^"]*(")/\1ACTIVATED\2/' CMakeLists.txt
-sed -i '/^        "-gencode=arch=compute_80,code=sm_80"/a\        "-gencode=arch=compute_87,code=sm_87"' CMakeLists.txt
-sed -i '/^            "-gencode=arch=compute_80,code=sm_80"/a\            "-gencode=arch=compute_87,code=sm_87"' CMakeLists.txt
-sed -i -E '
-s/"torch>=2\.7\.1"/"torch>=2.7.0"/;
-s/"torchaudio==2\.7\.1"/"torchaudio>=2.7.0"/;
-s/"torchvision==0\.22\.1"/"torchvision>=0.22.0"/
-' pyproject.toml
-sed -i -E '
-s/torch==2\.7\.1/torch>=2.7.0/;
-s/torchaudio==2\.7\.1/torchaudio>=2.7.0/;
-s/torchvision==0\.22\.1/torchvision>=0.22.0/
-' pyproject.toml
 sed -i 's/==/>=/g' pyproject.toml
-
 
 # 🔧 Build step for sgl-kernel
 echo "🔨  Building sgl-kernel…"
@@ -43,8 +24,10 @@ if [[ "${IS_SBSA:-}" == "0" || "${IS_SBSA,,}" == "false" ]]; then
   export CORES=2
 else
   export CORES=32  # GH200
+  # Set the correct include path to point to the CCCL directory
+  export CPLUS_INCLUDE_PATH=$(echo /usr/local/cuda-*/targets/sbsa-linux/include/cccl | head -n1)
 fi
-pip3 install "cmake<4"  # Ensure compatible CMake version
+
 echo "🚀  Building with MAX_JOBS=${CORES} and CMAKE_BUILD_PARALLEL_LEVEL=${CORES}"
 export TORCH_CUDA_ARCH_LIST="${TORCH_CUDA_ARCH_LIST}"
 MAX_JOBS="${CORES}" \
