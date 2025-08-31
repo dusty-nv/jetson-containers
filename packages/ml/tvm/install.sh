87e4b5e93f70daef38e4cbc64ae9e8e0199bdbcb
johnnynunez <johnnynuca14@gmail.com>
2025-09-01 01:16:44 +0200

    clean tvm

--- packages/ml/tvm/install.sh
+++ packages/ml/tvm/install.sh
@@ -6,4 +6,8 @@ if [ "$FORCE_BUILD" == "on" ]; then
 	exit 1
 fi
 
-pip3 install apache-tvm==${TVM_VERSION}
+pip3 install apache-tvm==${TVM_VERSION} ||
+pip3 install --no-cache-dir tvm==${TVM_VERSION} ||
+pip3 install pre apache-tvm==${TVM_VERSION} ||
+pip3 install --no-cache-dir pre tvm==${TVM_VERSION} ||
+echo "failed to install TVM ${TVM_VERSION}"
