4c4336612208597274086270d79a153ba9a9ca3c
johnnynunez <johnnynuca14@gmail.com>
2025-08-31 17:41:02 +0200

    upgrade

--- packages/cuda/gdrcopy/config.py
+++ packages/cuda/gdrcopy/config.py
@@ -2,6 +2,6 @@ from jetson_containers import PYTHON_VERSION, LSB_RELEASE
 from packaging.version import Version
 
 package['build_args'] = {
-    'GDRCOPY_VERSION': 'main',
+    'GDRCOPY_VERSION': '2.5.1',
     'DISTRO': f"ubuntu{LSB_RELEASE.replace('.','')}",
 }
