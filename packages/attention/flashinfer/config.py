17118b70a0c42b10b5f1d96aafec361f9467cecb
johnnynunez <johnnynuca14@gmail.com>
2025-09-01 10:52:56 +0200

    fix

--- packages/attention/flashinfer/config.py
+++ packages/attention/flashinfer/config.py
@@ -35,5 +35,5 @@ package = [
     flash_infer('0.2.6.post1', '0.2.6.post1', default=False),
     flash_infer('0.2.7', '0.2.7'),
     flash_infer('0.2.9', '0.2.9'),
-    flash_infer('0.2.15', '0.2.15', default=(CUDA_VERSION >= Version('12.6'))),
+    flash_infer('0.3.0', '0.3.0', default=(CUDA_VERSION >= Version('12.6'))),
 ]
