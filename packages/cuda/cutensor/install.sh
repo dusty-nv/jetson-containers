a3239aee698b019aa9c8ef37f6da6e3fb00a3508
johnnynunez <johnnynuca14@gmail.com>
2025-09-02 16:39:29 +0200

    3dgrut

--- packages/cuda/cutensor/install.sh
+++ packages/cuda/cutensor/install.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
-if [[ "$CUDA_ARCH" == "tegra-aarch64" ]]; then
-  echo 'Cutensor not supported by tegra (Jetson Orin)'
-fi
-
 echo "Detected architecture: ${CUDA_ARCH}"
 if [[ "$CUDA_ARCH" == "aarch64" ]]; then
   wget $WGET_FLAGS \
   https://developer.download.nvidia.com/compute/cutensor/${CUTENSOR_VERSION}/local_installers/cutensor-local-repo-${DISTRO}-${CUTENSOR_VERSION}_${CUTENSOR_VERSION}-1_arm64.deb
+elseif [[ "$CUDA_ARCH" == "tegra-aarch64" ]]; then
+  echo 'Cutensor not supported by tegra (Jetson Orin)'
 else
   wget $WGET_FLAGS \
   https://developer.download.nvidia.com/compute/cutensor/${CUTENSOR_VERSION}/local_installers/cutensor-local-repo-${DISTRO}-${CUTENSOR_VERSION}_${CUTENSOR_VERSION}-1_amd64.deb
