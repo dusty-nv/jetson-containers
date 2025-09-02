f9235ef07f62daaa28ee60258fd914cf9f3428af
johnnynunez <johnnynuca14@gmail.com>
2025-09-02 14:40:22 +0200

    cutensor working

--- packages/cuda/cutensor/install.sh
+++ packages/cuda/cutensor/install.sh
@@ -13,7 +13,7 @@ else
   wget $WGET_FLAGS \
   https://developer.download.nvidia.com/compute/cutensor/${CUTENSOR_VERSION}/local_installers/cutensor-local-repo-${DISTRO}-${CUTENSOR_VERSION}_${CUTENSOR_VERSION}-1_amd64.deb
 fi
-  sudo dpkg -i cutensor-local-repo-*-*_1.0-1_amd64.deb
+  sudo dpkg -i cutensor-local-repo-${DISTRO}-${CUTENSOR_VERSION}_${CUTENSOR_VERSION}-1_*.deb
   sudo cp /var/cutensor-local-repo-*-*/cutensor-*-keyring.gpg /usr/share/keyrings/
   sudo apt-get update
   sudo apt-get -y install libcutensor2 libcutensor-dev libcutensor-doc
