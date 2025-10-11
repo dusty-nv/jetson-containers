96adcffef6a22a3408fd1981d30d7bb3a4fc7f77
Johnny <johnnynuca14@gmail.com>
2025-10-11 20:30:41 +0200

    Update build.sh

--- packages/attention/flashinfer/build.sh
+++ packages/attention/flashinfer/build.sh
@@ -31,17 +31,23 @@ else
 fi
 echo "Using NVSHMEM package: $NVSHMEM_PKG"
 
-python3 -m pip install --no-cache-dir build setuptools wheel ninja mpi4py nvidia-ml-py einops $NVSHMEM_PKG requests
+python3 -m pip install --no-cache-dir build setuptools wheel ninja mpi4py nvidia-ml-py einops $NVSHMEM_PKG requests tqdm
 
 export CUDA_HOME=/usr/local/cuda
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64/stubs:${LD_LIBRARY_PATH}
 export LIBRARY_PATH=${CUDA_HOME}/lib64/stubs:${LIBRARY_PATH}
-export FLASHINFER_CUDA_ARCH_LIST=$FLASHINFER_CUDA_ARCH_LIST
+export FLASHINFER_CUDA_ARCH_LIST="8.7 9.0a 10.0a 10.3a 11.0a 12.0a 12.1a"
 
 uv pip install apache-tvm-ffi
-python3 -m flashinfer.aot
-python3 -m build --no-isolation --wheel
-# Install AOT wheel
-python3 -m pip install dist/flashinfer_python-*.whl
+uv pip install -r requirements.txt
+cd /opt/flashinfer/
+uv build --no-build-isolation -v --wheel . --out-dir /opt/wheels/
+cd /opt/flashinfer/flashinfer-cubin
+uv build --no-build-isolation -v --wheel . --out-dir /opt/wheels/
+cd /opt/flashinfer/flashinfer-jit-cache
+uv build --no-build-isolation -v --wheel . --out-dir /opt/wheels/
 
-twine upload --verbose dist/flashinfer_python-*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
+# Install AOT wheel
+python3 -m pip install /opt/wheels/flashinfer-*.whl
+
+twine upload --verbose /opt/wheels/flashinfer-*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
