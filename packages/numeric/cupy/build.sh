d947b051fa8fc2b0c249b203dfaaebfd125385c1
johnnynunez <johnnynuca14@gmail.com>
2025-10-13 14:43:27 +0200

    fix

--- packages/numeric/cupy/build.sh
+++ packages/numeric/cupy/build.sh
@@ -7,10 +7,9 @@ git clone --branch ${CUPY_VERSION} --depth 1 --recursive https://github.com/cupy
 cd /opt/cupy
 
 uv pip install fastrlock
-uv build --wheel --no-build-isolation --wheel-dir /opt/cupy/wheels/ .
+uv build --wheel --no-build-isolation -v --out-dir /opt/cupy/wheels/ .
 cp /opt/cupy/wheels/*.whl /opt
 
 uv pip install /opt/cupy/wheels/*.whl
-uv pip show cupy && python3 -c 'import cupy; print(cupy.show_config())'
-
+uv pip show cupy
 twine upload --verbose /opt/cupy/wheels/*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
