1b0af190970fcbc7e60cb7d8461f974e73d540de
Johnny <johnnynuca14@gmail.com>
2025-10-15 05:19:15 +0200

    FlashAttention4

--- packages/attention/flash-attention/build.sh
+++ packages/attention/flash-attention/build.sh
@@ -44,14 +44,15 @@ CMAKE_BUILD_PARALLEL_LEVEL=$MAX_JOBS \
 FLASH_ATTENTION_FORCE_BUILD="TRUE" \
 FLASH_ATTENTION_FORCE_CXX11_ABI="FALSE" \
 FLASH_ATTENTION_SKIP_CUDA_BUILD="FALSE" \
-pip3 wheel . -v --no-deps -w /opt/flash-attention/wheels/
+uv build --wheel . -v --no-build-isolation --out-dir /opt/flash-attention/wheels/
 
 ls /opt
 cd /
 
-pip3 install /opt/flash-attention/wheels/flash_attn*.whl
+uv pip install /opt/flash-attention/wheels/flash_attn*.whl
 #pip3 show flash-attn && python3 -c 'import flash_attn'
 
+#FlashAttention4
 cd /opt/flash-attention/flash_attn/cute
 uv build --wheel -w /opt/flash-attention/wheels/
 uv pip install dist/flash_attn_cute-*.whl
