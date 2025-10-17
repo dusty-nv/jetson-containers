88f8d952d562a2e2c781a2ccbbcdbad28efc5e72
johnnynunez <johnnynuca14@gmail.com>
2025-10-17 16:04:19 +0200

    FlashAttention4

--- packages/attention/flash-attention/build.sh
+++ packages/attention/flash-attention/build.sh
@@ -55,6 +55,6 @@ uv pip install /opt/flash-attention/wheels/flash_attn*.whl
 #FlashAttention4
 cd /opt/flash-attention/flash_attn/cute
 uv build --wheel . -v --no-build-isolation --out-dir /opt/flash-attention/wheels/
-uv pip install /opt/flash-attention/flash_attn_cute*.whl
+uv pip install /opt/flash-attention/wheels/flash_attn_cute*.whl
 
 twine upload --verbose /opt/flash-attention/wheels/flash_attn*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
