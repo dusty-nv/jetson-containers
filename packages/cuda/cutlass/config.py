0459712d8129325b252107747fbd1144e2c5ca4c
johnnynunez <johnnynuca14@gmail.com>
2025-10-21 11:57:50 -0700

    cutlass v4.3.0

--- packages/cuda/cutlass/config.py
+++ packages/cuda/cutlass/config.py
@@ -26,5 +26,5 @@ def cutlass(version, version_spec=None, requires=None, default=False):
 
 package = [
     cutlass('3.9.2', default=(CUDA_VERSION < Version('13.0'))),
-    cutlass('4.2.1', default=(CUDA_VERSION >= Version('13.0'))),
+    cutlass('4.3.0', default=(CUDA_VERSION >= Version('13.0'))),
 ]
