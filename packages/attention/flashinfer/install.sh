a7a30f2a550dc3265d77c145db0ceac853be6022
johnnynunez <johnnynuca14@gmail.com>
2025-10-21 18:15:03 -0700

    fix

--- packages/attention/flashinfer/install.sh
+++ packages/attention/flashinfer/install.sh
@@ -6,7 +6,7 @@ if [ "$FORCE_BUILD" == "on" ]; then
 	exit 1
 fi
 
-uv pip install flashinfer-python==${FLASHINFER_VERSION} || \
-uv pip install flashinfer-python==${FLASHINFER_VERSION_SPEC}
+uv pip install flashinfer-python==${FLASHINFER_VERSION} --prerelease=allow || \
+uv pip install flashinfer-python==${FLASHINFER_VERSION_SPEC} --prerelease=allow
 
 uv pip show flashinfer_python && python3 -c 'import flashinfer'
