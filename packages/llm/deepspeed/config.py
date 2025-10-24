58720400a1cacfeaaafb5a5f57e60139ae5287a4
johnnynunez <johnnynuca14@gmail.com>
2025-10-24 13:11:07 -0700

    fix cuda 13

--- packages/llm/deepspeed/config.py
+++ packages/llm/deepspeed/config.py
@@ -2,22 +2,22 @@ from jetson_containers import CUDA_ARCHITECTURES
 
 def deepspeed(version, branch=None, default=False, build_args=None):
     pkg = package.copy()
-    
+
     if not branch:
         branch = f'v{version}'
-        
+
     pkg['name'] = f'deepspeed:{version}'
-    
+
     pkg['build_args'] = {
         'DEEPSPEED_VERSION': version,
         'DEEPSPEED_BRANCH': branch
     }
-    
+
     if build_args:
         pkg['build_args'].update(build_args)
-        
+
     builder = pkg.copy()
-    
+
     builder['name'] = f'deepspeed:{version}-builder'
     builder['build_args'] = {**pkg['build_args'], **{'FORCE_BUILD': 'on'}}
 
@@ -30,5 +30,5 @@ def deepspeed(version, branch=None, default=False, build_args=None):
 package = [
     deepspeed('0.9.5', build_args={'DS_BUILD_OPS': 0}),
     deepspeed('0.15.2'),
-    deepspeed('0.18.0', default=True),
+    deepspeed('0.18.1', default=True),
 ]
