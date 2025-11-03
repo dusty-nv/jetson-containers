cff0b5b9b4057cf28d911b3dbc58a0fb2a3e11ba
Johnny <johnnynuca14@gmail.com>
2025-11-03 12:54:29 +0100

    Refactor build script for decord installation

--- packages/multimedia/decord/build.sh
+++ packages/multimedia/decord/build.sh
@@ -7,21 +7,20 @@ git clone --depth=1 --recursive https://github.com/johnnynunez/decord2 /opt/deco
 
 cd /opt/decord
 mkdir build && cd build
--DUSE_CUDA=ON \
--DCMAKE_BUILD_TYPE=Release \
 CUDA_HOME="/usr/local/cuda" \
 NVCC_PATH="${CUDA_HOME}/bin/nvcc" \
--DCMAKE_CUDA_ARCHITECTURES="80;90;100;110;120" \
-cmake .. -DUSE_CUDA=ON -DCMAKE_BUILD_TYPE=Release
-make
+cmake .. -DUSE_CUDA=ON -DCMAKE_BUILD_TYPE=Release \
+-DUSE_CUDA=ON \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_CUDA_ARCHITECTURES="80;90;100;110;120"
+make -j$(nproc)
+cp libdecord.so ..
 
 cd ../python
 
 uv build --wheel --no-build-isolation --out-dir /opt/decord/wheels .
 uv pip install /opt/decord/wheels/decord*.whl
-
 cd /opt/decord
-
 
 # Optionally upload to a repository using Twine
 twine upload --verbose /opt/decord/wheels/decord*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
