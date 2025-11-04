21f3b2b3bfeb732e699b054c22c3d25371cc5e1c
Johnny <johnnynuca14@gmail.com>
2025-11-04 22:05:42 +0100

    Update flash_infer version from 0.5.0 to 0.5.1

--- packages/attention/flashinfer/config.py
+++ packages/attention/flashinfer/config.py
@@ -38,5 +38,5 @@ package = [
     flash_infer('0.2.6.post1', '0.2.6.post1', default=False),
     flash_infer('0.2.7', '0.2.7'),
     flash_infer('0.2.9', '0.2.9'),
-    flash_infer('0.5.0', '0.5.0', default=(CUDA_VERSION >= Version('12.6'))), # Thor compatibility
+    flash_infer('0.5.1', '0.5.1', default=(CUDA_VERSION >= Version('12.6'))), # Thor compatibility
 ]
