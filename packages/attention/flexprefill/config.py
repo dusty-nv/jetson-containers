a5286c7d488639cbd59452fb0c54fa09742c301b
johnnynunez <johnnynuca14@gmail.com>
2025-11-07 11:18:50 +0100

    fix

--- packages/attention/flexprefill/config.py
+++ packages/attention/flexprefill/config.py
@@ -1,4 +1,3 @@
-
 from jetson_containers import CUDA_VERSION
 from packaging.version import Version
 
@@ -6,23 +5,23 @@ def flex_prefill(version, requires=None, default=False):
     pkg = package.copy()
 
     if requires:
-        pkg['requires'] = requires   
+        pkg['requires'] = requires
 
     pkg['name'] = f'flexprefill:{version}'
-    
+
     pkg['build_args'] = {
         'FLEXPREFILL_VERSION': version,
     }
-    
+
     builder = pkg.copy()
-    
+
     builder['name'] = f'flexprefill:{version}-builder'
     builder['build_args'] = {**pkg['build_args'], **{'FORCE_BUILD': 'on'}}
 
     if default:
         pkg['alias'] = 'flexprefill'
         builder['alias'] = 'flexprefill:builder'
-        
+
     return pkg, builder
 
 package = [
