431e8a4a25b3f0e82a8bd74bc6c082e9e313ee2d
johnnynunez <johnnynuca14@gmail.com>
2025-11-11 12:39:55 +0100

    sageattention3

--- packages/attention/sage-attention/build.sh
+++ packages/attention/sage-attention/build.sh
@@ -3,8 +3,8 @@ set -ex
 
 echo "Building SageAttention ${SAGE_ATTENTION_VERSION}"
 
-git clone --depth=1 --branch=v${SAGE_ATTENTION_VERSION} https://github.com/johnnynunez/SageAttention /opt/sage-attention ||
-git clone --depth=1 https://github.com/johnnynunez/SageAttention /opt/sage-attention
+git clone --recursive --depth=1 --branch=v${SAGE_ATTENTION_VERSION} https://github.com/thu-ml/SageAttention /opt/sage-attention ||
+git clone --recursive --depth=1 https://github.com/thu-ml/SageAttention /opt/sage-attention
 
 cd /opt/sage-attention
 
@@ -12,13 +12,19 @@ export MAX_JOBS="$(nproc)"
 export CMAKE_BUILD_PARALLEL_LEVEL=$MAX_JOBS
 echo "Building with MAX_JOBS=$MAX_JOBS and CMAKE_BUILD_PARALLEL_LEVEL=$CMAKE_BUILD_PARALLEL_LEVEL"
 
+export TORCH_CUDA_ARCH_LIST=$TORCH_CUDA_ARCH_LIST
 MAX_JOBS="$(nproc)" \
 CMAKE_BUILD_PARALLEL_LEVEL=$MAX_JOBS \
-python3 setup.py --verbose bdist_wheel --dist-dir /opt/sage-attention/wheels
+uv build --wheel . -v --no-build-isolation --out-dir /opt/sage-attention/wheels/
 
-ls /opt/sage-attention/wheels
+# ls /opt/sage-attention/wheels
+
+cd /opt/sage-attention/sageattention3_blackwell/
+uv build --wheel . -v --no-build-isolation --out-dir /opt/sage-attention/wheels/
+
 cd /
 
 uv pip install /opt/sage-attention/wheels/sageattention*.whl
+uv pip install /opt/sage-attention/wheels/sageattn3*.whl
 
 twine upload --verbose /opt/sage-attention/wheels/sage-attention*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
--- packages/attention/sage-attention/config.py
+++ packages/attention/sage-attention/config.py
@@ -1,5 +1,4 @@
-
-from jetson_containers import CUDA_VERSION
+from jetson_containers import CUDA_VERSION, CUDA_ARCHITECTURES
 from packaging.version import Version
 
 def sage_attn(version, requires=None, default=False):
@@ -12,6 +11,7 @@ def sage_attn(version, requires=None, default=False):
 
     pkg['build_args'] = {
         'SAGE_ATTENTION_VERSION': version,
+        'TORCH_CUDA_ARCH_LIST': ';'.join([f'{x / 10:.1f}' for x in CUDA_ARCHITECTURES])
     }
 
     builder = pkg.copy()
