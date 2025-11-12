5d99b144d42fef93d5693f7fadcba179fcfd6e55
johnnynunez <johnnynuca14@gmail.com>
2025-11-12 20:22:56 +0100

    bump version

--- packages/robots/isaac-ros/config.py
+++ packages/robots/isaac-ros/config.py
@@ -3,7 +3,7 @@ from ..robots.ros import ros_container, ROS_DISTROS
 
 ISAAC_ROS_URL="https://github.com/NVIDIA-ISAAC-ROS"
 
-def isaac_ros(repo, version='3.2', base_url=ISAAC_ROS_URL, workspace='/opt/isaac-ros', depends=[], tag=None, **kwargs):
+def isaac_ros(repo, version='4.0', base_url=ISAAC_ROS_URL, workspace='/opt/isaac-ros', depends=[], tag=None, **kwargs):
     """
     Generate a container build with an Isaac ROS package added to it,
     which by default is under `/opt/isaac-ros` and sourced on startup.
@@ -15,18 +15,18 @@ def isaac_ros(repo, version='3.2', base_url=ISAAC_ROS_URL, workspace='/opt/isaac
         prefix='isaac_ros_'
         if repo.startswith(prefix):
             tag=repo[len(prefix):].replace('_', '-')
-        
+
     for idx, dep in enumerate(depends):
         if dep.startswith('isaac-ros:') and not any([x in dep for x in ROS_DISTROS + ['ROS_DISTRO']]):
             depends[idx] = dep + f"-{version}-$ROS_DISTRO-$ROS_PACKAGE"
-            
+
     kwargs.setdefault('name', f'isaac-ros:$TAG-{version}-$ROS_DISTRO-$ROS_PACKAGE')
     kwargs.setdefault('group', 'robots')
     kwargs.setdefault('distros', ['humble', 'jazzy'])
     kwargs.setdefault('test', ['test.sh'])
 
     return ros_container(package, f"{base_url}/{repo}", depends=depends, tag=tag, workspace=workspace, **kwargs)
-    
+
 package = [
     isaac_ros('isaac_ros_common', dockerfile='Dockerfile'),
     isaac_ros('isaac_ros_nitros', depends=['cuda-python', 'cv-cuda:cpp']),
