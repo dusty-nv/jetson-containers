2b43b4b635fe6fec443ef2492c0de6ffec764be3
johnnynunez <johnnynuca14@gmail.com>
2025-11-13 02:28:14 +0100

    bump version

--- packages/pytorch/install.sh
+++ packages/pytorch/install.sh
@@ -59,7 +59,7 @@ fi
 
 if [ "$PYTORCH_OFFICIAL_WHL" == "on" ]; then
 	echo "##### 🏢Using official PyTorch 2.9 WHL #####"
-  uv pip install torch==2.9.0 --index-url https://download.pytorch.org/whl/cu130
+  uv pip install torch==2.9.1 --index-url https://download.pytorch.org/whl/cu130
 else
   # on x86_64, install from pytorch nightly server
   # on aarch64, install from the Jetson pypi server ($PIP_INSTALL_URL)
