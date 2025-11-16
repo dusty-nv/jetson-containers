b5677aae1483456a85ceabc46d461b376dcab73b
Dimitar Dimitrov <5708006+D-G-Dimitrov@users.noreply.github.com>
2025-11-16 12:57:51 +0200

    SGLang:0.5.6 dependencies on Jetson SM 87 (#1521)

--- packages/attention/xformers/config.py
+++ packages/attention/xformers/config.py
@@ -1,12 +1,22 @@
-
 from jetson_containers import CUDA_VERSION
 from packaging.version import Version
+from ..pytorch.version import PYTORCH_VERSION
+from jetson_containers import update_dependencies
 
-def xformers(version, requires=None, default=True):
+
+def xformers(version, requires=None, pytorch=None, default=True):
     pkg = package.copy()
+
+    if pytorch:
+        pkg['depends'] = update_dependencies(pkg['depends'], f"pytorch:{pytorch}")
+    else:
+        pytorch = PYTORCH_VERSION
 
     if requires:
         pkg['requires'] = requires
+
+    if not isinstance(pytorch, Version):
+        pytorch = Version(pytorch)
 
     pkg['name'] = f'xformers:{version}'
 
@@ -19,15 +29,20 @@ def xformers(version, requires=None, default=True):
     builder['name'] = f'xformers:{version}-builder'
     builder['build_args'] = {**pkg['build_args'], **{'FORCE_BUILD': 'on'}}
 
-    if default:
+    if pytorch == PYTORCH_VERSION and default:
         pkg['alias'] = 'xformers'
         builder['alias'] = 'xformers:builder'
 
     return pkg, builder
 
+
 package = [
-    xformers('0.0.26', requires='<=cu122'),
-    xformers('0.0.29', requires='<cu126'), # support pytorch 2.5.1
-    xformers('0.0.30', default=(CUDA_VERSION < Version('12.6'))), # support pytorch 2.6.0
-    xformers('0.0.33', default=(CUDA_VERSION >= Version('12.6'))), # Support Blackwell and pytorch 2.7.0
+    xformers('0.0.26', pytorch='2.1', requires='<=cu122'),
+    xformers('0.0.29', pytorch='2.4', requires='<cu126'),  # support pytorch 2.5.1
+    xformers('0.0.30', pytorch='2.7', default=(CUDA_VERSION < Version('12.6'))),
+    # support pytorch 2.6.0
+    xformers('0.0.32.post2', pytorch='2.8', default=(CUDA_VERSION >= Version('12.6'))),
+    # support pytorch 2.8.0
+    xformers('0.0.33', default=(CUDA_VERSION >= Version('12.6'))),
+    # Support Blackwell and pytorch 2.9.0
 ]
