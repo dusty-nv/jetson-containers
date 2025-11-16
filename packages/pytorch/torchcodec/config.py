78193dc2e4aa75095056acfc0b337f8b29db6244
D-G-Dimitrov <5708006+D-G-Dimitrov@users.noreply.github.com>
2025-11-16 17:14:59 +0200

    Retire torchcodec:0.6.0

--- packages/pytorch/torchcodec/config.py
+++ packages/pytorch/torchcodec/config.py
@@ -42,7 +42,7 @@ def torchcodec(version, pytorch=None, depends=None, requires=None):
 
 package = [
     # JetPack 5/6/7
-    torchcodec('0.6.0', pytorch='2.8', depends=['ffmpeg:7.1'], requires='>=36'),
+    torchcodec('0.6.0', pytorch='2.7', depends=['ffmpeg:7.1'], requires='>=36'),
     torchcodec('0.7.0', pytorch='2.8', depends=['ffmpeg:7.1'], requires='>=36'),
     torchcodec('0.8.1', pytorch='2.9', requires='>=36'),
     torchcodec('0.8.1', pytorch='2.9.1', requires='>=36'),
