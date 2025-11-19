bad20c365aff0ed8c437e1b8b245f0f146ad629d
johnnynunez <johnnynuca14@gmail.com>
2025-11-19 08:07:47 +0100

    clean old bugged vllm versions for jetson Thor and DGX Spark

--- packages/llm/vllm/config.py
+++ packages/llm/vllm/config.py
@@ -32,15 +32,8 @@ def vllm(version, branch=None, requires=None, default=False, depends=None):
     return pkg, builder
 
 package = [
-    vllm('0.7.4', default=False),
-    vllm('0.8.4', depends=['flashinfer:0.2.1.post2'], default=False),
-    vllm('0.8.5', branch='v0.8.5.post1', depends=['flashinfer:0.2.2.post1'], default=False),
-    vllm('0.9.0', depends=['flashinfer'], default=False),
-    vllm('0.9.2', depends=['flashinfer'], default=False),
-    vllm('0.10.0', depends=['flashinfer'], default=False),
     vllm('0.10.2', depends=['flashinfer'], requires=['<=36', '<cu130'], default=False),
-    vllm('0.11.0', depends=['flashinfer'], default=False),
-    vllm('0.11.1', depends=['flashinfer:0.5.3'], default=True),
+    vllm('0.11.2', depends=['flashinfer:0.5.3'], default=True),
     vllm('0.12.0', depends=['flashinfer:0.5.3'], default=False),
     vllm('0.13.0', depends=['flashinfer'], default=False),
 ]
