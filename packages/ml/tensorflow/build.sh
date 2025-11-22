733de68dcf4afec5c0165942e343e99cd91cfad2
johnnynunez <johnnynuca14@gmail.com>
2025-11-22 04:27:03 +0100

    fix tensorflow blackwell

--- packages/ml/tensorflow/build.sh
+++ packages/ml/tensorflow/build.sh
@@ -11,15 +11,37 @@ git clone --branch "v${TENSORFLOW_VERSION}" --depth=1 https://github.com/tensorf
 git clone --depth=1 https://github.com/tensorflow/tensorflow.git /opt/tensorflow
 
 cd /opt/tensorflow
-# Set up environment variables for the configure script
-BUILD_FLAGS+='--clang_path=/usr/lib/llvm-21/bin/clang '
-BUILD_FLAGS+='--output_path=/opt/tensorflow/wheels/ '
-# Build jaxlib from source with detected versions
+mkdir -p /opt/tensorflow/wheels/
+
+# Initialize flags
+BUILD_FLAGS="--clang_path=/usr/lib/llvm-21/bin/clang --output_path=/opt/tensorflow/wheels/ "
+
 if [ "${IS_SBSA}" -eq 1 ]; then
     echo "Building for SBSA architecture"
     BUILD_FLAGS+='--cuda_compute_capabilities="sm_87,sm_89,sm_90,sm_100,sm_110,sm_120,sm_121" '
-    BUILD_FLAGS+='--cuda_version=13.0.0 --cudnn_version=9.12.0 '
-    BUILD_FLAGS+='--bazel_options=--config=ci_linux_aarch64_cuda13 '
+    BUILD_FLAGS+='--cuda_version=13.0.2 --cudnn_version=9.12.0 '
+
+    # --- BAZEL CONFIGURATION ---
+    # 2. Fix Abseil: Disable nullability attributes.
+    #    Clang 20+ enables them, but Abseil's headers place them incorrectly, causing syntax errors.
+    BUILD_FLAGS+='--bazel_options=--copt=-DABSL_HAVE_NULLABILITY_ATTRIBUTES=0 '
+    BUILD_FLAGS+='--bazel_options=--cxxopt=-DABSL_HAVE_NULLABILITY_ATTRIBUTES=0 '
+    BUILD_FLAGS+='--bazel_options=--copt=-D_Nullable= '
+    BUILD_FLAGS+='--bazel_options=--cxxopt=-D_Nullable= '
+    BUILD_FLAGS+='--bazel_options=--copt=-D_Nonnull= '
+    BUILD_FLAGS+='--bazel_options=--cxxopt=-D_Nonnull= '
+
+    # 3. Fix Protobuf: Polyfill the missing __is_bitwise_cloneable builtin.
+    #    We alias it to __is_trivially_copyable which is supported.
+    BUILD_FLAGS+='--bazel_options=--copt=-D__is_bitwise_cloneable=__is_trivially_copyable '
+    BUILD_FLAGS+='--bazel_options=--cxxopt=-D__is_bitwise_cloneable=__is_trivially_copyable '
+
+    # 4. Fix Abseil: Polyfill the missing __builtin_is_cpp_trivially_relocatable builtin.
+    #    NVCC/Clang interaction causes this builtin to be undefined despite passing feature checks.
+    #    We alias it to __is_trivially_copyable, which is a safe fallback for build purposes.
+    BUILD_FLAGS+='--bazel_options=--copt=-D__builtin_is_cpp_trivially_relocatable=__is_trivially_copyable '
+    BUILD_FLAGS+='--bazel_options=--cxxopt=-D__builtin_is_cpp_trivially_relocatable=__is_trivially_copyable '
+
 else
     echo "Building for non-SBSA architecture"
     BUILD_FLAGS+='--cuda_compute_capabilities="sm_87" '
@@ -28,7 +50,7 @@ fi
 
 
 # Build the TensorFlow pip package
-bazel build //tensorflow/tools/pip_package:wheel --action_env CLANG_CUDA_COMPILER_PATH="/usr/lib/llvm-18/bin/clang" --config=cuda_clang --repo_env=WHEEL_NAME=tensorflow --config=cuda --config=cuda_wheel --config=nonccl --copt=-Wno-sign-compare --copt=-Wno-gnu-offsetof-extensions --copt=-Wno-error=unused-command-line-argument
+bazel build //tensorflow/tools/pip_package:wheel --action_env CLANG_CUDA_COMPILER_PATH="/usr/lib/llvm-21/bin/clang" --config=cuda_clang --repo_env=WHEEL_NAME=tensorflow --config=cuda --config=cuda_wheel --config=nonccl --copt=-Wno-sign-compare --copt=-Wno-gnu-offsetof-extensions --copt=-Wno-error=unused-command-line-argument
 
 # Upload the wheels to mirror
 twine upload --verbose /opt/tensorflow/bazel-bin/tensorflow/tools/pip_package/wheel_house/tensorflow*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
