ad22294b3cfe0f0d70014ad7efa9b8ce0bc10812
johnnynunez <johnnynuca14@gmail.com>
2025-12-29 14:21:23 +0100

    fix colmap

--- packages/3d/3dvision/pycolmap/install.sh
+++ packages/3d/3dvision/pycolmap/install.sh
@@ -5,10 +5,34 @@ echo "Installing pycolmap ${PYCOLMAP_VERSION}"
 
 apt-get update && \
 apt-get install -y --no-install-recommends \
+    git \
+    cmake \
+    ninja-build \
+    build-essential \
+    libboost-program-options-dev \
+    libboost-graph-dev \
+    libboost-system-dev \
+    libeigen3-dev \
     libopenimageio-dev \
+    openimageio-tools \
+    libmetis-dev \
+    libgoogle-glog-dev \
+    libgtest-dev \
+    libgmock-dev \
+    libsqlite3-dev \
+    libglew-dev \
+    qt6-base-dev \
+    libqt6opengl6-dev \
+    libqt6openglwidgets6 \
+    libcgal-dev \
+    libceres-dev \
+    libsuitesparse-dev \
+    libcurl4-openssl-dev \
+    libssl-dev \
+    libopenblas-dev \
 && rm -rf /var/lib/apt/lists/* \
 && apt-get clean
-
+mkdir -p /usr/include/opencv4
 if [ "$FORCE_BUILD" == "on" ]; then
   echo "Forcing build of pycolmap ${PYCOLMAP_VERSION}"
   exit 1
@@ -17,6 +41,4 @@ fi
 PYCOLMAP_TARPACK_NAME="${PYCOLMAP_TARPACK_NAME:-colmap-${PYCOLMAP_VERSION}}"
 
 tarpack install "colmap-${PYCOLMAP_VERSION}" || {echo "tarpack install failed for colmap-${PYCOLMAP_VERSION}, falling back to pip."}
-
-# Fallback general (o arquitecturas no-tegra): instala desde PyPI
 uv pip install "pycolmap==${PYCOLMAP_VERSION}"
