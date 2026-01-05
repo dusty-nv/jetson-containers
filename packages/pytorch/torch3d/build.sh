4fedad11413e09421821c651c3154d24b53ee8b4
johnnynunez <johnnynuca14@gmail.com>
2026-01-05 15:02:53 +0100

    fix

--- packages/pytorch/torch3d/build.sh
+++ packages/pytorch/torch3d/build.sh
@@ -10,7 +10,9 @@ uv pip install scikit-image matplotlib imageio plotly opencv-contrib-python
 #export TORCH_CUDA_ARCH_LIST="8.7"
 export CUB_HOME=/usr/local/cuda-*/include/
 export MAX_JOBS=$(nproc)
-export NVCC_FLAGS="-static-global-template-stub=false"
+if [[ "$CUDA_VERSION" == "13.0" ]]; then
+  export NVCC_FLAGS="-static-global-template-stub=false"
+fi
 python3 setup.py --verbose bdist_wheel --dist-dir /opt
 
 cd ../
