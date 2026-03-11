5878f7ba54f72eac2016c2c30761188356523300
Paulo Henrique Trecenti <ptrecenti@gmail.com>
2026-03-11 17:23:07 +0000

    cudastack: revert to verified library versions to fix 404 errors

--- packages/cuda/cudastack/config.py
+++ packages/cuda/cudastack/config.py
@@ -105,11 +105,11 @@ def cuda_stack_args():
 
         # Additional libraries
         'CUDSS_VERSION': '0.7.1',
-        'CUSPARSELT_VERSION': '0.9.0',
-        'CUTENSOR_VERSION': '2.6.0',
+        'CUSPARSELT_VERSION': '0.8.1',
+        'CUTENSOR_VERSION': '2.5.0',
         'GDRCOPY_VERSION': '2.5.1',
         'NVPL_VERSION': '25.11',
-        'NVSHMEM_VERSION': '3.6.0',
+        'NVSHMEM_VERSION': '3.5.21',
 
         # Architecture and CUDA info
         'CUDA_ARCH': CUDA_ARCH,
