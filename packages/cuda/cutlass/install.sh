67c223094c82efe4ff303e985bd9de3873b82d24
D-G-Dimitrov <5708006+D-G-Dimitrov@users.noreply.github.com>
2026-04-14 12:22:58 +0300

    fix missing cutlass install packages

--- packages/cuda/cutlass/install.sh
+++ packages/cuda/cutlass/install.sh
@@ -6,7 +6,7 @@ if [ "$FORCE_BUILD" == "on" ]; then
 	exit 1
 fi
 
-uv pip install --no-cache-dir nvidia-cutlass==${CUTLASS_VERSION} pycute --prerelease=allow || echo "failed to install nvidia-cutlass for Python ${PYTHON_VERSION}"
+uv pip install --no-cache-dir nvidia-cutlass==${CUTLASS_VERSION} pycute --prerelease=allow || { echo "failed to install nvidia-cutlass for Python ${PYTHON_VERSION}"; exit 1; }
 
 # if #PYTHON_VERSION == "3.12" then install the DSL version
 echo "Installing nvidia-cutlass-dsl for Python 3.12"
